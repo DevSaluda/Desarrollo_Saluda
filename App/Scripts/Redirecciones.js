4f52f3e32165d44564170e738570903e78f4a597
SaludaDev <devsaluda1@gmail.com>
2024-01-12 16:15:24 -0600

    Update Redirecciones.js

--- App/Scripts/Redirecciones.js
+++ App/Scripts/Redirecciones.js
@@ -1,18 +1,18 @@
 // Se refiere al login del punto de venta en su version 3, el punto de venta aun se maneja en version v2
 function POSV(){
-  location.href="https://saludapos.com/App/Secure/POSV3"
+  location.href="Secure/POSV3"
 }
 
 
 
 
 function Servicios(){
-  location.href="https://saludapos.com/App/Secure/ServicioEspecial"
+  location.href="Secure/ServicioEspecial"
 }
 
 
 function Citas(){
-  location.href="https://saludapos.com/App/Secure/IngresoAgenda" 
+  location.href="Secure/IngresoAgenda" 
 }
 
 /*Redirecciones para volver a identificacion de perfiles, y soporte tecnico */
@@ -20,5 +20,5 @@ function Citas(){
 
 
 function Home(){
-  location.href="https://saludapos.com/App/"
+  location.href=""
 }
