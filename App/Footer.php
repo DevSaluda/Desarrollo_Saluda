dc8c62a7a75cc96fbdcf2fc773fe98d4cce8337e
SaludaDev <devsaluda1@gmail.com>
2024-01-12 14:49:06 -0600

    Localizador

---
+++ App/Footer.php
@@ -0,0 +1,23 @@
+<footer class="page-footer font-small blue">
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/608050d65eb20e09cf35417b/1f3qjo7oa';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">Copyright &copy; 2020 <a href="https://somosgrupoe.com/">Somos Grupo E</a> <br>
+  <b>Control Farmacia</b> V 1.3.1
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
