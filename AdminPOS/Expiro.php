c1d0da46d8f15901260c1cec6d470768d571c332
SaludaDev <devsaluda1@gmail.com>
2024-01-15 15:46:24 -0600

    Update Expiro.php

--- AdminPOS/Expiro.php
+++ AdminPOS/Expiro.php
@@ -2,6 +2,8 @@
   <!-- Agrega este código para incluir jQuery desde un CDN -->
   <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI/t1i8n4LgLunLBNxMZi1GjeFqeaCDd1nIGF/O8=" crossorigin="anonymous"></script>
 </head>
+
+<?php include "Modales/Expirado.php"?>
 <!-- Agrega este código al final de tu archivo PHP -->
 <script>
   // Utilizando jQuery para activar el modal automáticamente
