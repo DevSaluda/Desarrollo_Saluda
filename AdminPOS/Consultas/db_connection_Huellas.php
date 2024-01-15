5b04196babb44dbdae1a19cb66391913f3db535c
SaludaDev <devsaluda1@gmail.com>
2024-01-15 16:19:12 -0600

    Update db_connection_Huellas.php

--- AdminPOS/Consultas/db_connection_Huellas.php
+++ AdminPOS/Consultas/db_connection_Huellas.php
@@ -2,9 +2,9 @@
 
 /* Database connection start */
 $servername = "localhost";
-$username = "u155356178_SaludaHuellas";
+$username = "u155356178_SaludaCapturad";
 $password = ";5Kt&rY@fNmPp8/B0VU3wfbI324\Zavp2zJ:9TLx{]L&QMcmh";
-$dbname = "u155356178_SaludaCapturad";
+$dbname = "u155356178_SaludaHuellas";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("No podemos conectar a la base de datos: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("algo salio mal, no podemos conectarnos a la base de datos %s\n", mysqli_connect_error());
