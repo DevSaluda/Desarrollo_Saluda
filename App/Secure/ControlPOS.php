96355d60daaf4a616021b538f59b15fd71812328
SaludaDev <devsaluda1@gmail.com>
2024-01-15 10:34:50 -0600

    Update ControlPOS.php

--- App/Secure/ControlPOS.php
+++ App/Secure/ControlPOS.php
@@ -6,49 +6,49 @@ if($_SESSION["SuperAdmin"])	//Condicion admin
 {
 	
 
-	header("location:https://controlfarmacia.com/AdminPOS");	
+	header("location:https://saludapos.com/AdminPOS");	
 
 }
 if($_SESSION["VentasPos"])	//Condicion personal
 {
 
-	header("location: https://controlfarmacia.com/POS2"); 
+	header("location: https://saludapos.com/POS2"); 
 }
 
 if($_SESSION["AdminPOS"])	//Condicion personal
 {
 
-	header("location: https://controlfarmacia.com/AdministracionPOS"); 
+	header("location: https://saludapos.com/AdministracionPOS"); 
 }
 
 
 if($_SESSION["LogisticaPOS"])	//Condicion personal
 {
 
-	header("location: https://controlfarmacia.com/POSLogistica"); 
+	header("location: https://saludapos.com/POSLogistica"); 
 }
 
 if($_SESSION["ResponsableCedis"])	//Condicion personal
 {
 
-	header("location: https://controlfarmacia.com/CEDIS"); 
+	header("location: https://saludapos.com/CEDIS"); 
 }
 
 if($_SESSION["ResponsableInventarios"])	//Condicion personal
 {
 
-	header("location: https://controlfarmacia.com/Inventarios"); 
+	header("location: https://saludapos.com/Inventarios"); 
 }
 
 if($_SESSION["ResponsableDeFarmacias"])	//Condicion personal
-	{	header("location: https://controlfarmacia.com/ResponsableDeFarmacias");
+	{	header("location: https://saludapos.com/ResponsableDeFarmacias");
 	}
 	if($_SESSION["CoordinadorDental"])	//Condicion personal
-	{	header("location: https://controlfarmacia.com/JefeDental");
+	{	header("location: https://saludapos.com/JefeDental");
 	}
 	if($_SESSION["Supervisor"])	//Condicion personal
-	{	header("location: https://controlfarmacia.com/CEDISMOVIL");
+	{	header("location: https://saludapos.com/CEDISMOVIL");
 	}
 	if($_SESSION["JefeEnfermeros"])	//Condicion personal
-	{	header("location: https://controlfarmacia.com/JefaturaEnfermeria");
+	{	header("location: https://saludapos.com/JefaturaEnfermeria");
 	}
