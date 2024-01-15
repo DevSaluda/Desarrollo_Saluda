9d57f48a439f0db36602cabc63cf60c5bb281920
SaludaDev <devsaluda1@gmail.com>
2024-01-15 15:47:14 -0600

    Update Expiro.php

--- AdminPOS/Expiro.php
+++ AdminPOS/Expiro.php
@@ -1,4 +1,10 @@
 <head>
+
+  <!-- Agrega estos enlaces para incluir los estilos de Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+WyD8iUueNW4PqGg0XlFgkeqRUmzC6dY4WI" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
   <!-- Agrega este código para incluir jQuery desde un CDN -->
   <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI/t1i8n4LgLunLBNxMZi1GjeFqeaCDd1nIGF/O8=" crossorigin="anonymous"></script>
 </head>
