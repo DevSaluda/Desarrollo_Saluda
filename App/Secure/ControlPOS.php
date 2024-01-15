66bef43ddb098ed2bdd789ef14aa6600babd2690
SaludaDev <devsaluda1@gmail.com>
2024-01-15 11:13:18 -0600

    Update ControlPOS.php

--- App/Secure/ControlPOS.php
+++ App/Secure/ControlPOS.php
@@ -2,53 +2,55 @@
 
 session_start();
 include ("Scripts/POS.php");
-if($_SESSION["SuperAdmin"])	//Condicion admin
-{
-	
 
-	header("location:https://saludapos.com/AdminPOS");	
+// Mapeo de roles a URLs de redirección
+$roleRedirects = [
+    "SuperAdmin" => "https://saludapos.com/AdminPOS",
+    "VentasPos" => "https://saludapos.com/POS2",
+    "AdminPOS" => "https://saludapos.com/AdministracionPOS",
+    "LogisticaPOS" => "https://saludapos.com/POSLogistica",
+    "ResponsableCedis" => "https://saludapos.com/CEDIS",
+    "ResponsableInventarios" => "https://saludapos.com/Inventarios",
+    "ResponsableDeFarmacias" => "https://saludapos.com/ResponsableDeFarmacias",
+    "CoordinadorDental" => "https://saludapos.com/JefeDental",
+    "Supervisor" => "https://saludapos.com/CEDISMOVIL",
+    "JefeEnfermeros" => "https://saludapos.com/JefaturaEnfermeria",
+];
 
-}
-if($_SESSION["VentasPos"])	//Condicion personal
-{
-
-	header("location: https://saludapos.com/POS2"); 
-}
-
-if($_SESSION["AdminPOS"])	//Condicion personal
-{
-
-	header("location: https://saludapos.com/AdministracionPOS"); 
-}
-
-
-if($_SESSION["LogisticaPOS"])	//Condicion personal
-{
-
-	header("location: https://saludapos.com/POSLogistica"); 
+// Función para redirigir según el rol
+function redirectToRole($role) {
+    global $roleRedirects;
+    if (isset($roleRedirects[$role])) {
+        header("location: " . $roleRedirects[$role]);
+        exit();
+    }
 }
 
-if($_SESSION["ResponsableCedis"])	//Condicion personal
-{
-
-	header("location: https://saludapos.com/CEDIS"); 
+// Verificar sesiones y redirigir
+if ($_SESSION["SuperAdmin"]) {
+    redirectToRole("SuperAdmin");
+} elseif ($_SESSION["VentasPos"]) {
+    redirectToRole("VentasPos");
+} elseif ($_SESSION["AdminPOS"]) {
+    redirectToRole("AdminPOS");
+} elseif ($_SESSION["LogisticaPOS"]) {
+    redirectToRole("LogisticaPOS");
+} elseif ($_SESSION["ResponsableCedis"]) {
+    redirectToRole("ResponsableCedis");
+} elseif ($_SESSION["ResponsableInventarios"]) {
+    redirectToRole("ResponsableInventarios");
+} elseif ($_SESSION["ResponsableDeFarmacias"]) {
+    redirectToRole("ResponsableDeFarmacias");
+} elseif ($_SESSION["CoordinadorDental"]) {
+    redirectToRole("CoordinadorDental");
+} elseif ($_SESSION["Supervisor"]) {
+    redirectToRole("Supervisor");
+} elseif ($_SESSION["JefeEnfermeros"]) {
+    redirectToRole("JefeEnfermeros");
 }
+// Agrega aquí cualquier otra lógica que puedas necesitar
 
-if($_SESSION["ResponsableInventarios"])	//Condicion personal
-{
-
-	header("location: https://saludapos.com/Inventarios"); 
-}
-
-if($_SESSION["ResponsableDeFarmacias"])	//Condicion personal
-	{	header("location: https://saludapos.com/ResponsableDeFarmacias");
-	}
-	if($_SESSION["CoordinadorDental"])	//Condicion personal
-	{	header("location: https://saludapos.com/JefeDental");
-	}
-	if($_SESSION["Supervisor"])	//Condicion personal
-	{	header("location: https://saludapos.com/CEDISMOVIL");
-	}
-	if($_SESSION["JefeEnfermeros"])	//Condicion personal
-	{	header("location: https://saludapos.com/JefaturaEnfermeria");
-	}
+// Redirección predeterminada o mensaje de error si ninguna condición se cumple
+header("location: https://saludapos.com/pagina-de-error");
+exit();
+?>
