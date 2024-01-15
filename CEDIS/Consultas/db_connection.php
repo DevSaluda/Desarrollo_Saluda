f4fbd4f07147a4b252b8b40ddb34dd7b0844433b
WebSite403 <wsite2816@gmail.com>
2024-01-15 16:31:37 -0600

    conexion

--- CEDIS/Consultas/db_connection.php
+++ CEDIS/Consultas/db_connection.php
@@ -2,8 +2,8 @@
 
 /* Database connection start */
 $servername = "localhost";
-$username = "somosgr1_SHWEB";
-$password = "yH.0a-v?T*1R";
+$username = "u155356178_SaludDevCenter";
+$password = "uE;bAISz;*6c|I4PvEnfSys324\Zavp2zJ:9TLx{]L&QMcmhAdmSCDBSN3iH4UV3D24WMF@2024myV>";
 $dbname = "u155356178_saludapos";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("No podemos conectar a la base de datos: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
