a5aeb050a54a73cad3327948d7dc098c553219c5
WebSite403 <wsite2816@gmail.com>
2024-01-16 10:00:57 -0600

    bd

--- CEDIS/Consultas/Consultas.php
+++ CEDIS/Consultas/Consultas.php
@@ -1,32 +1,42 @@
-<?php 
+<?php
 date_default_timezone_set("America/Monterrey");
 session_start();
-if(!isset($_SESSION['ResponsableCedis'])){
-	header("Location: Expiro.php");
+
+if (!isset($_SESSION['ResponsableCedis'])) {
+    header("Location: Expiro.php");
+    exit();
 }
-include_once("../db_connection.php");
-$sql = "SELECT PersonalPOS.Pos_ID,PersonalPOS.Nombre_Apellidos,PersonalPOS.file_name,PersonalPOS.Fk_Usuario,PersonalPOS.Fk_Sucursal,PersonalPOS.ID_H_O_D,
-Roles_Puestos.ID_rol,Roles_Puestos.Nombre_rol, SucursalesCorre.ID_SucursalC,SucursalesCorre.Nombre_Sucursal from PersonalPOS,Roles_Puestos,SucursalesCorre 
-where PersonalPOS.Fk_Usuario = Roles_Puestos.ID_rol and PersonalPOS.Fk_Sucursal = SucursalesCorre.ID_SucursalC and PersonalPOS.Pos_ID='".$_SESSION['ResponsableCedis']."'";
-$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
+
+include_once("../db_connect.php");
+
+// Evitar inyección SQL utilizando sentencias preparadas
+$responsableCedis = mysqli_real_escape_string($conn, $_SESSION['ResponsableCedis']);
+
+$sql = "SELECT PersonalPOS.Pos_ID, PersonalPOS.Nombre_Apellidos, PersonalPOS.file_name, PersonalPOS.Fk_Usuario,
+        PersonalPOS.Fk_Sucursal, PersonalPOS.ID_H_O_D, Roles_Puestos.ID_rol, Roles_Puestos.Nombre_rol,
+        SucursalesCorre.ID_SucursalC, SucursalesCorre.Nombre_Sucursal
+        FROM PersonalPOS
+        INNER JOIN Roles_Puestos ON PersonalPOS.Fk_Usuario = Roles_Puestos.ID_rol
+        INNER JOIN SucursalesCorre ON PersonalPOS.Fk_Sucursal = SucursalesCorre.ID_SucursalC
+        WHERE PersonalPOS.Pos_ID='$responsableCedis'";
+
+$resultset = mysqli_query($conn, $sql);
+
+// Manejo de errores
+if (!$resultset) {
+    die("Error en la consulta: " . mysqli_error($conn));
+}
+
 $row = mysqli_fetch_assoc($resultset);
-$hora = date('G'); if (($hora >= 0) AND ($hora < 6)) 
-  { 
-    
-    $mensaje = "Hola, que tengas una excelente madrugada."; 
-  } 
-  else if (($hora >= 6) AND ($hora < 12)) 
-  { 
-    $mensaje = "Buenos días"; 
-  } 
-  else if (($hora >= 12) AND ($hora < 18)) 
-  { 
-    $mensaje = "Buenas tardes"; 
-  } 
-  else
-  { 
-  $mensaje = "Buenas noches"; 
-  } 
 
-   ?>
-
+$hora = date('G');
+if ($hora >= 0 && $hora < 6) {
+    $mensaje = "Hola, que tengas una excelente madrugada.";
+} elseif ($hora >= 6 && $hora < 12) {
+    $mensaje = "Buenos días";
+} elseif ($hora >= 12 && $hora < 18) {
+    $mensaje = "Buenas tardes";
+} else {
+    $mensaje = "Buenas noches";
+}
+?>
