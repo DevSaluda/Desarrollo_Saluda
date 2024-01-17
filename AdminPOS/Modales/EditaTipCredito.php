658e03a383e5e1227864e15dc43be875beac006d
SaludaDev <devsaluda1@gmail.com>
2024-01-17 09:41:07 -0600

    Update EditaTipCredito.php

--- AdminPOS/Modales/EditaTipCredito.php
+++ AdminPOS/Modales/EditaTipCredito.php
@@ -1,7 +1,7 @@
-<?
+<?php
 include "../Consultas/db_connection.php";
 include "../Consultas/Consultas.php";
-include "../Consultas/Sesion.php";
+
 $user_id=null;
 $sql1= "SELECT * FROM Tipos_Credit_POS WHERE ID_H_O_D='".$row['ID_H_O_D']."' AND ID_Tip_Cred = ".$_POST["id"];
 $query = $conn->query($sql1);
@@ -15,7 +15,7 @@ while ($r=$query->fetch_object()){
   }
 ?>
 
-<? if($Especialistas!=null):?>
+<?php if($Especialistas!=null):?>
 
 <form action="javascript:void(0)" method="post" id="EditaTipoCreditos" >
 <div class="form-group">
@@ -23,7 +23,7 @@ while ($r=$query->fetch_object()){
     <div class="input-group mb-3">
   <div class="input-group-prepend">  <span class="input-group-text" id="Tarjeta"><i class="fas fa-receipt"></i></span>
   </div>
-  <input type="text" class="form-control " disabled readonly value="<? echo $Especialistas->ID_Tip_Cred; ?>">
+  <input type="text" class="form-control " disabled readonly value="<?php echo $Especialistas->ID_Tip_Cred; ?>">
     </div>
     </div>
     
@@ -35,7 +35,7 @@ while ($r=$query->fetch_object()){
   
     <span class="input-group-text" id="Tarjeta"><i class="fas fa-file-signature"></i></span>
   </div>
-  <input type="text" class="form-control "  name="ActCrediNom" id="actcredinom" value="<? echo $Especialistas->Nombre_Tip; ?>" aria-describedby="basic-addon1" maxlength="60">            
+  <input type="text" class="form-control "  name="ActCrediNom" id="actcredinom" value="<?php echo $Especialistas->Nombre_Tip; ?>" aria-describedby="basic-addon1" maxlength="60">            
 </div></div></div>
 
 <div class="form-group">
@@ -45,7 +45,7 @@ while ($r=$query->fetch_object()){
   
     <span class="input-group-text" id="Tarjeta"><i class="fas fa-file-signature"></i></span>
   </div>
-  <input type="text" class="form-control "  name="ActCosto" id="actcosto" value="<? echo $Especialistas->Costo; ?>" aria-describedby="basic-addon1" maxlength="60">            
+  <input type="text" class="form-control "  name="ActCosto" id="actcosto" value="<?php echo $Especialistas->Costo; ?>" aria-describedby="basic-addon1" maxlength="60">            
 </div></div></div>
 
 <div class="row">
@@ -57,7 +57,7 @@ while ($r=$query->fetch_object()){
   <select name="Vigencia" class="form-control" id="vigencia" onchange="TipoVigencia();">
                  
                     
-                   <option  value="<? echo $Especialistas->CodigoEstatus; ?>"><? echo $Especialistas->Estatus; ?></option>		
+                   <option  value="<?php echo $Especialistas->CodigoEstatus; ?>"><?php echo $Especialistas->Estatus; ?></option>		
               <option  value="background-color: #2BBB1D !important;">Vigente</option>		
               <option  value="background-color: #FE0000 !important;">Descontinuado</option>						  	
               				  				  
@@ -77,7 +77,7 @@ while ($r=$query->fetch_object()){
   <tbody>
     <tr>
 <td>
-<button id="VigenciaBD" class="btn btn-default btn-sm" style=<? echo $Especialistas->CodigoEstatus; ?>><? echo $Especialistas->Estatus; ?></button> 
+<button id="VigenciaBD" class="btn btn-default btn-sm" style=<?php echo $Especialistas->CodigoEstatus; ?>><?php echo $Especialistas->Estatus; ?></button> 
      <button id="SiVigente" class="divOculto btn btn-default btn-sm" style="background-color: #2BBB1D !important;">Vigente</button> 
       <button id="NoVigente" class="divOculto btn btn-default btn-sm" style="background-color: #FE0000 !important;">Descontinuado</button>
     
@@ -90,9 +90,9 @@ while ($r=$query->fetch_object()){
   </div></div>
     </div>
     
-<input type="text" class="form-control " hidden  readonly name="Usuario" id="usuario"  readonly value="<?echo $row['Nombre_Apellidos']?>">
-<input type="text" class="form-control "  hidden  readonly id="sistema" name="Sistema" readonly value="POS <?echo $row['Nombre_rol']?>">
-<input type="text" class="form-control "  hidden id="empresa" name="Empresa" readonly value="<?echo $row['ID_H_O_D']?>">
+<input type="text" class="form-control " hidden  readonly name="Usuario" id="usuario"  readonly value="<?php echo $row['Nombre_Apellidos']?>">
+<input type="text" class="form-control "  hidden  readonly id="sistema" name="Sistema" readonly value="POS <?php echo $row['Nombre_rol']?>">
+<input type="text" class="form-control "  hidden id="empresa" name="Empresa" readonly value="<?php echo $row['ID_H_O_D']?>">
     <input type="text"  class="form-control " hidden readonly name="VigenciaEst" id="vigenciaest">
 <input type="hidden" name="id" id="id" value="<?php echo $Especialistas->ID_Tip_Cred; ?>">
 <button type="submit"  id="submit"  class="btn btn-info">Aplicar cambios <i class="fas fa-check"></i></button>
@@ -100,9 +100,9 @@ while ($r=$query->fetch_object()){
 </form>
 <script src="js/ActualizaTipoCreditos.js"></script>
 
-<? else:?>
+<?php else:?>
   <p class="alert alert-danger">404 No se encuentra</p>
-<? endif;?>
+<?php endif;?>
 <script type="text/javascript">
   
 
