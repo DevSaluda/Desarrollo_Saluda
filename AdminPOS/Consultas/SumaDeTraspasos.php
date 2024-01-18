0a42162f3122032dc469a5a9fdbed29f75232c3d
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-18 15:49:10 -0600

    Update SumaDeTraspasos.php

--- AdminPOS/Consultas/SumaDeTraspasos.php
+++ AdminPOS/Consultas/SumaDeTraspasos.php
@@ -1,12 +1,15 @@
 <?php
-    include_once("db_connection.php");
+    include_once("db_connect.php");
     include "Consultas.php";
-
-    $sql ="SELECT * FROM Traspasos_generadosV2 ORDER BY ID_Traspaso_Generado DESC LIMIT 1";
+    $sql ="SELECT * FROM Traspasos_generadosV2 order by ID_Traspaso_Generado desc limit 1";
     $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-    $Ticketss = mysqli_fetch_assoc($resultset);
+$Ticketss = mysqli_fetch_assoc($resultset);
+ 
 
-    $monto1 = intval($Ticketss['Folio_Traspaso']);
-    $monto2 = 000000000001; 
-    $totalmonto = $monto1 + $monto2;
+$monto1 = $Ticketss['Folio_Traspaso'];
+$monto2 = 000000000001; 
+$totalmonto =  $monto1 + $monto2; 
+ 
+    
+  
 ?>
