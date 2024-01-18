b3e54a4d7f3827f98c84c992318e28fc7599d955
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-18 16:38:30 -0600

    ajustes

--- AdminPOS/AjusteDeInventarios.php
+++ AdminPOS/AjusteDeInventarios.php
@@ -1,7 +1,6 @@
 <?php
 include "Consultas/Consultas.php";
-include "Consultas/Sesion.php";
-include "Consultas/AnalisisIndex.php";
+
 
 ?>
 <!DOCTYPE html>
@@ -11,7 +10,7 @@ include "Consultas/AnalisisIndex.php";
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>Ajustes de inventarios <?echo $row['ID_H_O_D']?> </title>
+  <title>Ajustes de inventarios <?php echo $row['ID_H_O_D']?> </title>
 
 <?include "Header.php"?>
  <style>
@@ -28,13 +27,13 @@ table td {
 
     </style>
 </head>
-<?include_once ("Menu.php")?>
+<?php include_once ("Menu.php")?>
 
 
 
   <div class="card text-center">
   <div class="card-header" style="background-color:#2b73bb !important;color: white;">
-  Stock por sucursales de <?echo $row['ID_H_O_D']?> al <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>  
+  Stock por sucursales de <?php echo $row['ID_H_O_D']?> al <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>  
   </div>
  
   <div >
@@ -64,7 +63,7 @@ table td {
   <!-- Control Sidebar -->
  
   <!-- Main Footer -->
-<?
+<?php
   include ("Modales/FiltroSucursales.php");
   include ("Modales/FiltroServicios.php");
   include ("Modales/Vacios.php");
@@ -104,7 +103,7 @@ table td {
 
 </body>
 </html>
-<?
+<?php
 
 function fechaCastellano ($fecha) {
   $fecha = substr($fecha, 0, 10);
