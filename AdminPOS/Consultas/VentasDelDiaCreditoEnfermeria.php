8961fb4b4f34e4ff3e42452b020383bdd1d85714
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-18 09:54:56 -0600

    creditosEnfermeria

--- AdminPOS/Consultas/VentasDelDiaCreditoEnfermeria.php
+++ AdminPOS/Consultas/VentasDelDiaCreditoEnfermeria.php
@@ -8,7 +8,7 @@ tabla = $('#ProductosEnfermeria').DataTable({
  "stateSave":true,
  "bAutoWidth": true,
  "order": [[ 0, "desc" ]],
- "sAjaxSource": "https://controlfarmacia.com/AdminPOS/Consultas/ArrayDesgloseVentasCreditoEnfermeria.php",
+ "sAjaxSource": "https://saludapos.com/AdminPOS/Consultas/ArrayDesgloseVentasCreditoEnfermeria.php",
  "aoColumns": [
        { mData: 'Cod_Barra' },
        { mData: 'Nombre_Prod' },
