1e3909fc5245e7bc768a9e7e29f83c2ec988d353
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-18 15:39:35 -0600

    Update SumaDeTraspasos.php

--- AdminPOS/Consultas/SumaDeTraspasos.php
+++ AdminPOS/Consultas/SumaDeTraspasos.php
@@ -1,7 +1,7 @@
 <?php
     include_once("../db_connect.php");
     include "../Consultas.php";
-include "../Sesion.php";
+
     $sql ="SELECT * FROM Traspasos_generadosV2 order by ID_Traspaso_Generado desc limit 1";
     $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
 $Ticketss = mysqli_fetch_assoc($resultset);
