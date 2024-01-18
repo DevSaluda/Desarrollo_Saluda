368afbd6bbba7f4f8e4c6bc2420d0403e18c6c4b
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-18 10:03:54 -0600

    Update CreditoFarmaceuticoGeneral.php

--- AdminPOS/CreditoFarmaceuticoGeneral.php
+++ AdminPOS/CreditoFarmaceuticoGeneral.php
@@ -23,12 +23,7 @@ include "Consultas/Consultas.php";
     </style>
 </head>
 <?php include_once ("Menu.php")?>
-<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Ventas del</a>
-  </li>
-  
-</ul>
+
 
 <div class="tab-content" id="pills-tabContent">
 <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
