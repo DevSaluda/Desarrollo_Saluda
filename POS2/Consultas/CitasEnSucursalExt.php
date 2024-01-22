817177ebd0b21a6c06e83b9017c0e9c0039a3b82
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-22 14:49:40 -0600

    Update CitasEnSucursalExt.php

--- POS2/Consultas/CitasEnSucursalExt.php
+++ POS2/Consultas/CitasEnSucursalExt.php
@@ -33,7 +33,7 @@ $meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"
 }
 include("db_connection.php");
 include "Consultas.php";
-include "Sesion.php";
+
 
 $user_id=null;
 $sql1="SELECT AgendaCitas_EspecialistasExt.ID_Agenda_Especialista,AgendaCitas_EspecialistasExt.Fk_Especialidad,AgendaCitas_EspecialistasExt.Fk_Especialista,
