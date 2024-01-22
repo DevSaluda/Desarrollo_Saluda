449f47347308c017459da5c7270cd6c2788cd698
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-22 10:51:14 -0600

    turno

--- POS2/Menu.php
+++ POS2/Menu.php
@@ -1,23 +1,5 @@
 <?php include "Consultas/ContadorParaNotificacion.php";
-
-
-$sql ="SELECT Cajas_POS.ID_Caja,Cajas_POS.Cantidad_Fondo,Cajas_POS.Empleado,Cajas_POS.Sucursal,Cajas_POS.Estatus,Cajas_POS.CodigoEstatus,Cajas_POS.Turno,Cajas_POS.Asignacion,
-Cajas_POS.Fecha_Apertura,Cajas_POS.Valor_Total_Caja,Cajas_POS.ID_H_O_D, SucursalesCorre.ID_SucursalC, SucursalesCorre.Nombre_Sucursal 
-FROM Cajas_POS,SucursalesCorre where Cajas_POS.Sucursal = SucursalesCorre.ID_SucursalC AND Cajas_POS.Fecha_Apertura = CURRENT_DATE()
- AND Cajas_POS.Sucursal='".$row['Fk_Sucursal']."' AND Cajas_POS.Asignacion = 1 AND
-Cajas_POS.Estatus='Abierta'  AND Cajas_POS.Empleado='".$row['Nombre_Apellidos']."'  AND Cajas_POS.ID_H_O_D='".$row['ID_H_O_D']."'"; 
-$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-$ValorCaja = "";
-// Check if the query returned any results
-if ($resultset && mysqli_num_rows($resultset) > 0) {
-    $ValorCaja = mysqli_fetch_assoc($resultset);
-
-    // Now you can access array elements safely
-    // For example, $ValorCaja['ID_Caja'], $ValorCaja['Cantidad_Fondo'], etc.
-} else {
-    // Handle the case where no results were found
-    echo "Por el momento sin turno.";
-}
+include "Consultas/ConsultaCaja.php";
 
 
 
@@ -190,7 +172,7 @@ if ($TraspasosPendientes['traspasopendiente'] != 0) {
         if ($ValorCaja !== null && isset($ValorCaja['Turno'])) {
             echo 'Turno actual: <strong>' . $ValorCaja['Turno'] . '</strong>';
         } else {
-            echo 'Turno no disponible'; // o cualquier otro mensaje que desees mostrar
+            echo 'Sin turno por el momento'; // o cualquier otro mensaje que desees mostrar
         }
         ?>
     </small>
