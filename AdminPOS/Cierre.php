e3213ea09058941863f2bf355b22a66ced1e731d
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-23 08:43:00 -0600

    xd

--- AdminPOS/Cierre.php
+++ AdminPOS/Cierre.php
@@ -4,7 +4,7 @@ session_start();
 setcookie ("mostrarModal", "", time() - 3600);
 session_unset();
 session_destroy();
-setcookie('csrf_token', '', time() - 3600, '/');
+
 
 header("Location:https://saludapos.com/App/Secure/POS");
 ?>
