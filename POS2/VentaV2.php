7a5a3d19d5c8546c1c2234b52d11b90afa0264d1
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-22 21:20:29 -0600

    Update VentaV2.php

--- POS2/VentaV2.php
+++ POS2/VentaV2.php
@@ -1,7 +1,5 @@
-
 <?php
 include "Consultas/Consultas.php";
-include "Consultas/Sesion.php";
 include "Consultas/ConsultaCaja.php";
 include "Consultas/SumadeFolioTickets.php";
 include ("db_connection.php");
