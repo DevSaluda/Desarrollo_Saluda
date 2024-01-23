910d610c428737e03ae93e70dbc6945acb458287
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-23 11:51:19 -0600

    Update db_connection.php

--- POS2/Consultas/db_connection.php
+++ POS2/Consultas/db_connection.php
@@ -5,10 +5,19 @@ $servername = "localhost";
 $username = "u155356178_SaludDevCenter";
 $password = "uE;bAISz;*6c|I4PvEnfSys324\Zavp2zJ:9TLx{]L&QMcmhAdmSCDBSN3iH4UV3D24WMF@2024myV>";
 $dbname = "u155356178_saludapos";
+
+// Crear conexión
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("No podemos conectar a la base de datos: " . mysqli_connect_error());
+
+// Verificar la conexión
 if (mysqli_connect_errno()) {
-    printf("algo salio mal, no podemos conectarnos a la base de datos %s\n", mysqli_connect_error());
+    printf("Algo salió mal, no podemos conectarnos a la base de datos: %s\n", mysqli_connect_error());
     exit();
 }
+
+// Establecer la zona horaria
+mysqli_query($conn, "SET time_zone = '-6:00'");  // Ajusta esto según tu zona horaria
+
+// Resto de tu código aquí...
 
 ?>
