6fe1b7bddf25f66568886879962b34bcb16b2d73
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-22 20:04:54 -0600

    Update SumadeFolioTickets.php

--- POS2/Consultas/SumadeFolioTickets.php
+++ POS2/Consultas/SumadeFolioTickets.php
@@ -1,6 +1,6 @@
 <?php
-include_once("../db_connection.php");
-include "../Consultas.php";
+include_once("db_connection.php");
+include "Consultas.php";
 
 
 $sql = "SELECT * FROM Ventas_POS  WHERE Fk_sucursal='" . $row['Fk_Sucursal'] . "' ORDER BY Venta_POS_ID DESC LIMIT 1";
