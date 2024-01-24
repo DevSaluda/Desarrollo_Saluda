c1512e3c05385dcf71bfb75a6d9e7de39ba4fb30
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-24 16:47:04 -0600

    Update VentasAlmomento.php

--- POS2/Consultas/VentasAlmomento.php
+++ POS2/Consultas/VentasAlmomento.php
@@ -9,7 +9,8 @@ if (isset($_POST["pro_FKID"]) && is_array($_POST["pro_FKID"])) {
 
     $query = "INSERT INTO Ventas_POS (ID_Prod_POS, Identificador_tipo, Turno, FolioSucursal, Folio_Ticket, Clave_adicional, Cod_Barra, Nombre_Prod, Cantidad_Venta,
         Fk_sucursal, Total_Venta, Importe, Total_VentaG, DescuentoAplicado, FormaDePago, CantidadPago, Cambio, Cliente, Fecha_venta, Fk_Caja, Lote, Sistema, AgregadoPor, ID_H_O_D, FolioSignoVital, TicketAnterior) VALUES ";
-    $queryValues = [];
+
+    $queryValues = array();
 
     for ($i = 0; $i < $contador; $i++) {
         if (
@@ -42,21 +43,57 @@ if (isset($_POST["pro_FKID"]) && is_array($_POST["pro_FKID"])) {
         ) {
             $ProContador++;
 
-            $queryValues[] = "('" . $_POST["pro_FKID"][$i] . "','" . $_POST["IdentificadorTip"][$i] . "','" . $_POST["TurnoCaja"][$i] . "','" . $_POST["TicketSucursalName"] . "','" . $_POST["TicketVal"] . "','" . $_POST["pro_clavad"][$i] . "',
-                    '" . $_POST["CodBarras"][$i] . "','" . $_POST["NombreProd"][$i] . "','" . $_POST["CantidadTotal"][$i] . "','" . $_POST["Sucursaleventas"][$i] . "','" . $_POST["pro_cantidad"][$i] . "',
-                    '" . $_POST["ImporteT"][$i] . "','" . $_POST["TotalVentas"][$i] . "','" . $_POST["DescuentoAplicado"][$i] . "','" . $_POST["FormaPago"][$i] . "','" . $_POST["PagoReal"][$i] . "','" . $_POST["Cambio"][$i] . "','" . $_POST["cliente"][$i] . "','" . $_POST["Fecha"][$i] . "','" . $_POST["CajaSucursal"][$i] . "','" . $_POST["pro_lote"][$i] . "','" . $_POST["Sistema"][$i] . "','" . $_POST["Vendedor"][$i] . "','" . $_POST["Empresa"][$i] . "','" . $_POST["foliosv"][$i] . "','" . $_POST["ticketant"][$i] . "')";
+            // Escapar y agregar los valores al array $queryValues
+            $values = array(
+                mysqli_real_escape_string($conn, $_POST["pro_FKID"][$i]),
+                mysqli_real_escape_string($conn, $_POST["IdentificadorTip"][$i]),
+                mysqli_real_escape_string($conn, $_POST["TurnoCaja"][$i]),
+                mysqli_real_escape_string($conn, $_POST["TicketSucursalName"]),
+                mysqli_real_escape_string($conn, $_POST["TicketVal"]),
+                mysqli_real_escape_string($conn, $_POST["pro_clavad"][$i]),
+                mysqli_real_escape_string($conn, $_POST["CodBarras"][$i]),
+                mysqli_real_escape_string($conn, $_POST["NombreProd"][$i]),
+                mysqli_real_escape_string($conn, $_POST["CantidadTotal"][$i]),
+                mysqli_real_escape_string($conn, $_POST["Sucursaleventas"][$i]),
+                mysqli_real_escape_string($conn, $_POST["pro_cantidad"][$i]),
+                mysqli_real_escape_string($conn, $_POST["ImporteT"][$i]),
+                mysqli_real_escape_string($conn, $_POST["TotalVentas"][$i]),
+                mysqli_real_escape_string($conn, $_POST["DescuentoAplicado"][$i]),
+                mysqli_real_escape_string($conn, $_POST["FormaPago"][$i]),
+                mysqli_real_escape_string($conn, $_POST["PagoReal"][$i]),
+                mysqli_real_escape_string($conn, $_POST["Cambio"][$i]),
+                mysqli_real_escape_string($conn, $_POST["cliente"][$i]),
+                mysqli_real_escape_string($conn, $_POST["Fecha"][$i]),
+                mysqli_real_escape_string($conn, $_POST["CajaSucursal"][$i]),
+                mysqli_real_escape_string($conn, $_POST["pro_lote"][$i]),
+                mysqli_real_escape_string($conn, $_POST["Sistema"][$i]),
+                mysqli_real_escape_string($conn, $_POST["Vendedor"][$i]),
+                mysqli_real_escape_string($conn, $_POST["Empresa"][$i]),
+                mysqli_real_escape_string($conn, $_POST["foliosv"][$i]),
+                mysqli_real_escape_string($conn, $_POST["ticketant"][$i])
+            );
+
+            $queryValues[] = "('" . implode("','", $values) . "')";
         }
     }
 
+    // Combinar la consulta y los valores
     $sql = $query . implode(",", $queryValues);
 
     if ($ProContador != 0) {
+        // Ejecutar la consulta
         $resultadocon = mysqli_query($conn, $sql);
 
-        if (!empty($resultadocon)) {
+        if ($resultadocon) {
             $resultado = " <br><ul class='list-group' style='margin-top:15px;'>
                <li class='list-group-item'>Registro(s) Agregado Correctamente.</li></ul>";
+        } else {
+            // Manejar errores de la consulta
+            $resultado = "Error al ejecutar la consulta: " . mysqli_error($conn);
         }
+    } else {
+        // Mensaje si no hay datos para procesar
+        $resultado = "No hay datos para procesar.";
     }
 } else {
     // La clave del array no está definida en $_POST o no es un array
