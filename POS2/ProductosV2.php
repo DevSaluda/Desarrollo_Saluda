25e9026b62ce9e4296daa06b504761e4cb73d602
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-24 08:30:11 -0600

    Update ProductosV2.php

--- POS2/ProductosV2.php
+++ POS2/ProductosV2.php
@@ -29,15 +29,7 @@ include "Consultas/Consultas.php";
   <div id="loading-text" style="color: white; margin-top: 10px; font-size: 18px;"></div>
 </div>
 <?php include_once ("Menu.php")?>
-<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Stock</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Productos prontos a caducar</a>
-  </li>
-  
-</ul>
+
 
 <div class="tab-content" id="pills-tabContent">
 <div class="tab-pane fade show active" id="pills-profile" role="tabpanel" aria-labelledby="pills-home-tab">
