a80fbaf3c3e17da6fc262f9cc1f3218073cd0ffa
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-25 10:20:55 -0600

    Update RealizaVentas.js

--- POS2/js/RealizaVentas.js
+++ POS2/js/RealizaVentas.js
@@ -68,15 +68,15 @@ $('document').ready(function ($) {
              
                 var FormBuscarSiniestro = $('#VentasAlmomento');
               
-      //           $("#VentasAlmomento").removeAttr("id");
-      //           FormBuscarSiniestro.attr("id","Pruebas");
-      //     FormBuscarSiniestro.attr("action","http://localhost:8080/ticket/TicketVenta.php/");
-      // /*     FormBuscarSiniestro.attr("action","http://localhost/ticket/Pruebasarreglos.php/"); */
-      //     document.getElementById('Pruebas').submit()
-      //     $("#submit_registro").html("Realizar venta <i class='fas fa-save'></i>></span>");
-      //     setTimeout(function(){ 
-      //       location.reload();
-      //   }, 2000); // abrir
+                $("#VentasAlmomento").removeAttr("id");
+                FormBuscarSiniestro.attr("id","Pruebas");
+          FormBuscarSiniestro.attr("action","http://localhost:8080/ticket/TicketVenta.php/");
+      /*     FormBuscarSiniestro.attr("action","http://localhost/ticket/Pruebasarreglos.php/"); */
+          document.getElementById('Pruebas').submit()
+          $("#submit_registro").html("Realizar venta <i class='fas fa-save'></i>></span>");
+          setTimeout(function(){ 
+            location.reload();
+        }, 2000); // abrir
        
                      },
                      error: function(){
