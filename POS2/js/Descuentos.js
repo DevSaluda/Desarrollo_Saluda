a1c854c58f006e7b008a7963127e61ec66bbcd14
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-25 12:44:43 -0600

    Update Descuentos.js

--- POS2/js/Descuentos.js
+++ POS2/js/Descuentos.js
@@ -1,19 +1,26 @@
+// Función de descuento genérica
+function aplicarDescuento(identificador) {
+  const campoDescuento = document.getElementById(`cantidadadescontar${identificador}`);
+  const campoCostoVenta = document.getElementById(`costoventa${identificador}`);
+  const campoDescuentoReal = document.getElementById(`descuento${identificador}`);
 
-    // Función de descuento genérica
-    function aplicarDescuento(identificador) {
-        const campoDescuento = document.getElementById(`cantidadadescontar${identificador}`);
-        const campoCostoVenta = document.getElementById(`costoventa${identificador}`);
-        const campoDescuentoReal = document.getElementById(`descuento${identificador}`);
+  const cantidadDescuento = campoDescuento.value;
+  const r = parseFloat(campoCostoVenta.value);  // Asegúrate de obtener el valor correcto del campoCostoVenta y convertirlo a número
+  const totalDescuento = (r * cantidadDescuento) / 100;
+  const valorConDescuento = r - totalDescuento;
 
-        const cantidadDescuento = campoDescuento.value;
-        const totalDescuento = (r * cantidadDescuento) / 100;
-        const valorConDescuento = r - totalDescuento;
+  campoCostoVenta.value = valorConDescuento;
+  campoDescuentoReal.value = cantidadDescuento;
+}
 
-        campoCostoVenta.value = valorConDescuento;
-        campoDescuentoReal.value = cantidadDescuento;
-    }
+// Obtener todos los botones que abren el modal
+const buttons = document.querySelectorAll('[data-toggle="modal"][data-target="#DescuentoDetalles"]');
 
-    // Asignar eventos a los botones de aplicar descuento
-    for (let i = 1; i <= 10; i++) {
-        document.getElementById(`btnAplicarDescuento${i}`).addEventListener('click', () => aplicarDescuento(i));
-    }
+// Asignar eventos a los botones de aplicar descuento
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+      // Obtener el identificador de la fila desde el botón
+      const identificador = button.getAttribute('data-identificador');
+      aplicarDescuento(identificador);
+  });
+});
