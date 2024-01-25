c0cf30cd51e8d40bff26e8248a4ac7b8c6101b4d
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-25 11:27:29 -0600

    xd

--- POS2/VistaVentas.php
+++ POS2/VistaVentas.php
@@ -110,7 +110,27 @@ $resultado_en_mayusculas = strtoupper($resultado_concatenado);
    <!-- SEGUNDO PRODUCTP -->
 
 <script>
+document.getElementById("contenedor-dinamico").addEventListener("focusout", function(event) {
+  // Verifica si el evento se originó desde un campo con la clase "cantidadventa"
+  if (event.target.classList.contains("cantidadventa")) {
+    multiplicar(event.target);
+  }
+});
 
+function multiplicar(inputCantidad) {
+  // Obtiene los valores de los campos relacionados al input de cantidad
+  var m1 = parseFloat(inputCantidad.closest(".fila").querySelector(".precioprod").value);
+  var m2 = parseFloat(inputCantidad.value);
+
+ 
+  var r = m1 * m2;
+
+  // Actualiza el campo de resultado dentro de la misma fila
+  inputCantidad.closest(".fila").querySelector(".costoventa").value = r;
+
+  // Llama a la función sumar para realizar cualquier otra operación necesaria
+  sumar();
+}
  function sumar()
   {
     var $total = document.getElementById('totalventa2');
