b3c8aa56cd6b7e06f6d1627180b3e796285814e7
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-25 12:49:01 -0600

    Update Descuentos.js

--- POS2/js/Descuentos.js
+++ POS2/js/Descuentos.js
@@ -4,10 +4,20 @@ function aplicarDescuento(identificador) {
   const campoCostoVenta = document.getElementById(`costoventa${identificador}`);
   const campoDescuentoReal = document.getElementById(`descuento${identificador}`);
 
+  console.log(`Identificador: ${identificador}`);
+  console.log(`Campo Descuento: ${campoDescuento.value}`);
+  console.log(`Campo Costo Venta: ${campoCostoVenta.value}`);
+
   const cantidadDescuento = campoDescuento.value;
-  const r = parseFloat(campoCostoVenta.value);  // Asegúrate de obtener el valor correcto del campoCostoVenta y convertirlo a número
+  const r = parseFloat(campoCostoVenta.value);
+
+  console.log(`Valor de r: ${r}`);
+
   const totalDescuento = (r * cantidadDescuento) / 100;
   const valorConDescuento = r - totalDescuento;
+
+  console.log(`Total Descuento: ${totalDescuento}`);
+  console.log(`Valor con Descuento: ${valorConDescuento}`);
 
   campoCostoVenta.value = valorConDescuento;
   campoDescuentoReal.value = cantidadDescuento;
@@ -19,8 +29,9 @@ const buttons = document.querySelectorAll('[data-toggle="modal"][data-target="#D
 // Asignar eventos a los botones de aplicar descuento
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-      // Obtener el identificador de la fila desde el botón
-      const identificador = button.getAttribute('data-identificador');
-      aplicarDescuento(identificador);
+    // Obtener el identificador de la fila desde el botón
+    const identificador = button.getAttribute('data-identificador');
+    console.log(`Botón clicado - Identificador: ${identificador}`);
+    aplicarDescuento(identificador);
   });
 });
