29e13153e45478e589090b385826004a17dd3fae
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-25 16:00:28 -0600

    Update VistaVentas.php

--- POS2/VistaVentas.php
+++ POS2/VistaVentas.php
@@ -109,7 +109,31 @@ $resultado_en_mayusculas = strtoupper($resultado_concatenado);
 <input type="text"hidden class="form-control " name="TurnoCaja[]" readonly value="<?php echo $ValorCaja['Turno']?>" >
   
    <!-- SEGUNDO PRODUCTP -->
+   <script>
+  function multiplicar() {
+    // Iterar sobre todas las filas dinámicas
+    $('.row').each(function () {
+        // Obtener elementos relevantes en la fila actual
+        var cantidadInput = $(this).find('.cantidadventa');
+        var precioInput = $(this).find('.Precio');
+        var importeInput = $(this).find('.montoreal');
 
+        // Obtener valores como números
+        var cantidad = parseFloat(cantidadInput.val()) || 0;
+        var precio = parseFloat(precioInput.val()) || 0;
+
+        // Calcular el importe
+        var importe = cantidad * precio;
+
+        // Actualizar el campo de importe en la misma fila
+        importeInput.val(importe);
+    });
+
+    // Llamar a la función sumar si es necesario
+    sumar();
+}
+
+</script>
 <script>
 
  function sumar()
@@ -223,31 +247,7 @@ $(document).ready(function () {
     });
 });
 </script>
-<script>
-  function multiplicar() {
-    // Iterar sobre todas las filas dinámicas
-    $('.row').each(function () {
-        // Obtener elementos relevantes en la fila actual
-        var cantidadInput = $(this).find('.cantidadventa');
-        var precioInput = $(this).find('.Precio');
-        var importeInput = $(this).find('.montoreal');
 
-        // Obtener valores como números
-        var cantidad = parseFloat(cantidadInput.val()) || 0;
-        var precio = parseFloat(precioInput.val()) || 0;
-
-        // Calcular el importe
-        var importe = cantidad * precio;
-
-        // Actualizar el campo de importe en la misma fila
-        importeInput.val(importe);
-    });
-
-    // Llamar a la función sumar si es necesario
-    sumar();
-}
-
-</script>
 </div></div>
  
 <?php
