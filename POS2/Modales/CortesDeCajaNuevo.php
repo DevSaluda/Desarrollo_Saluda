5eff8acaf596ee20336abe6cf189867cfbba1ddc
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-26 11:43:17 -0600

    Update CortesDeCajaNuevo.php

--- POS2/Modales/CortesDeCajaNuevo.php
+++ POS2/Modales/CortesDeCajaNuevo.php
@@ -452,7 +452,7 @@ $query8888 = $conn->query($sql8888);
     <span class="input-group-text" id="Tarjeta"><i class="fas fa-file-signature"></i></span>
   </div>
           
-  <input type="number" class="form-control "  id="cantidadtotalventas" name="Cantidad" step="any" readonly value="<?php echo $Especialistas33->number_format($ventaTotalEfectivoRedondeado, 2); ?>" aria-describedby="basic-addon1" >  
+  <input type="number" class="form-control "  id="cantidadtotalventas" name="Cantidad" step="any" readonly value="<?php echo $Especialistas33->VentaTotaldeEfectivoPAraElcorte; ?>" aria-describedby="basic-addon1" >  
 </div></div>
 <div class="col">
     <label for="exampleFormControlInput1">Conteo de billetes<span class="text-danger">*</span></label>
