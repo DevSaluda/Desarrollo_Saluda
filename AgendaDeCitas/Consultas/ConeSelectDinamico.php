5445838c3ada78b9d1a69613fc783a1e3ecaf3a2
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-27 17:26:14 -0600

    Update ConeSelectDinamico.php

--- AgendaDeCitas/Consultas/ConeSelectDinamico.php
+++ AgendaDeCitas/Consultas/ConeSelectDinamico.php
@@ -1,3 +1,16 @@
 <?php
-$conn = new mysqli('localhost', 'u155356178_SaludDevCenter', 'uE;bAISz;*6c|I4PvEnfSys324\Zavp2zJ:9TLx{]L&QMcmhAdmSCDBSN3iH4UV3D24WMF@2024myV>', 'u155356178_saludapos') or die(mysqli_error());	
+$servername = 'localhost';
+$username = 'u155356178_SaludDevCenter';
+$password = 'uE;bAISz;*6c|I4PvEnfSys324\\Zavp2zJ:9TLx{]L&QMcmhAdmSCDBSN3iH4UV3D24WMF@2024myV>';
+$database = 'u155356178_saludapos';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+echo 'Connected successfully';
 ?>
