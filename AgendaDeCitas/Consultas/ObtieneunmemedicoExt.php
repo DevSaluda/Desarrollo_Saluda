92c2c027e2f373b33b279f92c8f53122eb7756c4
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-27 20:17:49 -0600

    Update ObtieneunmemedicoExt.php

--- AgendaDeCitas/Consultas/ObtieneunmemedicoExt.php
+++ AgendaDeCitas/Consultas/ObtieneunmemedicoExt.php
@@ -1,12 +1,30 @@
 <?php
-	include("ConeSelectDinamico.php");
-	$sucursal=intval($_REQUEST['especialidadext']);
-	$medicos = $conn->prepare("SELECT * FROM Personal_Medico_Express WHERE Estatus='Disponible' AND Especialidad_Express = '$sucursal'") or die(mysqli_error());
-		echo '<option value = "">Selecciona un medico </option>';
-	if($medicos->execute()){
-		$a_result = $medicos->get_result();
-	}
-		while($row = $a_result->fetch_array()){
-			echo '<option value = "'.$row['Medico_ID'].'">'.( $row['Nombre_Apellidos']).'</option>';
-		}
+include("ConeSelectDinamico.php");
+
+// Verificar si 'especialidadext' está definido en $_REQUEST
+if (isset($_REQUEST['especialidadext'])) {
+    $sucursal = intval($_REQUEST['especialidadext']);
+    
+    // Utilizar sentencia preparada para evitar SQL injection
+    $medicos = $conn->prepare("SELECT * FROM Personal_Medico_Express WHERE Estatus='Disponible' AND Especialidad_Express = ?") or die(mysqli_error());
+    $medicos->bind_param("i", $sucursal);
+
+    echo '<option value = "">Selecciona un medico </option>';
+
+    if ($medicos->execute()) {
+        $a_result = $medicos->get_result();
+
+        while ($row = $a_result->fetch_array()) {
+            echo '<option value = "' . $row['Medico_ID'] . '">' . $row['Nombre_Apellidos'] . '</option>';
+        }
+    } else {
+        echo "Error en la ejecución de la consulta.";
+    }
+
+    // Cerrar la conexión, si es necesario
+    $medicos->close();
+    $conn->close();
+} else {
+    echo "Parámetro 'especialidadext' no definido en la solicitud.";
+}
 ?>
