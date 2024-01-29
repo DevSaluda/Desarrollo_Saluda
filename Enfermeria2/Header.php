afbd3f74c73e5c7c81e4f825caab35649963d428
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-29 14:52:34 -0600

    header_enfermeros

--- Enfermeria2/Header.php
+++ Enfermeria2/Header.php
@@ -1,5 +1,5 @@
 <!-- Font Awesome Icons -->
-<script src="https://kit.fontawesome.com/f976a08a4e.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/ef7185cf29.js" crossorigin="anonymous"></script>
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Theme style -->
