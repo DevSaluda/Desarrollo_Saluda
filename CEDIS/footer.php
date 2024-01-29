2fb7c2dd35eb8caa111ae0e62a67dba539430ea9
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-29 12:17:29 -0600

    Update footer.php

--- CEDIS/footer.php
+++ CEDIS/footer.php
@@ -1,11 +1,11 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="https://somosgrupoe.com/">Somos Grupo E</a>.</strong>
+
     Derechos reservados.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0
+      <b>Version</b> 4.0
     </div>
   </footer>
-</div><?include ("Modales/Salidas.php");
+</div><?php include ("Modales/Salidas.php");
 ?>
 <script src="js/LogSalida.js"></script>
-<script src="https://controlfarmacia.com/CEDIS/js/Refresca.js"></script>
+<script src="https://saludapos.com/CEDIS/js/Refresca.js"></script>
