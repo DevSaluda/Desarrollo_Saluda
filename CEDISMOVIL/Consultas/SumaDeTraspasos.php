00be021dfc4fedcd4edc022383d671f793eb9153
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-29 09:22:35 -0600

    Update SumaDeTraspasos.php

--- CEDISMOVIL/Consultas/SumaDeTraspasos.php
+++ CEDISMOVIL/Consultas/SumaDeTraspasos.php
@@ -2,22 +2,22 @@
 include("db_connection.php");
 include "Consultas.php";
 
-    $sql = "SELECT * FROM Traspasos_generados ORDER BY ID_Traspaso_Generado DESC LIMIT 1";
-    $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
+$sql = "SELECT * FROM Traspasos_generados ORDER BY ID_Traspaso_Generado DESC LIMIT 1";
+$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
 
-    // Verificar si hay resultados
-    if ($resultset && mysqli_num_rows($resultset) > 0) {
-        $Ticketss = mysqli_fetch_assoc($resultset);
+// Verificar si hay resultados
+if ($resultset && mysqli_num_rows($resultset) > 0) {
+    $Ticketss = mysqli_fetch_assoc($resultset);
 
-        // Verificar si 'Folio_Traspaso' está definido y no es nulo
-        if (isset($Ticketss['Folio_Traspaso']) && $Ticketss['Folio_Traspaso'] !== null) {
-            $monto1 = intval($Ticketss['Folio_Traspaso']);
-            $monto2 = 000000000001; 
-            $totalmonto = $monto1 + $monto2;
-        } else {
-            echo "El valor de 'Folio_Traspaso' es nulo o no está definido.";
-        }
+    // Verificar si 'NumFactura' está definido y no es nulo
+    if (isset($Ticketss['NumFactura']) && $Ticketss['NumFactura'] !== null) {
+        $monto1 = intval($Ticketss['NumFactura']);
+        $monto2 = 000000000001; 
+        $totalmonto = $monto1 + $monto2;
     } else {
-        echo "No se encontraron resultados en la consulta.";
+        echo "El valor de 'NumFactura' es nulo o no está definido.";
     }
+} else {
+    echo "No se encontraron resultados en la consulta.";
+}
 ?>
