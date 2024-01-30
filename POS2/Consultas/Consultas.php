040855d766e49f7eed36ce29b9708abb31769baa
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-30 16:22:37 -0600

    Update Consultas.php

--- POS2/Consultas/Consultas.php
+++ POS2/Consultas/Consultas.php
@@ -19,10 +19,23 @@ else
 {header("Location:Stop");
 }
 
-$fkSucursal = 'valor_Fk_Sucursal';
-$idHOD = 'valor_ID_H_O_D';
-
-$ventasData = obtenerVentasPorSucursalYIDHOD($fkSucursal, $idHOD);
+$hora = date('G'); if (($hora >= 0) AND ($hora < 6)) 
+  { 
+    
+    $mensaje = "Hola, que tengas una excelente madrugada."; 
+  } 
+  else if (($hora >= 6) AND ($hora < 12)) 
+  { 
+    $mensaje = "Buenos días"; 
+  } 
+  else if (($hora >= 12) AND ($hora < 18)) 
+  { 
+    $mensaje = "Buenas tardes"; 
+  } 
+  else
+  { 
+  $mensaje = "Buenas noches"; 
+  } 
 
    ?>
 
