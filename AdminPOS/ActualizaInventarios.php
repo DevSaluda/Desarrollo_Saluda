9bd1c3d4d68ff449fc9db9949fe82376837baf0a
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-30 09:57:27 -0600

    Update ActualizaInventarios.php

--- AdminPOS/ActualizaInventarios.php
+++ AdminPOS/ActualizaInventarios.php
@@ -215,7 +215,9 @@ function actualizarlote() {
      
     <input type="text" class="form-control"  hidden name="AgregaProductosBy" id="agrega" readonly value=" <?php echo $row['Nombre_Apellidos']?>">
     <input type="text" class="form-control"  hidden name="SistemaProductos" id="sistema" readonly value=" POS <?php echo $row['Nombre_rol']?>">
-    
+    <input type="text" name="Loteee" value="">
+    <input type="text" name="fechacad" value="">
+    <input type="text" name="fechaingreso" value="<?php echo date('Y-m-d'); ?>" readonly>
       
 
        <button type="submit"   id="EnviarDatossss" value="Guardar" class="btn btn-success">Guardar <i class="fas fa-save"></i></button>
