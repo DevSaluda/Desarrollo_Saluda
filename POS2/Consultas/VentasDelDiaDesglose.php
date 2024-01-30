59015ef7b83e9ea48c3dbd7363bb0e50107f45f7
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-30 00:52:17 -0600

    ajustesenvistaventas

--- POS2/Consultas/VentasDelDiaDesglose.php
+++ POS2/Consultas/VentasDelDiaDesglose.php
@@ -90,7 +90,7 @@ $query = $conn->query($sql1);
     <td><?php echo $Usuarios["Cantidad_Venta"]; ?></td>
     <td><?php echo $Usuarios["Total_Venta"]; ?></td>
     <td><?php echo $Usuarios["Importe"]; ?></td>
-    <td><?php echo $Usuarios["DescuentoAplicado"]; ?> %</td>
+    <td><?php echo isset($Usuarios["DescuentoAplicado"]) ? $Usuarios["DescuentoAplicado"] . " %" : ""; ?></td>
     <td><?php echo $Usuarios["FormaDePago"]; ?></td>
     <td><?php echo $Usuarios["Cliente"]; ?></td>
     <td><?php echo $Usuarios["FolioSignoVital"]; ?></td>
