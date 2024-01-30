e2c42ec49b938095514b06faf7e9c5fd235dca75
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-30 15:17:42 -0600

    Update VentasDelDiaDesgloseEnfermeria.php

--- POS2/Consultas/VentasDelDiaDesgloseEnfermeria.php
+++ POS2/Consultas/VentasDelDiaDesgloseEnfermeria.php
@@ -122,7 +122,7 @@ $query = $conn->query($sql1);
     <td><?php echo $Usuarios["FormaDePago"]; ?></td>
     <td><?php echo $Usuarios["Nom_Serv"]; ?></td>
     <td><?php echo fechaCastellano($Usuarios["Fecha_venta"]); ?> <br>
-      <?php echo date("g:i a",strtotime($Usuarios["AgregadoElAdjusted"])); ?>
+      <?php echo $Usuarios["AgregadoElAdjusted"]?>
     </td>
     <td><?php echo $Usuarios["AgregadoPor"]; ?></button></td>
   
