44899d4b71543670bd287eceb5c211173aa70ab0
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-30 16:00:58 -0600

    Update ConsultaVentas.php

--- POS2/ConsultaVentas.php
+++ POS2/ConsultaVentas.php
@@ -20,6 +20,10 @@ include "Consultas/Consultas.php";
 
     </style>
 </head>
+<div id="loading-overlay">
+  <div class="loader"></div>
+  <div id="loading-text" style="color: white; margin-top: 10px; font-size: 18px;"></div>
+</div>
 <?php include_once ("Menu.php")?>
 
 
