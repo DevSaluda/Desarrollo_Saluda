d7957e165ae28324114fcf539a6f6b8ac1a24eee
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 16:35:45 -0600

    Update index.php

--- POS2/index.php
+++ POS2/index.php
@@ -235,6 +235,123 @@ $(document).ready(function()
   $("#Ingreso").modal("show");
 });
 </script>
+<style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+      background-color: #f0f0f0;
+    }
+
+    .modal {
+      display: none;
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      padding: 20px;
+      max-width: 400px; /* Ajusta el ancho máximo según tus preferencias */
+      width: 100%;
+      background-color: #fff;
+      border-radius: 8px;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+      text-align: left;
+      overflow: hidden; /* Evita que el modal sobresalga en pantallas pequeñas */
+    }
+
+    .modal img {
+      width: 100%;
+      max-width: 300px;
+      border-radius: 8px;
+      margin-top: 10px;
+    }
+
+    .overlay {
+      display: none;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+      z-index: 1;
+    }
+
+    .modal-content {
+      margin-bottom: 10px;
+    }
+
+    .modal ul {
+      list-style-type: none;
+      padding: 0;
+    }
+
+    .modal ul li {
+      margin-bottom: 10px;
+    }
+
+    .modal p {
+      margin-bottom: 10px;
+    }
+
+    .modal button {
+      background-color: #25D366;
+      color: #fff;
+      padding: 10px 15px;
+      text-decoration: none;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  </style>
+  <div class="modal" id="modal">
+  <div class="modal-content">
+    <p>Lamentamos las molestias ocasionadas por el mantenimiento que sufrió el sistema. Agradecemos su paciencia y apoyo.</p>
+
+    <ul>
+      <li>La agenda de citas ya funciona y pueden agendar sin problemas.</li>
+      <li>La recepción de traspasos opera con normalidad.</li>
+      <li>La apertura y cobro de abonos dentales para las sucursales que lo manejan estarán listas en el transcurso del día.</li>
+      <li>Estamos trabajando en el tema del cierre de sesión por inactividad.</li>
+    </ul>
+
+    <p>Gracias nuevamente por su paciencia y apoyo.</p>
+  </div>
+
+  <!-- Gif de Cheems -->
+  <img src="https://c.tenor.com/YEqsyuOsPVwAAAAd/tenor.gif" alt="Cheems">
+
+  
+</div>
+
+<script>
+  // Función para abrir la modal automáticamente al cargar la página
+  window.onload = function() {
+    openModal();
+  };
+
+  function openModal() {
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+  }
+
+  function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+  }
+
+  // Cerrar modal al hacer clic fuera de él
+  window.onclick = function (event) {
+    var modal = document.getElementById('modal');
+    if (event.target === modal) {
+      closeModal();
+    }
+  };
+</script>
 <?php endif; ?> 
 
 <script>
