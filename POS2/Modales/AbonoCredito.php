6ff260e494272188f015eaec0065300d5bdb08d7
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 15:00:58 -0600

    Update AbonoCredito.php

--- POS2/Modales/AbonoCredito.php
+++ POS2/Modales/AbonoCredito.php
@@ -30,13 +30,13 @@ while ($r=$query->fetch_object()){
 
       <input type="number" class="form-control " name="NumeroTicket" value="<?php echo $totalmonto;?>"readonly  >
       <input type="text" class="form-control "  name="FolioCredito"  readonly value="<?php echo $Especialistas->Folio_Credito; ?>">
-      <input type="text" class="form-control "  name="TramientoTicket" readonly value="<?php echo $Especialistas->Nombre_Tip; ?>" aria-describedby="basic-addon1" maxlength="60"> 
-      <input type="number" class="form-control "  name="AbonoTicket" id="abonoticket" readonly   aria-describedby="basic-addon1" maxlength="60">  
-      <input type="number" class="form-control "  name="SaldoTicket" id="saldoticket" readonly   aria-describedby="basic-addon1" maxlength="60">  
-      <input type="text" class="form-control "  readonly name="TitularCredito" value="<?php echo $Especialistas->Nombre_Cred; ?>" aria-describedby="basic-addon1" maxlength="60"> 
+      <input type="text" class="form-control "  name="TramientoTicket" readonly value="<?php echo $Especialistas->Nombre_Tip; ?>" aria-describedby="basic-addon1" > 
+      <input type="number" class="form-control "  name="AbonoTicket" id="abonoticket" readonly   aria-describedby="basic-addon1" >  
+      <input type="number" class="form-control "  name="SaldoTicket" id="saldoticket" readonly   aria-describedby="basic-addon1" >  
+      <input type="text" class="form-control "  readonly name="TitularCredito" value="<?php echo $Especialistas->Nombre_Cred; ?>" aria-describedby="basic-addon1" > 
       <input type="text" class="form-control "   readonly name="VendedorTicket"  readonly value="<?php echo $row['Nombre_Apellidos']?>">
-      <input type="text" class="form-control "  readonly name="SaldoActualTicket" value="<?php echo $Especialistas->Saldo; ?>" aria-describedby="basic-addon1" maxlength="60">   
-      <input type="text" class="form-control "  readonly name="FechaValidez" value="<?php echo $Especialistas->Validez; ?>" aria-describedby="basic-addon1" maxlength="60">   
+      <input type="text" class="form-control "  readonly name="SaldoActualTicket" value="<?php echo $Especialistas->Saldo; ?>" aria-describedby="basic-addon1" >   
+      <input type="text" class="form-control "  readonly name="FechaValidez" value="<?php echo $Especialistas->Validez; ?>" aria-describedby="basic-addon1" >   
       <input type="datetime" name="Horadeimpresion" value="<?php echo date('h:i:s A');?>">
       <button type="submit"  id="EnviaTicket"  class="btn btn-info">Realizar abono <i class="fas fa-money-check-alt"></i></button>
 </form>
@@ -45,18 +45,18 @@ while ($r=$query->fetch_object()){
 <form action="javascript:void(0)" method="post" id="GuardaReimpresionTicket" >
       <input type="number" class="form-control " name="NumeroTicketR" value="<?php echo $totalmonto;?>"readonly  >
       <input type="text" class="form-control "  name="FolioCreditoR"  readonly value="<?php echo $Especialistas->Folio_Credito; ?>">
-      <input type="text" class="form-control "  name="TramientoTicketR" readonly value="<?php echo $Especialistas->Nombre_Tip; ?>" aria-describedby="basic-addon1" maxlength="60"> 
-      <input type="number" class="form-control "  name="AbonoTicketR" id="abonoticketr" readonly   aria-describedby="basic-addon1" maxlength="60">  
-      <input type="number" class="form-control "  name="SaldoTicketR" id="saldoticketr" readonly   aria-describedby="basic-addon1" maxlength="60">  
-      <input type="text" class="form-control "  readonly name="TitularCreditoR" value="<?php echo $Especialistas->Nombre_Cred; ?>" aria-describedby="basic-addon1" maxlength="60"> 
+      <input type="text" class="form-control "  name="TramientoTicketR" readonly value="<?php echo $Especialistas->Nombre_Tip; ?>" aria-describedby="basic-addon1" > 
+      <input type="number" class="form-control "  name="AbonoTicketR" id="abonoticketr" readonly   aria-describedby="basic-addon1" >  
+      <input type="number" class="form-control "  name="SaldoTicketR" id="saldoticketr" readonly   aria-describedby="basic-addon1" >  
+      <input type="text" class="form-control "  readonly name="TitularCreditoR" value="<?php echo $Especialistas->Nombre_Cred; ?>" aria-describedby="basic-addon1" > 
       <input type="text" class="form-control "   readonly name="VendedorTicketR"  readonly value="<?php echo $row['Nombre_Apellidos']?>">
-      <input type="text" class="form-control "  readonly name="SaldoAnteriorTicketR" value="<?php echo $Especialistas->Saldo; ?>" aria-describedby="basic-addon1" maxlength="60">   
-      <input type="text" class="form-control "  readonly name="FechaValidezR" value="<?php echo $Especialistas->Validez; ?>" aria-describedby="basic-addon1" maxlength="60">   
+      <input type="text" class="form-control "  readonly name="SaldoAnteriorTicketR" value="<?php echo $Especialistas->Saldo; ?>" aria-describedby="basic-addon1" >   
+      <input type="text" class="form-control "  readonly name="FechaValidezR" value="<?php echo $Especialistas->Validez; ?>" aria-describedby="basic-addon1" >   
       <input type="datetime" name="HoraPago" value="<?php echo date('h:i:s A');?>">
       <input type="text" class="form-control "  readonly id="sistemacajar" name="SistemaCajaR" readonly value="POS <?php echo $row['Nombre_rol']?>">
 <input type="text" class="form-control "    readonly id="empresacajar" name="EmpresaCajaR" readonly value="<?php echo $row['ID_H_O_D']?>">
-<input type="text" class="form-control " name="SucursalR" id="sucursalr" value="<?php echo $Especialistas->Fk_Sucursal; ?>" aria-describedby="basic-addon1" maxlength="60">   
-<input type="date" class="form-control " readonly name="FechaAbonoR" id="fechaabonor" value="<?php echo $fcha;?>" aria-describedby="basic-addon1" maxlength="60">
+<input type="text" class="form-control " name="SucursalR" id="sucursalr" value="<?php echo $Especialistas->Fk_Sucursal; ?>" aria-describedby="basic-addon1" >   
+<input type="date" class="form-control " readonly name="FechaAbonoR" id="fechaabonor" value="<?php echo $fcha;?>" aria-describedby="basic-addon1" >
       <button type="submit"  id="EnviaReimpresionTicket"  class="btn btn-info">Realizar abono <i class="fas fa-money-check-alt"></i></button>
 </form>
 </div>
@@ -83,7 +83,7 @@ while ($r=$query->fetch_object()){
   
     <span class="input-group-text" id="Tarjeta"><i class="fas fa-file-signature"></i></span>
   </div>
-  <input type="text" class="form-control "  readonly value="<?php echo $Especialistas->Nombre_Tip; ?>" aria-describedby="basic-addon1" maxlength="60">            
+  <input type="text" class="form-control "  readonly value="<?php echo $Especialistas->Nombre_Tip; ?>" aria-describedby="basic-addon1" >            
   <input type="text" class="form-control " hidden name="FolioTipocred" id="foliotipocred" readonly value="<?php echo $Especialistas->Fk_tipo_Credi; ?>">
 </div></div></div>
 
@@ -95,7 +95,7 @@ while ($r=$query->fetch_object()){
   
     <span class="input-group-text" id="Tarjeta"><i class="fas fa-file-signature"></i></span>
   </div>
-  <input type="text" class="form-control "  readonly name="Titular" id="titular" value="<?php echo $Especialistas->Nombre_Cred; ?>" aria-describedby="basic-addon1" maxlength="60">            
+  <input type="text" class="form-control "  readonly name="Titular" id="titular" value="<?php echo $Especialistas->Nombre_Cred; ?>" aria-describedby="basic-addon1" >            
 </div></div>
 <div class="col">
     <label for="exampleFormControlInput1">Fecha<span class="text-danger">*</span></label>
@@ -104,7 +104,7 @@ while ($r=$query->fetch_object()){
   
     <span class="input-group-text" id="Tarjeta"><i class="fas fa-file-signature"></i></span>
   </div>
-  <input type="date" class="form-control " readonly name="FechaAbono" id="fechaabono" value="<?php echo $fcha;?>" aria-describedby="basic-addon1" maxlength="60">            
+  <input type="date" class="form-control " readonly name="FechaAbono" id="fechaabono" value="<?php echo $fcha;?>" aria-describedby="basic-addon1" >            
 </div></div></div>
 <div class="row">
     <div class="col">
@@ -112,8 +112,8 @@ while ($r=$query->fetch_object()){
     <div class="input-group mb-3">
   <div class="input-group-prepend">  <span class="input-group-text" id="Tarjeta"><i class="fas fa-receipt"></i></span>
   </div>
-  <input type="text" class="form-control "  readonly value="<?php echo $Especialistas->Nombre_Sucursal; ?>" aria-describedby="basic-addon1" maxlength="60">
-  <input type="text" class="form-control " hidden name="Sucursal" id="sucursal" value="<?php echo $Especialistas->Fk_Sucursal; ?>" aria-describedby="basic-addon1" maxlength="60">   
+  <input type="text" class="form-control "  readonly value="<?php echo $Especialistas->Nombre_Sucursal; ?>" aria-describedby="basic-addon1" >
+  <input type="text" class="form-control " hidden name="Sucursal" id="sucursal" value="<?php echo $Especialistas->Fk_Sucursal; ?>" aria-describedby="basic-addon1" >   
     </div>
     </div>
     <div class="col">
@@ -121,7 +121,7 @@ while ($r=$query->fetch_object()){
     <div class="input-group mb-3">
   <div class="input-group-prepend">  <span class="input-group-text" id="Tarjeta"><i class="fas fa-receipt"></i></span>
   </div>
-  <input type="text" class="form-control "  readonly name="SaldoActual" id="saldoactual" value="<?php echo $Especialistas->Saldo; ?>" aria-describedby="basic-addon1" maxlength="60">   
+  <input type="text" class="form-control "  readonly name="SaldoActual" id="saldoactual" value="<?php echo $Especialistas->Saldo; ?>" aria-describedby="basic-addon1" >   
     </div>
     <label for="abono" class="error"> 
   </div></div>
@@ -131,7 +131,7 @@ while ($r=$query->fetch_object()){
     <div class="input-group mb-3">
   <div class="input-group-prepend">  <span class="input-group-text" id="Tarjeta"><i class="fas fa-receipt"></i></span>
   </div>
-  <input type="text" class="form-control "  name="Abono" id="abono" oninput="CapturaValorVenta()" aria-describedby="basic-addon1" maxlength="60">   
+  <input type="text" class="form-control "  name="Abono" id="abono" oninput="CapturaValorVenta()" aria-describedby="basic-addon1" >   
     </div>
     </div>
     <div class="col">
@@ -168,7 +168,7 @@ while ($r=$query->fetch_object()){
   
     <span class="input-group-text" id="Tarjeta"><i class="fas fa-file-signature"></i></span>
   </div>
-  <input type="number" class="form-control "  name="Ajuste" id="ajuste" readonly   aria-describedby="basic-addon1" maxlength="60">            
+  <input type="number" class="form-control "  name="Ajuste" id="ajuste" readonly   aria-describedby="basic-addon1" >            
 </div></div>
 
 
@@ -182,17 +182,17 @@ while ($r=$query->fetch_object()){
 
   
    
-  <input type="number" class="form-control "  name="ID_PROD" value="0000000000" readonly   aria-describedby="basic-addon1" maxlength="60">            
+  <input type="number" class="form-control "  name="ID_PROD" value="0000000000" readonly   aria-describedby="basic-addon1" >            
  
-  <input type="Text" class="form-control "  name="IDentificador" value="Créditos" readonly   aria-describedby="basic-addon1" maxlength="60">  
+  <input type="Text" class="form-control "  name="IDentificador" value="Créditos" readonly   aria-describedby="basic-addon1" >  
 
   <input type="number" class="form-control " id="ticketval" name="TicketVal" value="<?php echo $totalmonto;?>"readonly  > 
-  <input type="number" class="form-control "  name="ClavAd" value="0000000000" readonly   aria-describedby="basic-addon1" maxlength="60">
-  <input type="number" class="form-control "  name="CodBarra" value="0000000000" readonly   aria-describedby="basic-addon1" maxlength="60">
-  <input type="text" class="form-control "  name="NombreProducto" value="Abono de crédito" readonly   aria-describedby="basic-addon1" maxlength="60">
-  <input type="number" class="form-control "  name="CantidadVenta" value="1" readonly   aria-describedby="basic-addon1" maxlength="60">  
-  <input type="text" class="form-control "  name="SucursalesS" value="<?php echo $row['Fk_Sucursal']?>" readonly   aria-describedby="basic-addon1" maxlength="60">      
-  <input type="number" class="form-control "  name="TotalVenta" id="totalventa" readonly   aria-describedby="basic-addon1" maxlength="60">  
+  <input type="number" class="form-control "  name="ClavAd" value="0000000000" readonly   aria-describedby="basic-addon1" >
+  <input type="number" class="form-control "  name="CodBarra" value="0000000000" readonly   aria-describedby="basic-addon1" >
+  <input type="text" class="form-control "  name="NombreProducto" value="Abono de crédito" readonly   aria-describedby="basic-addon1" >
+  <input type="number" class="form-control "  name="CantidadVenta" value="1" readonly   aria-describedby="basic-addon1" >  
+  <input type="text" class="form-control "  name="SucursalesS" value="<?php echo $row['Fk_Sucursal']?>" readonly   aria-describedby="basic-addon1" >      
+  <input type="number" class="form-control "  name="TotalVenta" id="totalventa" readonly   aria-describedby="basic-addon1" >  
   <input type="text" class="form-control "  name="CajaAsignada" id="cajaasignada" value="<?php echo $ValorCaja["ID_Caja"];?>">  
   <input type="text" class="form-control "  name="Lote"  value="N/A">  
   <input type="text" class="form-control "  readonly id="sistemacaja" name="SistemaCaja" readonly value="POS <?php echo $row['Nombre_rol']?>">
