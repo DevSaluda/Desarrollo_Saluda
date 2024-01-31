b83f924e36738fae74f9929346b1282e550ba693
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 10:39:42 -0600

    Update DesgloseTicket.php

--- POS2/Modales/DesgloseTicket.php
+++ POS2/Modales/DesgloseTicket.php
@@ -152,7 +152,7 @@ while ($r=$query->fetch_object()){
   
 <?php else:?>
   <p class="alert alert-danger">404 No se encuentra  <br>El ticket puede corresponder a un crédito, te sugerimos revisar el área de créditos  </p>
-<? endif;?>
+<?php endif;?>
 <?php
 
 function fechaCastellano ($fecha) {
