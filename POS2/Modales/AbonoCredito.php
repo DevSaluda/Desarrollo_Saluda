560dee88129651f261815d05bd4894c48b6de12b
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 15:17:24 -0600

    Update AbonoCredito.php

--- POS2/Modales/AbonoCredito.php
+++ POS2/Modales/AbonoCredito.php
@@ -7,7 +7,15 @@ $fcha = date("Y-m-d");
 $user_id = null;
 
 // Usando consulta preparada para evitar problemas de seguridad
-$sql1 = "SELECT Creditos_POS.Folio_Credito, Creditos_POS.Fk_tipo_Credi, ... WHERE Creditos_POS.Fk_tipo_Credi=Tipos_Credit_POS.ID_Tip_Cred AND Creditos_POS.Fk_Sucursal = SucursalesCorre.ID_SucursalC AND Creditos_POS.ID_H_O_D=? AND Creditos_POS.Folio_Credito=?";
+$sql1 = "SELECT Creditos_POS.Folio_Credito, Creditos_POS.Fk_tipo_Credi, Creditos_POS.Nombre_Cred, Creditos_POS.Cant_Apertura, Creditos_POS.Fk_Sucursal, Creditos_POS.Validez, Creditos_POS.Saldo,
+    Creditos_POS.Estatus, Creditos_POS.CodigoEstatus, Creditos_POS.ID_H_O_D, Tipos_Credit_POS.ID_Tip_Cred,
+    Tipos_Credit_POS.Nombre_Tip, SucursalesCorre.ID_SucursalC, SucursalesCorre.Nombre_Sucursal 
+    FROM Creditos_POS, Tipos_Credit_POS, SucursalesCorre 
+    WHERE Creditos_POS.Fk_tipo_Credi=Tipos_Credit_POS.ID_Tip_Cred 
+    AND Creditos_POS.Fk_Sucursal = SucursalesCorre.ID_SucursalC 
+    AND Creditos_POS.ID_H_O_D = ?
+    AND Creditos_POS.Folio_Credito = ?";
+
 $stmt = $conn->prepare($sql1);
 $stmt->bind_param("ss", $row['ID_H_O_D'], $_POST["id"]);
 
@@ -29,7 +37,6 @@ if ($stmt->execute()) {
     // Error en la ejecución de la consulta preparada
     echo "Error en la ejecución de la consulta preparada: " . $stmt->error;
 }
-
 
 $Especialistas = null;
 
