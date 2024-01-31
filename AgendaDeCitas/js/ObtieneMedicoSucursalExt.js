74a2be6f341b344b2508ff888dc9e86def66af7d
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 15:56:18 -0600

    Update ObtieneMedicoSucursalExt.js

--- AgendaDeCitas/js/ObtieneMedicoSucursalExt.js
+++ AgendaDeCitas/js/ObtieneMedicoSucursalExt.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
     $('#especialidadext').on('change', function(){
-        if ($('#especialidadext').val() == "") {
+        var especialidadextValue = $(this).val();
+
+        if (especialidadextValue !== "") {
+            $('#medicoext').prop('disabled', false);
+            $('#medicoext').load('Consultas/ObtieneMedicoProgramacion.php?sucursalExt=' + especialidadextValue);
+        } else {
             $('#medicoext').empty();
             $('<option value="">Selecciona un medicoext</option>').appendTo('#medicoext');
             $('#medicoext').prop('disabled', true);
-        } else {
-            $('#medicoext').prop('disabled', false);
-            var url = 'Consultas/ObtieneMedicoProgramacion.php?sucursalExt=' + $('#especialidadext').val();
-            console.log('URL de solicitud:', url);
-            $('#medicoext').load(url, function(response, status, xhr) {
-                console.log('Respuesta del servidor:', response);
-            });
         }
     });
 });
