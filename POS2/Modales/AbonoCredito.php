f974e1bfe39e698f2096184a8b6289fdf00264e6
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 15:15:58 -0600

    Update AbonoCredito.php

--- POS2/Modales/AbonoCredito.php
+++ POS2/Modales/AbonoCredito.php
@@ -7,19 +7,29 @@ $fcha = date("Y-m-d");
 $user_id = null;
 
 // Usando consulta preparada para evitar problemas de seguridad
-$sql1 = "SELECT Creditos_POS.Folio_Credito, Creditos_POS.Fk_tipo_Credi, Creditos_POS.Nombre_Cred, Creditos_POS.Cant_Apertura, Creditos_POS.Fk_Sucursal, Creditos_POS.Validez, Creditos_POS.Saldo,
-    Creditos_POS.Estatus, Creditos_POS.CodigoEstatus, Creditos_POS.ID_H_O_D, Tipos_Credit_POS.ID_Tip_Cred,
-    Tipos_Credit_POS.Nombre_Tip, SucursalesCorre.ID_SucursalC, SucursalesCorre.Nombre_Sucursal 
-    FROM Creditos_POS, Tipos_Credit_POS, SucursalesCorre 
-    WHERE Creditos_POS.Fk_tipo_Credi=Tipos_Credit_POS.ID_Tip_Cred 
-    AND Creditos_POS.Fk_Sucursal = SucursalesCorre.ID_SucursalC 
-    AND Creditos_POS.ID_H_O_D = ?
-    AND Creditos_POS.Folio_Credito = ?";
-
+$sql1 = "SELECT Creditos_POS.Folio_Credito, Creditos_POS.Fk_tipo_Credi, ... WHERE Creditos_POS.Fk_tipo_Credi=Tipos_Credit_POS.ID_Tip_Cred AND Creditos_POS.Fk_Sucursal = SucursalesCorre.ID_SucursalC AND Creditos_POS.ID_H_O_D=? AND Creditos_POS.Folio_Credito=?";
 $stmt = $conn->prepare($sql1);
 $stmt->bind_param("ss", $row['ID_H_O_D'], $_POST["id"]);
-$stmt->execute();
-$result = $stmt->get_result();
+
+if ($stmt->execute()) {
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        while ($r = $result->fetch_object()) {
+            $Especialistas = $r;
+            break;
+        }
+    } else {
+        // No se encontraron resultados
+        echo "No se encontraron resultados para el ID " . $_POST["id"];
+    }
+
+    $stmt->close();
+} else {
+    // Error en la ejecución de la consulta preparada
+    echo "Error en la ejecución de la consulta preparada: " . $stmt->error;
+}
+
 
 $Especialistas = null;
 
@@ -205,7 +215,7 @@ if ($result->num_rows > 0) {
 <input type="text" class="form-control "    readonly id="empresacaja" name="EmpresaCaja" readonly value="<?php echo $row['ID_H_O_D']?>">
 <input type="text" class="form-control "   readonly name="UsuarioCaja" id="usuariocaja"  readonly value="<?php echo $row['Nombre_Apellidos']?>">
 </form>
-<!-- <script>
+<script>
         var precio1 = document.getElementById("saldoactual");
         var precio2 = document.getElementById("abono");
         var precio3 = document.getElementById("saldonuevo");
@@ -235,7 +245,7 @@ if ($result->num_rows > 0) {
             document.getElementById("abonoticket").value = abono;
             document.getElementById("abonoticketr").value = abono;
         }
-    </script> -->
+    </script>
 
 <?php else: ?>
     <p class="alert alert-danger">404 No se encuentra</p>
