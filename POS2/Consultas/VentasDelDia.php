3dde0b3802a852da869999d9101e1cc224a795dc
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 10:38:50 -0600

    Update VentasDelDia.php

--- POS2/Consultas/VentasDelDia.php
+++ POS2/Consultas/VentasDelDia.php
@@ -143,7 +143,7 @@ $(".btn-Reimpresion").click(function(){
     $('#Cancelacionmodal').modal('show');
 });
 </script>
-<?
+<?php
 
 function fechaCastellano ($fecha) {
   $fecha = substr($fecha, 0, 10);
