6d588edf8fceaa3fa8cbdb48143652fcd3ecf228
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 16:20:32 -0600

    xdd

--- AgendaDeCitas/js/ObtieneMedicoSucursalExt.js
+++ AgendaDeCitas/js/ObtieneMedicoSucursalExt.js
@@ -1,21 +1,14 @@
-$(document).ready(function(){
+$('document').ready(function(){
     $('#especialidadext').on('change', function(){
-        if ($('#especialidadext').val() == "") {
-            $('#medicoext').empty();
-            $('<option value="">Selecciona un medicoext</option>').appendTo('#medicoext');
-            $('#medicoext').prop('disabled', true);
-        } else {
-            // Habilitar el elemento select
-            $('#medicoext').prop('disabled', false);
-
-            // Obtener el valor del elemento select
-            var especialidadValue = $('#especialidadext').val();
-
-            // Deshabilitar el elemento select nuevamente
-            $('#medicoext').prop('disabled', true);
-
-            // Llamar a la carga de datos con el valor obtenido
-            $('#medicoext').load('Consultas/ObtieneMedicoProgramacion.php?sucursalExt=' + especialidadValue);
-        }
+            if($('#especialidadext').val() == ""){
+                $('#medicoext').empty();
+                $('<option value = "">Selecciona un medicoext</option>').appendTo('#medicoext');
+                $('#medicoext').attr('disabled', 'disabled');
+            }else{
+                $('#medicoext').removeAttr('disabled', 'disabled');
+                $('#medicoext').load('Consultas/ObtieneMedicoProgramacion.php?especialidadext=' + $('#especialidadext').val());
+                
+            }
     });
 });
+            
