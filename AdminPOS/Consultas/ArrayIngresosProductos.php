a3c38d6b7912207f116b57f6f2b64503b96e9631
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-01 11:44:56 -0600

    Update ArrayIngresosProductos.php

--- AdminPOS/Consultas/ArrayIngresosProductos.php
+++ AdminPOS/Consultas/ArrayIngresosProductos.php
@@ -4,10 +4,26 @@ header('Content-Type: application/json');
 include("db_connection.php");
 include "Consultas.php";
 
-$sql = "SELECT Stock_registrosNuevos.Folio_Ingreso,Stock_registrosNuevos.ID_Prod_POS,Stock_registrosNuevos.Fk_sucursal,Stock_registrosNuevos.Factura,
-Stock_registrosNuevos.Existencias_R,Stock_registrosNuevos.ExistenciaPrev,Stock_registrosNuevos.Recibido,Stock_registrosNuevos.AgregadoPor,Stock_registrosNuevos.AgregadoEl, SucursalesCorre.ID_SucursalC,SucursalesCorre.Nombre_Sucursal,Productos_POS.ID_Prod_POS,Productos_POS.Cod_Barra,Productos_POS.Nombre_Prod FROM
-Productos_POS,SucursalesCorre,Stock_registrosNuevos WHERE Stock_registrosNuevos.Fk_sucursal = SucursalesCorre.ID_SucursalC and 
-Stock_registrosNuevos.ID_Prod_POS = Productos_POS.ID_Prod_POS AND Stock_registrosNuevos.Fk_sucursal='".$row['Fk_Sucursal']."'";
+$sql = "SELECT 
+Stock_registrosNuevos.Folio_Ingreso,
+Stock_registrosNuevos.ID_Prod_POS,
+Stock_registrosNuevos.Fk_sucursal,
+Stock_registrosNuevos.Factura,
+Stock_registrosNuevos.Existencias_R,
+Stock_registrosNuevos.ExistenciaPrev,
+Stock_registrosNuevos.Recibido,
+Stock_registrosNuevos.AgregadoPor,
+Stock_registrosNuevos.AgregadoEl,
+SucursalesCorre.ID_SucursalC,
+SucursalesCorre.Nombre_Sucursal,
+Productos_POS.ID_Prod_POS,
+Productos_POS.Cod_Barra,
+Productos_POS.Nombre_Prod,
+Productos_POS.Precio_C
+FROM
+Stock_registrosNuevos
+JOIN SucursalesCorre ON Stock_registrosNuevos.Fk_sucursal = SucursalesCorre.ID_SucursalC
+JOIN Productos_POS ON Stock_registrosNuevos.ID_Prod_POS = Productos_POS.ID_Prod_POS AND Stock_registrosNuevos.Fk_sucursal='".$row['Fk_Sucursal']."'";
  
 $result = mysqli_query($conn, $sql);
  
