17817611b62a7bd930ba699990f7871772f1b894
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 21:42:53 -0600

    xddd

--- AdminPOS/Consultas/config.php
+++ AdminPOS/Consultas/config.php
@@ -1,16 +1,18 @@
 <?php
 $server = "localhost";
-$username = "somosgr1_SHWEB";
-$password = "yH.0a-v?T*1R";
-$dbname = "somosgr1_Sistema_Hospitalario";
+$username = "u155356178_SaludDevCenter";
+$password = "uE;bAISz;*6c|I4PvEnfSys324\\Zavp2zJ:9TLx{]L&QMcmhAdmSCDBSN3iH4UV3D24WMF@2024myV>";
+$dbname = "u155356178_saludapos";
 
 // creamos la conexion con MySQL
-try{
-$db = new PDO("mysql:host=$server;dbname=$dbname","$username","$password");
-$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-die('No se pudo conectar con la base de datos');
+try {
+    $db = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Establecer el huso horario a GMT-6
+    $sqlSetTimeZone = "SET time_zone = '-6:00'";
+    $db->exec($sqlSetTimeZone);
+} catch (PDOException $e) {
+    die('No se pudo conectar con la base de datos');
 }
-
-
 ?>
