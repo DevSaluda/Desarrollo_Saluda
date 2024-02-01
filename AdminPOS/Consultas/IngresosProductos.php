97f56c402120d6b56faf339652d1e333c8479786
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-01 11:59:20 -0600

    Update IngresosProductos.php

--- AdminPOS/Consultas/IngresosProductos.php
+++ AdminPOS/Consultas/IngresosProductos.php
@@ -10,19 +10,18 @@ tabla = $('#Productos').DataTable({
  "order": [[ 0, "desc" ]],
  "sAjaxSource": "https://saludapos.com/AdminPOS/Consultas/ArrayIngresosProductos.php",
  "aoColumns": [
-       { mData: 'Folio_Ingreso' },
-       { mData: 'Factura' },
-       { mData: 'Cod_Barra' },
-       { mData: 'Nombre_Prod' },
-       { mData: 'Precio_C' },
-       { mData: 'Existencias_R' },
-       { mData: 'ExistenciaPrev' },
-       { mData: 'Recibido' },
-       { mData: 'Sucursal' },
-       { mData: 'AgregadoPor' },
-
-       { mData: 'AgregadoEl' },
-       { mData: 'totalfactura' },
+    { mData: 'Folio_Ingreso' },
+                { mData: 'Factura' },
+                { mData: 'Cod_Barra' },
+                { mData: 'Nombre_Prod' },
+                { mData: 'Precio_C' },
+                { mData: 'Existencias_R' },
+                { mData: 'ExistenciaPrev' },
+                { mData: 'Recibido' },
+                { mData: 'totalfactura' },
+                { mData: 'Sucursal' },
+                { mData: 'AgregadoPor' },
+                { mData: 'AgregadoEl' },
        
       ],
      
@@ -93,94 +92,3 @@ tabla = $('#Productos').DataTable({
 </div>
 
 
-<!-- <script>
-  $(".btn-edit").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/ReasignaProducto.php","id="+id,function(data){
-        alert($(this).data("id"));
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Asignacion de productos en otras sucursales");
-       
-        $("#DiProductosG").removeClass("modal-dialog  modal-xl modal-notify modal-info");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").addClass("modal-dialog  modal-xl modal-notify modal-success");
-    });
-    $('#editModalProductosG').modal('show'); 
-});
-$(".btn-VerDistribucion").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/DistribucionesProductos.php","id="+id,function(data){
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Distribucion de productos");
-       
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-danger");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").addClass("modal-dialog  modal-xl modal-notify modal-info");
-    });
-    $('#editModalProductosG').modal('show');
-});
-$(".btn-editProd").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/EditaProductosStockGeneral.php","id="+id,function(data){
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Editar datos");
-       
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-danger");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").addClass("modal-dialog  modal-xl modal-notify modal-info");
-    });
-    $('#editModalProductosG').modal('show');
-});
-$(".btn-History").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/HistorialProductos.php","id="+id,function(data){
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Actualizaciones y movimientos");
-       
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-danger");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").addClass("modal-dialog  modal-xl modal-notify modal-info");
-    });
-    $('#editModalProductosG').modal('show');
-});
-
-
-$(".btn-Delete").click(function(){
-    id = $(this).data("id");
-    $.post("https://controlfarmacia.com/AdminPOS/Modales/DeleteProductos.php","id="+id,function(data){
-        alert($(this).data("id"));
-        $("#form-editProductosG").html(data);
-    $("#TituloProductosG").html("Eliminar producto");
-       
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-danger");
-        $("#DiProductosG").removeClass("modal-dialog modal-lg modal-notify modal-primary");
-        $("#DiProductosG").removeClass("modal-dialog  modal-xl modal-notify modal-info");
-        $("#DiProductosG").addClass("modal-dialog modal-sm modal-notify modal-danger");
-    });
-    $('#editModalProductosG').modal('show');
-});
-
-
-</script>
-
-
-  <div class="modal fade" id="editModalProductosG" tabindex="-1" role="dialog" style="overflow-y: scroll;" aria-labelledby="editModalLabel" aria-hidden="true">
-  <div id="DiProductosG"class="modal-dialog modal-lg modal-notify modal-info">
-      <div class="modal-content">
-      <div class="modal-header">
-         <p class="heading lead" id="TituloProductosG"></p>
-
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-           <span aria-hidden="true" class="white-text">&times;</span>
-         </button>
-       </div>
-       
-	        <div class="modal-body">
-          <div class="text-center">
-        <div id="form-editProductosG"></div>
-        
-        </div>
-
-      </div>
-    </div>
-  </div> -->
