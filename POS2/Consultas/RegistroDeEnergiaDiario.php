52a586ebbb654b68988fcf657e55e131b81a98ca
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-01 09:47:28 -0600

    Update RegistroDeEnergiaDiario.php

--- POS2/Consultas/RegistroDeEnergiaDiario.php
+++ POS2/Consultas/RegistroDeEnergiaDiario.php
@@ -1,58 +1,60 @@
 <?php
-
 include_once 'db_connection.php';
-if(!empty($_POST['name']) || !empty($_FILES['file']['name'])){
-    $uploadedFile = '';
-    if(!empty($_FILES["file"]["type"])){
-        $fileName = time().'_'.$_FILES['file']['name'];
-        $valid_extensions = array("jpeg", "jpg", "png");
-        $temporary = explode(".", $_FILES["file"]["name"]);
-        $file_extension = end($temporary);
-        if((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")|| ($_FILES["file"]["type"] == "image/png")) && in_array($file_extension, $valid_extensions)){
 
-            $sourcePath = $_FILES['file']['tmp_name'];
-            $targetPath = "../../FotosMedidores/$fileName";
-            if(move_uploaded_file($sourcePath,$targetPath)){
-                $uploadedFile = $fileName;
+if (!empty($_POST['name']) || !empty($_FILES['file']['name'])) {
+    $uploadedFile = '';
+
+    if (isset($_FILES["file"]["type"])) {
+        $fileName = '';
+        if (isset($_FILES['file']['name'])) {
+            $fileName = time() . '_' . $_FILES['file']['name'];
+            $valid_extensions = array("jpeg", "jpg", "png");
+            $temporary = explode(".", $_FILES["file"]["name"]);
+            $file_extension = end($temporary);
+
+            if (
+                (
+                    ($_FILES["file"]["type"] == "image/png") ||
+                    ($_FILES["file"]["type"] == "image/jpg") ||
+                    ($_FILES["file"]["type"] == "image/jpeg") ||
+                    ($_FILES["file"]["type"] == "image/png")
+                ) && in_array($file_extension, $valid_extensions)
+            ) {
+                $sourcePath = $_FILES['file']['tmp_name'];
+                $targetPath = "../../FotosMedidores/$fileName";
+                if (move_uploaded_file($sourcePath, $targetPath)) {
+                    $uploadedFile = $fileName;
+                }
             }
         }
+
+        $Registro_energia = $conn->real_escape_string(htmlentities(strip_tags(Trim($_POST['RegistroEnergia']))));
+        $Fecha_registro = $conn->real_escape_string(htmlentities(strip_tags(Trim($_POST['Fecha']))));
+        $Sucursal = $conn->real_escape_string(htmlentities(strip_tags(Trim($_POST['Sucursal']))));
+        $Comentario = $conn->real_escape_string(htmlentities(strip_tags(Trim($_POST['Comentario']))));
+        $Registro = $conn->real_escape_string(htmlentities(strip_tags(Trim($_POST['Registro']))));
+        $ID_H_O_D = $conn->real_escape_string(htmlentities(strip_tags(Trim($_POST['Empresa']))));
+
+        // Consulta para comprobar si ya existe un registro con los mismos valores
+        $sql = "SELECT Registro_energia,Fecha_registro,Registro FROM Registros_Energia WHERE Registro_energia='$Registro_energia' AND 
+            Fecha_registro='$Fecha_registro' AND Registro='$Registro'";
+        $resultset = mysqli_query($conn, $sql) or die("database error:" . mysqli_error($conn));
+        $row = mysqli_fetch_assoc($resultset);
+
+        if ($row && $row['Fecha_registro'] == $Fecha_registro && $row['Registro_energia'] == $Registro_energia && $row['Registro'] == $Registro) {
+            echo json_encode(array("statusCode" => 250));
+        } else {
+            $sql = "INSERT INTO `Registros_Energia`(`Registro_energia`, `Fecha_registro`, `Sucursal`, `Comentario`, `Registro`, `ID_H_O_D`,`file_name`) 
+                VALUES ('$Registro_energia', '$Fecha_registro', '$Sucursal', '$Comentario', '$Registro', '$ID_H_O_D','$uploadedFile')";
+
+            if (mysqli_query($conn, $sql)) {
+                echo json_encode(array("statusCode" => 200));
+            } else {
+                echo json_encode(array("statusCode" => 201));
+            }
+
+            mysqli_close($conn);
+        }
     }
-
-$Registro_energia=  $conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['RegistroEnergia']))));
- $Fecha_registro=  $conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['Fecha'])))); 
- $Sucursal=  $conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['Sucursal']))));
- $Comentario=  $conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['Comentario']))));
- $Registro=  $conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['Registro']))));
- $ID_H_O_D=  $conn -> real_escape_string(htmlentities(strip_tags(Trim($_POST['Empresa']))));
-
- //insert form data in the database
- $sql = "SELECT Registro_energia,Fecha_registro,Registro FROM Registros_Energia WHERE Registro_energia='$Registro_energia' AND 
- Fecha_registro='$Fecha_registro' AND Registro='$Registro' ";
-$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-$row = mysqli_fetch_assoc($resultset);	
-    //include database configuration file
-    if($row['Fecha_registro']==$Fecha_registro AND $row['Registro_energia']==$Registro_energia AND $row['Registro']==$Registro ){				
-        echo json_encode(array("statusCode"=>250));
-      
-    } 
-    else{
- 
-   
-    
-		$sql = "INSERT INTO `Registros_Energia`(`Registro_energia`, `Fecha_registro`, `Sucursal`, `Comentario`, `Registro`, `ID_H_O_D`,`file_name`) 
-		VALUES ( '$Registro_energia', '$Fecha_registro', '$Sucursal', '$Comentario', '$Registro', '$ID_H_O_D','$uploadedFile')";
-	
-		if (mysqli_query($conn, $sql)) {
-			echo json_encode(array("statusCode"=>200));
-		} 
-		else {
-			echo json_encode(array("statusCode"=>201));
-		}
-		
-   
-
-    mysqli_close($conn);
-	}
 }
 ?>
-
