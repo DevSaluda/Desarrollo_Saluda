ee3be7c8927db027513538d3add9bf07b18bd4bc
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-01-31 21:20:44 -0600

    zx

--- AdminPOS/Consultas/db_connection.php
+++ AdminPOS/Consultas/db_connection.php
@@ -7,8 +7,12 @@ $password = "uE;bAISz;*6c|I4PvEnfSys324\Zavp2zJ:9TLx{]L&QMcmhAdmSCDBSN3iH4UV3D24
 $dbname = "u155356178_saludapos";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("No podemos conectar a la base de datos: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
-    printf("algo salio mal, no podemos conectarnos a la base de datos %s\n", mysqli_connect_error());
+    printf("Algo salió mal, no podemos conectarnos a la base de datos %s\n", mysqli_connect_error());
     exit();
 }
+
+// Establecer el huso horario a GMT-6
+$sqlSetTimeZone = "SET time_zone = '-6:00'";
+mysqli_query($conn, $sqlSetTimeZone);
 
 ?>
