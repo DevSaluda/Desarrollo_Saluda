a891cf05255f71948a35127c8f7c81dc9e141cac
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-02 09:32:24 -0600

    xd

--- AdminPOS/Farmaceuticos.php
+++ AdminPOS/Farmaceuticos.php
@@ -22,14 +22,7 @@ include "Consultas/Consultas.php";
 </head>
 <?php include_once ("Menu.php")?>
 
-<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Personal Vigente</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Bajas</a>
-  </li>
-</ul>
+
 
 </ul>
 <div class="tab-content" id="pills-tabContent">
