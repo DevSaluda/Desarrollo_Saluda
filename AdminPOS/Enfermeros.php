c819ffbc4370cd5d62a56675a368d4ef825fae7c
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-02 09:23:20 -0600

    Update Enfermeros.php

--- AdminPOS/Enfermeros.php
+++ AdminPOS/Enfermeros.php
@@ -21,16 +21,7 @@ include "Consultas/Consultas.php";
     </style>
 </head>
 <?php include_once ("Menu.php")?>
-<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Personal Vigente</a>
-  </li>
-  
-  <li class="nav-item">
-    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Bajas</a>
-  </li>
- 
-</ul>
+
 <div class="tab-content" id="pills-tabContent">
 <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
 <div class="card text-center">
