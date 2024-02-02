d22a546079239f85719e90b12fca0bfb830513b5
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-02 12:05:26 -0600

    xdd

--- AdminPOS/js/AltaMedicos.js
+++ AdminPOS/js/AltaMedicos.js
@@ -121,7 +121,7 @@ vigencia: {
 
 		$.ajax({				
 			type : 'POST',
-			url  : 'https://controlconsulta.com/CEnfermeria/Consultas/AltaEmpMedico',
+			url  : 'https://saludapos.com/AdminPOS/Consultas/AltaEmpMedico',
 		  data: new FormData(this),
             contentType: false,
             cache: false,
