fe99196b65d7308d790dbd26524187534edd458c
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-02 09:26:13 -0600

    Update Farmaceuticos.php

--- AdminPOS/Farmaceuticos.php
+++ AdminPOS/Farmaceuticos.php
@@ -10,7 +10,7 @@ include "Consultas/Consultas.php";
 
   <title>Farmacéuticos | <?php echo $row['ID_H_O_D']?> </title>
 
-<?include "Header.php"?>
+<?php include "Header.php"?>
  <style>
         .error {
   color: red;
