7c020e8fb0d349785f7e844073c8496a9d55da0e
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-06 11:50:15 -0600

    Update Ultrasonidos.php

--- ServiciosEspecializados/Ultrasonidos.php
+++ ServiciosEspecializados/Ultrasonidos.php
@@ -35,8 +35,24 @@ include "Consultas/db_connection.php";
   
 
 
+  <div class="tab-content" id="pills-tabContent">
+<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+
     
 <div id="TablaPacientes"></div>
+
+</div>
+
+<!-- PRESENTACIONES -->
+
+<!-- POR CADUCAR -->
+  
+ 
+
+    
+</div>
+    
+
 
 
 </div>
