aff886f6dc1e0cf0b859c5e00b3cf6cda7cf041f
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-06 12:16:26 -0600

    Update AltaPacientes.php

--- ServiciosEspecializados/Modales/AltaPacientes.php
+++ ServiciosEspecializados/Modales/AltaPacientes.php
@@ -38,10 +38,8 @@
   <select name="Sucursal" id="sucursal" required class="form-control">
                                                <option value="0">Seleccione una sucursal:</option>
         <?php
-          $query = $conn -> query ("SELECT Nombre_Sucursal 
-          FROM Sucursales
-          WHERE Nombre_Sucursal NOT IN ('Hunucm&aacute;', 'Mama', 'Mani', 'Akil', 'CEDIS', 'CEDIS(Partner)', 'Capacitación');
-          ");
+          $query = $conn -> query ("SELECT Nombre_Sucursal  FROM SucursalesCorre
+          WHERE Nombre_Sucursal NOT IN ('Hunucm&aacute;', 'Mama', 'Mani', 'Akil', 'CEDIS', 'CEDIS(Partner)', 'Capacitación');");
           while ($valores = mysqli_fetch_array($query)) {
             echo '<option value="'.$valores["Nombre_Sucursal"].'">'.$valores["Nombre_Sucursal"].'</option>';
           }
