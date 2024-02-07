2e133503d60aa12581f497f7aee97856a40bab99
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-07 10:44:04 -0600

    Update Historial.php

--- ServiciosEspecializados/Consultas/Historial.php
+++ ServiciosEspecializados/Consultas/Historial.php
@@ -79,7 +79,7 @@ $query = $conn->query($sql1);
   <script>
   	$(".btn-edit").click(function(){
   		id = $(this).data("id");
-  		$.post("https://controlfarmacia.com/Servicios_Especializados/Modales/EditaEstatusUltraC.php","id="+id,function(data){
+  		$.post("https://saludapos.com/Servicios_Especializados/Modales/EditaEstatusUltraC.php","id="+id,function(data){
   			$("#form-edit").html(data);
   		});
   		$('#editModal').modal('show');
