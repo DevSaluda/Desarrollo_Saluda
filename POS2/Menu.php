01fadefe2b81f5597e6be934060c448c791c854b
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-15 15:01:40 -0600

    Update Menu.php

--- POS2/Menu.php
+++ POS2/Menu.php
@@ -10,144 +10,105 @@ include "Consultas/ConsultaCaja.php";
   <nav class="main-header navbar navbar-expand navbar-primary navbar-light" style="background-color: #c80096 !important;">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars" style="color: white;"></i></a>
-        </li>
-        <li class="nav-item">
-            <span class="scrolling-message">Hola, te recordamos que tenemos promociones vigentes hasta el 20 de febrero, en Laboratorios tenemos 30% descuento, Ultrasonidos *Sujeto a previa agenda* el 20% descuento y en Electrocardiogramas un 30% de descuento, recuerda aplicarlos antes de cobrarle al cliente.</span>
-        </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars" style="color: white;"></i></a>
+      </li>
+      
     </ul>
+
+    
 
     <ul class="navbar-nav ml-auto">
-        <!-- Messages Dropdown Menu -->
-        <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="far fa-comments" style="color: white;"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                                Brad Diesel
-                                <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">Call me whenever you can...</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                        </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                                John Pierce
-                                <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">I got your message bro</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                        </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <!-- Message Start -->
-                    <div class="media">
-                        <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                                Nora Silvester
-                                <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">The subject goes here</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                        </div>
-                    </div>
-                    <!-- Message End -->
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-comments" style="color: white;" ></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Brad Diesel
+                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">Call me whenever you can...</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
             </div>
-        </li>
-        <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
-                <i class="far fa-bell" style="color: white;"></i>
-                <span class="badge badge-info navbar-badge"><?php echo $totalmontotraspasos?></span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <span class="dropdown-item dropdown-header">Notificaciones</span>
-                <div class="dropdown-divider"></div>
-                <a href="ProductosConCambiosDePrecios" class="dropdown-item">
-                    <i class="fas fa-dollar-sign mr-2"></i> <?php echo $CambiosdepreciosNuevos['totalnotifi']?>  Cambios de precios
-                </a>
-                <div class="dropdown-divider"></div>
-                <a href="https://saludapos.com/POS2/ListadoDeTraspasos" class="dropdown-item">
-                    <i class="fas fa-exchange-alt mr-2"></i> <?php echo $TraspasosPendientes['traspasopendiente']?>  Traspasos pendientes
-                </a>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  John Pierce
+                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">I got your message bro</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
             </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link"><i class="fas fa-sign-out-alt" style="color: white;" data-toggle="modal" data-target="#Salida"></i></a>
-        </li>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Nora Silvester
+                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">The subject goes here</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+        </div>
+      </li>
+      <!-- Notifications Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-bell" style="color: white;"></i>
+          <span class="badge badge-info navbar-badge"><?php echo $totalmontotraspasos?></span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="dropdown-item dropdown-header">Notificaciones</span>
+          <div class="dropdown-divider"></div>
+          <a href="ProductosConCambiosDePrecios" class="dropdown-item">
+            <i class="fas fa-dollar-sign mr-2"></i> <?php echo $CambiosdepreciosNuevos['totalnotifi']?>  Cambios de precios
+           
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="https://saludapos.com/POS2/ListadoDeTraspasos" class="dropdown-item">
+          <i class="fas fa-exchange-alt mr-2"></i> <?php echo $TraspasosPendientes['traspasopendiente']?>  Traspasos pendientes
+          
+          </a>
+          <!-- <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-file mr-2"></i> 3 new reports
+            <span class="float-right text-muted text-sm">2 days</span>
+          </a>
+          <div class="dropdown-divider"></div> -->
+         
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"><i
+        class="fas fa-sign-out-alt" style="color: white;" data-toggle="modal" data-target="#Salida"></i></a>
+      </li>
     </ul>
-</nav>
-
-<style>
-    .scrolling-message {
-        animation: scrollText 30s linear infinite;
-        white-space: nowrap;
-        overflow: hidden;
-        display: inline-block;
-    }
-
-    @keyframes scrollText {
-        0% {
-            transform: translateX(100%);
-        }
-        100% {
-            transform: translateX(-100%);
-        }
-    }
-</style>
-
-
-<style>
-    .scrolling-message {
-        animation: scrollText 30s linear infinite;
-        white-space: nowrap;
-        overflow: hidden;
-        display: inline-block;
-    }
-
-    @keyframes scrollText {
-        0% {
-            transform: translateX(100%);
-        }
-        100% {
-            transform: translateX(-100%);
-        }
-    }
-</style>
-
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        var message = document.querySelector(".scrolling-message");
-        message.addEventListener("animationiteration", function() {
-            message.style.animation = "none";
-            void message.offsetWidth; /* Reinicia la animación */
-            message.style.animation = "scrollText 30s linear infinite"; /* Ajusta la duración según tus necesidades */
-        });
-    });
-</script>
-
+  </nav>
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
