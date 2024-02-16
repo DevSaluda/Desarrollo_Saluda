e3caf36c6c6a31d65fd105074738498852f41390
WebSite403 <wsite2816@gmail.com>
2024-02-16 09:34:03 -0600

    IanDev

--- JefaturaEnfermeria/RegistroPersonalDia.php
+++ JefaturaEnfermeria/RegistroPersonalDia.php
@@ -10,7 +10,7 @@ include "Consultas/Consultas.php";
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>Total de horas trabajadas del personal de  <?php echo $row['ID_H_O_D']?> </title>
+  <title>Total de horas trabajadas del personal   <?php echo $row['ID_H_O_D']?> </title>
 
 <?php include "Header.php"?>
  <style>
