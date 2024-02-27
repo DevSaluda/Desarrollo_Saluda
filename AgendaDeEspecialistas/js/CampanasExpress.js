6d0826b9ff1908a692baa139a4d3ecb2ba971389
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-02-27 16:46:39 -0600

    Update CampanasExpress.js

--- AgendaDeEspecialistas/js/CampanasExpress.js
+++ AgendaDeEspecialistas/js/CampanasExpress.js
@@ -1,7 +1,7 @@
 function CargaCitasEnSucursalExt(){
 
 
-    $.post("https://saludapos.com/AgendaDeCitas/Consultas/CitasEnSucursalExt.php","",function(data){
+    $.post("https://saludapos.com/AgendaDeEspecialistas/Consultas/CitasEnSucursalExt.php","",function(data){
       $("#CitasEnLaSucursalExt").html(data);
     })
   
