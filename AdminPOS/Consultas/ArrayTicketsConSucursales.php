81e7f3cd201704b92f58a0465e0c1a15ac27f575
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-04 16:54:39 -0600

    Ajuste en logica de los tickets

--- AdminPOS/Consultas/ArrayTicketsConSucursales.php
+++ AdminPOS/Consultas/ArrayTicketsConSucursales.php
@@ -43,7 +43,8 @@ Ventas_POS
 JOIN
 SucursalesCorre ON Ventas_POS.Fk_sucursal = SucursalesCorre.ID_SucursalC
 WHERE
-Ventas_POS.ID_H_O_D = '".$row['ID_H_O_D']."'
+Ventas_POS.Fk_sucursal = '".$row['Fk_Sucursal']."'
+AND Ventas_POS.ID_H_O_D = '".$row['ID_H_O_D']."'
 GROUP BY
 Ventas_POS.Folio_Ticket
 ORDER BY
