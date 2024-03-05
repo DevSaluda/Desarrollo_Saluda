3543d04c17e962f8cbbea8a7d17718fb6559d074
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-05 12:12:08 -0600

    Update index.php

--- ControlDental/index.php
+++ ControlDental/index.php
@@ -166,18 +166,7 @@
       
   <!-- Content Wrapper. Contains page content -->
   
-  <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-  
-  
-  <li class="nav-item">
-    <a class="nav-link active" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false" style="color:black !important;">Entradas Personal</a>
-  </li>
  
-  <!-- <li class="nav-item">
-    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-MoreVentas" role="tab" aria-controls="pills-contact" aria-selected="false" style="color:black !important;">Mas vendidos</a>
-  </li> -->
-</ul>
-
 <div class="tab-content" id="pills-tabContent">
 
 <!-- PRESENTACIONES -->
