5489a530c85825dd55304433c2a10c7612fe5fde
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-05 14:28:57 -0600

    InterfazVentas

--- ControlDental/js/ControlVentas.js
+++ ControlDental/js/ControlVentas.js
@@ -1,20 +1,12 @@
-function CargaVentasDelDia() {
-  // Realizar una solicitud AJAX a la URL de la API
-  $.ajax({
-      url: "https://saludapos.com/AdminPOS/Consultas/VentasDelDia.php",
-      type: "POST",
-      success: function(data) {
-          // Insertar los datos recibidos en el div con id "TableVentasDelDia"
-          $("#TableVentasDelDia").html(data);
-      },
-      error: function(xhr, status, error) {
-          // Manejar errores si ocurre alguno durante la solicitud
-          console.error("Error al cargar ventas del día:", error);
-      }
-  });
-}
+function CargaVentasDelDia(){
 
-// Llamar a la función para cargar las ventas del día cuando la página se cargue
-$(document).ready(function() {
+
+    $.post("https://saludapos.com/ControlDental/Consultas/VentasDelDia.php","",function(data){
+      $("#TableVentasDelDia").html(data);
+    })
+
+  }
+  
+  
+  
   CargaVentasDelDia();
-});
