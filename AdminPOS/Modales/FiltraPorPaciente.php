e6cef4379f9410c147d5d5666d479de877e5f2b6
WebSite403 <wsite2816@gmail.com>
2024-03-07 11:54:54 -0600

    filtrapaciente

--- AdminPOS/Modales/FiltraPorPaciente.php
+++ AdminPOS/Modales/FiltraPorPaciente.php
@@ -1,23 +1,20 @@
-
-  
-      <div class="modal fade bd-example-modal-xl" id="FiltraPorPaciente" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog  modal-notify modal-success">
-    <div class="modal-content">
-    
-    <div class="text-center">
-    <div class="modal-header">
-         <p class="heading lead">Filtra Por Nombre Paciente<i class="fas fa-credit-card"></i></p>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Buscar Paciente</title>
-</head>
-<body>
-    <h2>Buscar Paciente</h2>
-    <form action="FiltraPorNombrePaciente.php" method="GET">
-        <label for="nombre">Nombre del Paciente:</label>
-        <input type="text" id="nombre" name="nombre" required>
-        <button type="submit">Buscar</button>
-    </form>
-</body>
-</html>
+<div class="modal fade bd-example-modal-xl" id="FiltraPorPaciente" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-notify modal-success">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Filtra Por Nombre Paciente</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h2>Buscar Paciente</h2>
+                <form action="FiltraPorNombrePaciente.php" method="GET">
+                    <label for="nombre">Nombre del Paciente:</label>
+                    <input type="text" id="nombre" name="nombre" required>
+                    <button type="submit">Buscar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
