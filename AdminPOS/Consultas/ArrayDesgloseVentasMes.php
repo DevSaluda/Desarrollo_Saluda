7ee10d701cc78e27c2cfd089841e480cd936ec62
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-07 09:38:06 -0600

    Actualizaciondeventas

--- AdminPOS/Consultas/ArrayDesgloseVentasMes.php
+++ AdminPOS/Consultas/ArrayDesgloseVentasMes.php
@@ -5,11 +5,11 @@ include("db_connection.php");
 include "Consultas.php";
 
 
-$sql = "SELECT 
+$sql = "SELECT DISTINCT
+Ventas_POS.Venta_POS_ID,
 Ventas_POS.Folio_Ticket,
 Ventas_POS.FolioSucursal,
 Ventas_POS.Fk_Caja,
-Ventas_POS.Venta_POS_ID,
 Ventas_POS.Identificador_tipo,
 Ventas_POS.Fecha_venta, 
 Ventas_POS.Total_Venta,
@@ -25,7 +25,6 @@ Cajas_POS.MedicoEnturno,
 Cajas_POS.EnfermeroEnturno,
 Ventas_POS.Cod_Barra,
 Ventas_POS.Clave_adicional,
-Ventas_POS.Identificador_tipo,
 Ventas_POS.Nombre_Prod,
 Ventas_POS.Cantidad_Venta,
 Ventas_POS.Fk_sucursal,
@@ -37,7 +36,10 @@ SucursalesCorre.ID_SucursalC,
 SucursalesCorre.Nombre_Sucursal,
 Servicios_POS.Servicio_ID,
 Servicios_POS.Nom_Serv,
-Ventas_POS.DescuentoAplicado -- Agregamos la columna DescuentoAplicado
+Ventas_POS.DescuentoAplicado, -- Agregamos la columna DescuentoAplicado
+Stock_POS.ID_Prod_POS,
+Stock_POS.Precio_Venta,
+Stock_POS.Precio_C
 FROM 
 Ventas_POS
 INNER JOIN 
@@ -46,6 +48,8 @@ INNER JOIN
 Servicios_POS ON Ventas_POS.Identificador_tipo = Servicios_POS.Servicio_ID 
 INNER JOIN 
 Cajas_POS ON Cajas_POS.ID_Caja = Ventas_POS.Fk_Caja
+INNER JOIN 
+Stock_POS ON Stock_POS.ID_Prod_POS = Ventas_POS.ID_Prod_POS
 WHERE 
 (
     Ventas_POS.Fecha_venta >= DATE_FORMAT(NOW(), '%Y-%m-01') -- Primer día del mes en curso
@@ -56,8 +60,7 @@ OR
     Ventas_POS.Fecha_venta >= DATE_FORMAT(NOW() - INTERVAL 1 MONTH, '%Y-%m-01') -- Primer día del mes anterior
     AND Ventas_POS.Fecha_venta <= LAST_DAY(NOW() - INTERVAL 1 MONTH) -- Último día del mes anterior
 )
-AND Ventas_POS.Identificador_tipo = Servicios_POS.Servicio_ID;
-";
+AND Ventas_POS.Identificador_tipo = Servicios_POS.Servicio_ID";
 
 
 
