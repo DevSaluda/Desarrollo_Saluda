6cfb66fb88b6d79ce7b7ae3c0b31f25fd608c35d
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-07 14:15:25 -0600

    CitasLogica

--- POS2/AgendamientoDeCitasV3.php
+++ POS2/AgendamientoDeCitasV3.php
@@ -90,7 +90,7 @@ Citas de especialistas
 <script src="js/ObtieneMedicoDeSucursalExt.js"></script>
 <script src="js/ObtieneFechasExt.js"></script>
 <script src="js/ObtieneHorasExt.js"></script>
-<script src="js/GuardaCitasDeEspecialistas.js"></script>
+<script src="js/ArchivoParaGuardarCitas.js"></script>
 
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
