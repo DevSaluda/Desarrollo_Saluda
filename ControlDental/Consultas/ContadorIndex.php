5c8978f38aec35f8d761cdfcda1734a2c34de227
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-08 11:17:06 -0600

    Prueba de conteo

--- ControlDental/Consultas/ContadorIndex.php
+++ ControlDental/Consultas/ContadorIndex.php
@@ -4,10 +4,45 @@ include "Consultas.php";
 
 
 
-$sql ="SELECT Estatus,ID_H_O_D,Fecha_Apertura,COUNT(*) as CajasAbiertas FROM Cajas_POS WHERE Estatus='Abierta' AND ID_H_O_D='".$row['ID_H_O_D']."' AND Fecha_Apertura=CURRENT_DATE ";
+$sql ="SELECT COUNT(*) AS Total_Citas
+FROM (
+    SELECT
+        AgendaCitas_EspecialistasExt.ID_Agenda_Especialista,
+        Especialidades_Express.ID_Especialidad,
+        Especialidades_Express.Nombre_Especialidad,
+        Personal_Medico_Express.Medico_ID,
+        Personal_Medico_Express.Nombre_Apellidos,
+        SucursalesCorre.ID_SucursalC,
+        SucursalesCorre.Nombre_Sucursal,
+        Fechas_EspecialistasExt.ID_Fecha_Esp,
+        Fechas_EspecialistasExt.Fecha_Disponibilidad,
+        Horarios_Citas_Ext.ID_Horario,
+        Horarios_Citas_Ext.Horario_Disponibilidad,
+        AgendaCitas_EspecialistasExt.AgendadoPor,
+        AgendaCitas_EspecialistasExt.Nombre_Paciente,
+        AgendaCitas_EspecialistasExt.Telefono,
+        AgendaCitas_EspecialistasExt.Observaciones,
+        AgendaCitas_EspecialistasExt.Fecha_Hora
+    FROM
+        AgendaCitas_EspecialistasExt
+    LEFT JOIN
+        Especialidades_Express ON AgendaCitas_EspecialistasExt.Fk_Especialidad = Especialidades_Express.ID_Especialidad
+    LEFT JOIN
+        Personal_Medico_Express ON AgendaCitas_EspecialistasExt.Fk_Especialista = Personal_Medico_Express.Medico_ID
+    LEFT JOIN
+        SucursalesCorre ON AgendaCitas_EspecialistasExt.Fk_Sucursal = SucursalesCorre.ID_SucursalC
+    LEFT JOIN
+        Fechas_EspecialistasExt ON AgendaCitas_EspecialistasExt.Fecha = Fechas_EspecialistasExt.ID_Fecha_Esp
+    LEFT JOIN
+        Horarios_Citas_Ext ON AgendaCitas_EspecialistasExt.Hora = Horarios_Citas_Ext.ID_Horario
+    WHERE
+        AgendaCitas_EspecialistasExt.Fk_Especialidad IN (14, 15, 16, 17, 18, 19, 20, 65, 66, 67, 68, 76, 80, 81, 84, 85, 86, 87)
+        AND DATE(AgendaCitas_EspecialistasExt.Fecha_Hora) = CURDATE()
+) AS Todas_Las_Citas;
+";
 
 $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-$CajasAbiertas = mysqli_fetch_assoc($resultset);
+$PacientesGeneralDental = mysqli_fetch_assoc($resultset);
 
 $sql ="SELECT Fecha_venta,COUNT(*) Folio_Ticket FROM Ventas_POS WHERE  ID_H_O_D='".$row['ID_H_O_D']."' AND Fecha_venta=CURRENT_DATE";
 $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
@@ -18,20 +53,7 @@ $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($c
 $TotalGanancia = mysqli_fetch_assoc($resultset);
 
 
-$sql ="SELECT Estatus,ID_H_O_D,COUNT(*) as Farmaceuticos FROM `PersonalPOS` WHERE Fk_Usuario = 7 AND Estatus='Vigente' AND ID_H_O_D='".$row['ID_H_O_D']."'";
-$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-$TotalFarmaceuticos = mysqli_fetch_assoc($resultset);
 
-$sql ="SELECT Estatus,ID_H_O_D,COUNT(*) as Enfermeros FROM Personal_Enfermeria WHERE Fk_Usuario = 4 AND Estatus='Vigente' AND  ID_H_O_D='".$row['ID_H_O_D']."'";
-$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-$TotalEnfermeros = mysqli_fetch_assoc($resultset);
-
-$sql ="SELECT Estatus,ID_H_O_D,COUNT(*) as Medicos FROM Personal_Medico WHERE Estatus='Vigente' AND ID_H_O_D='".$row['ID_H_O_D']."'";
-$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-$TotalMedicos = mysqli_fetch_assoc($resultset);
-$sql ="SELECT Estatus,ID_H_O_D,COUNT(*) as Intendentes FROM Personal_Intendecia WHERE Estatus='Vigente' AND ID_H_O_D='".$row['ID_H_O_D']."'";
-$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
-$TotalLimpieza = mysqli_fetch_assoc($resultset);
 
 
 $sql ="SELECT Estatus,ID_H_O_D,COUNT(*) as TraspasosPendientes FROM Traspasos_generados WHERE Estatus='Generado' AND ID_H_O_D='".$row['ID_H_O_D']."'";
