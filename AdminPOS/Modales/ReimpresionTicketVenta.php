65ca16105bfdd7f844e5d96481f5f281ef00426b
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-11 16:42:33 -0600

    Update ReimpresionTicketVenta.php

--- AdminPOS/Modales/ReimpresionTicketVenta.php
+++ AdminPOS/Modales/ReimpresionTicketVenta.php
@@ -10,7 +10,7 @@ $sql1= "SELECT Ventas_POS.Folio_Ticket,Ventas_POS.Fk_Caja,Ventas_POS.Venta_POS_I
 Ventas_POS.Clave_adicional,Ventas_POS.Total_Venta,Ventas_POS.Importe,Ventas_POS.Total_VentaG,Ventas_POS.CantidadPago,
 Ventas_POS.Cambio,Servicios_POS.Servicio_ID,Servicios_POS.Nom_Serv, Ventas_POS.Nombre_Prod,Ventas_POS.Cantidad_Venta,Ventas_POS.
 Fk_sucursal,Ventas_POS.AgregadoPor,Ventas_POS.AgregadoEl, Ventas_POS.Lote,Ventas_POS.ID_H_O_D,SucursalesCorre.ID_SucursalC,SucursalesCorre.Nombre_Sucursal
- FROM Ventas_POS,SucursalesCorre,Servicios_POS WHERE Ventas_POS.Fk_sucursal= SucursalesCorre.ID_SucursalC   AND Ventas_POS.Folio_Ticket= '".$_POST["id"]."' AND
+ FROM Ventas_POS,SucursalesCorre,Servicios_POS WHERE Ventas_POS.Fk_sucursal= SucursalesCorre.ID_SucursalC   AND Ventas_POS.Folio_Ticket= '".$_POST["folioTicket"]."' AND
  Ventas_POS.FolioSucursal= '".$_POST["foliosucursal"]."' AND Ventas_POS.Identificador_tipo=Servicios_POS.Servicio_ID";
  
 
@@ -28,7 +28,7 @@ while ($r=$query->fetch_object()){
   Ventas_POS.Clave_adicional,Ventas_POS.Total_Venta,Ventas_POS.Importe,Ventas_POS.Total_VentaG,Ventas_POS.CantidadPago,Ventas_POS.Fecha_venta,
   Ventas_POS.Cambio,Servicios_POS.Servicio_ID,Servicios_POS.Nom_Serv, Ventas_POS.Nombre_Prod,Ventas_POS.Cantidad_Venta,Ventas_POS.
   Fk_sucursal,Ventas_POS.AgregadoPor,Ventas_POS.AgregadoEl, Ventas_POS.Lote,Ventas_POS.ID_H_O_D,SucursalesCorre.ID_SucursalC,SucursalesCorre.Nombre_Sucursal
-   FROM Ventas_POS,SucursalesCorre,Servicios_POS WHERE Ventas_POS.Fk_sucursal= SucursalesCorre.ID_SucursalC   AND Ventas_POS.Folio_Ticket= '".$_POST["id"]."' }
+   FROM Ventas_POS,SucursalesCorre,Servicios_POS WHERE Ventas_POS.Fk_sucursal= SucursalesCorre.ID_SucursalC   AND Ventas_POS.Folio_Ticket= '".$_POST["folioTicket"]."' }
    AND Ventas_POS.FolioSucursal= '".$_POST["foliosucursal"]."' AND Ventas_POS.Identificador_tipo=Servicios_POS.Servicio_ID";
    $query = $conn->query($sql2);
 ?>
