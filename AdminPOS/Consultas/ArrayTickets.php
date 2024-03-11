e5f5e0ed7938fc9646f049e70b8a7fe7821a6d2d
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-11 14:40:26 -0600

    Update ArrayTickets.php

--- AdminPOS/Consultas/ArrayTickets.php
+++ AdminPOS/Consultas/ArrayTickets.php
@@ -23,7 +23,7 @@ $meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"
 
 $sql = "SELECT
 Ventas_POS.Folio_Ticket,
-Ventas_POS.Folio_Sucursal,
+Ventas_POS.FolioSucursal,
 Ventas_POS.Fk_Caja,
 Ventas_POS.Venta_POS_ID,
 Ventas_POS.Identificador_tipo,
@@ -59,7 +59,7 @@ $c=0;
 while($fila=$result->fetch_assoc()){
   
     $data[$c]["NumberTicket"] = $fila["Folio_Ticket"];
-    $data[$c]["FolioSucursal"] = $fila["Folio_Sucursal"];
+    $data[$c]["FolioSucursal"] = $fila["FolioSucursal"];
     $data[$c]["Fecha"] = fechaCastellano($fila["AgregadoEl"]);
     
     $data[$c]["Hora"] = date("g:i:s a", strtotime($fila["AgregadoEl"]));
