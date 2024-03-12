e799de1a2f55d52ab65d14188379122217dd5024
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-12 12:15:31 -0600

    Update EditaValoresTickets.js

--- AdminPOS/js/EditaValoresTickets.js
+++ AdminPOS/js/EditaValoresTickets.js
@@ -1,64 +1,37 @@
-$('document').ready(function ($) {
-    $.validator.addMethod("Sololetras", function (value, element) {
-        return this.optional(element) || /[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]*$/.test(value);
-    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar letras!");
-    
-    $.validator.addMethod("Telefonico", function (value, element) {
-        return this.optional(element) || /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/.test(value);
-    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar numeros!");
-    
-    $.validator.addMethod("Correos", function (value, element) {
-        return this.optional(element) || /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(value);
-    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Ingresa un correo valido!");
-    
-    $.validator.addMethod("NEmpresa", function (value, element) {
-        return this.optional(element) || /^[\u00F1A-Za-z _]*[\u00F1A-Za-z][\u00F1A-Za-z _]*$/.test(value);
-    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar letras!");
-    
-    $.validator.addMethod("Problema", function (value, element) {
-        return this.optional(element) || /^[\u00F1A-Za-z _]*[\u00F1A-Za-z][\u00F1A-Za-z _]*$/.test(value);
-    }, "<i class='fas fa-exclamation-triangle' style='color:red'></i> Solo debes ingresar letras!");
+$(document).ready(function () {
+    $("#ActualizameLadatadelTicket").on("submit", function (event) {
+       
 
-    $("#ActualizameLadatadelTicket").validate({
-        rules: {
-            codbarras: {
-                required: true,
-            }
-        },
-        messages: {
-            codbarras: {
-                required: "<i class='fas fa-exclamation-triangle' style='color:red'></i> Dato requerido",
-            }
-        },
-        submitHandler: function () {
-            // Aquí puedes realizar las acciones que desees antes de enviar el formulario, como validar campos o realizar otras tareas.
-            // Luego, puedes enviar el formulario manualmente utilizando AJAX.
-            $.ajax({
-                type: 'POST',
-                url: "Consultas/ActualizaLasFormasDePago.php",
-                data: $('#ActualizameLadatadelTicket').serialize(), // Serializa los datos del formulario
-                cache: false,
-                beforeSend: function () {
-                    // Realiza acciones antes del envío, como mostrar un mensaje de carga
-                    $("#submit_registro").html("Verificando datos... <span class='fa fa-refresh fa-spin' role='status' aria-hidden='true'></span>");
-                },
-                success: function () {
-                    // Procesa la respuesta del servidor
-                    $('#Exito').modal('toggle');
-                    setTimeout(function () {
-                        $('#Exito').modal('hide');
-                    }, 2000); // abrir
-                    CargaCitasEnSucursalExt();
-                },
-                error: function () {
-                    // Acciones en caso de error de comunicación
-                    $("#show_error").fadeIn();
-                },
-                complete: function () {
-                    // Acciones después de que la solicitud se haya completado (éxito o error)
+        // Aquí puedes realizar las acciones que desees antes de enviar el formulario, como validar campos o realizar otras tareas.
+
+        // Luego, puedes enviar el formulario manualmente utilizando AJAX.
+        $.ajax({
+            type: 'POST',
+            url: "Consultas/ActualizaInfoDelTicket.php",
+            data: $(this).serialize(), // Serializa los datos del formulario
+            cache: false,
+            beforeSend: function () {
+                // Realiza acciones antes del envío, como mostrar un mensaje de carga
+            },
+            success: function (dataResult) {
+                // Procesa la respuesta del servidor
+                try {
+                    var response = JSON.parse(dataResult);
+                    if (response.status === 'success') {
+                        // Acciones en caso de éxito
+                    } else if (response.status === 'error') {
+                        // Acciones en caso de error
+                    }
+                } catch (error) {
+                    console.error("Error al parsear la respuesta JSON: " + error);
                 }
-            });
-            return false;
-        }
+            },
+            error: function () {
+                // Acciones en caso de error de comunicación
+            },
+            complete: function () {
+                // Acciones después de que la solicitud se haya completado (éxito o error)
+            }
+        });
     });
 });
