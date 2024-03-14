12d1e9c9a339ea89111bf0376694b7a06d153ca8
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-14 15:34:30 -0600

    Update Abonos.js

--- ControlDental/js/Abonos.js
+++ ControlDental/js/Abonos.js
@@ -1,7 +1,7 @@
 function CargaAbonos(){
 
 
-    $.post("https://saludapos.com/AdminPOS/Consultas/Abonos.php","",function(data){
+    $.post("https://saludapos.com/ControlDental/Consultas/Abonos.php","",function(data){
       $("#tablaAbonos").html(data);
     })
 
