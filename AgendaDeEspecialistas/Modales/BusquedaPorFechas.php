50a3b42a81fdd0d4d349f80b526166cf16f75a57
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-26 11:09:01 -0600

    AjusteEncitas

--- AgendaDeEspecialistas/Modales/BusquedaPorFechas.php
+++ AgendaDeEspecialistas/Modales/BusquedaPorFechas.php
@@ -1,4 +1,4 @@
-<div class="modal fade bd-example-modal-xl" id="CitasPorFechas" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+<div class="modal fade bd-example-modal-xl" id="CitasPorFechasBusqueda" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-notify modal-success">
         <div class="modal-content">
             <div class="text-center">
