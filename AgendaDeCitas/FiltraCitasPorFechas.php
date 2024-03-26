b15d618bf8a4ce8840028c0fcb63263e7932879b
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-26 15:32:45 -0600

    Update FiltraCitasPorFechas.php

--- AgendaDeCitas/FiltraCitasPorFechas.php
+++ AgendaDeCitas/FiltraCitasPorFechas.php
@@ -325,13 +325,7 @@ include "Consultas/Consultas.php";
 
   <!-- Modales y scripts -->
   <?php
-  include ("Modales/Error.php");
-  include ("Modales/Exito.php");
-  include ("Modales/ExitoActualiza.php");
-  include ("Modales/FiltraEspecificamente.php");
-  include ("Modales/FiltraFechasEspecialesVenta.php");
-  include ("Modales/FiltraPorMes.php");
-  include ("Modales/FiltroPorProducto.php");
+ 
   include ("footer.php");
   ?>
 
