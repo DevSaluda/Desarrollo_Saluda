3cfe623c2f531ae0fb361261a5a25777fe32427e
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-03-27 10:13:32 -0600

    Update AgendamientoDeCitasV3.php

--- AgendaDeCitas/AgendamientoDeCitasV3.php
+++ AgendaDeCitas/AgendamientoDeCitasV3.php
@@ -41,7 +41,12 @@ include "Consultas/Consultas.php";
 <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#FiltroEspecificoMesxd" style="background-color: #C80096 !important;" class="btn btn-default">
  Citas por fechas <i class="fas fa-calendar"></i>
 </button>
-
+<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#FiltroEspecificoMesxd" style="background-color: #C80096 !important;" class="btn btn-default">
+ Citas por sucursal <i class="fas fa-calendar"></i>
+</button>
+<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#FiltroEspecificoMesxd" style="background-color: #C80096 !important;" class="btn btn-default">
+ Citas por especialista <i class="fas fa-calendar"></i>
+</button>
 </div> <!-- Fin del container -->
 
 </div>
