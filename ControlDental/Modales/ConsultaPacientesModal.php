430a71347035d2df380ddf4912791f77df10af5c
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-04-02 15:40:14 -0600

    Update ConsultaPacientesModal.php

--- ControlDental/Modales/ConsultaPacientesModal.php
+++ ControlDental/Modales/ConsultaPacientesModal.php
@@ -63,8 +63,7 @@ AgendaCitas_EspecialistasExt.Fk_Especialidad = 84 OR
 AgendaCitas_EspecialistasExt.Fk_Especialidad = 85 OR
 AgendaCitas_EspecialistasExt.Fk_Especialidad = 86 OR
 AgendaCitas_EspecialistasExt.Fk_Especialidad = 87)
-AND DATE(AgendaCitas_EspecialistasExt.Fecha_Hora) = CURDATE();
-";
+AND DATE(AgendaCitas_EspecialistasExt.Fecha_Hora) = CURRENT_DATE();";
 $query = $conn->query($sql1);
 ?>
 
