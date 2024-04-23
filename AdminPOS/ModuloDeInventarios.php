16762a58548cb334b2dc9a3488a70908604951d1
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-04-23 09:10:13 -0600

    Update ModuloDeInventarios.php

--- AdminPOS/ModuloDeInventarios.php
+++ AdminPOS/ModuloDeInventarios.php
@@ -190,7 +190,7 @@ $(document).ready( function () {
     <div id="response" class="<?php if(!empty($type)) { echo $type . " display-block"; } ?>"><?php if(!empty($message)) { echo $message; } ?></div>
     
          
-<?php
+<!-- <?php
     $sqlSelect = "SELECT Inserciones_Excel_inventarios.Id_Insert, Inserciones_Excel_inventarios.Cod_Barra, Inserciones_Excel_inventarios.Nombre_prod, Inserciones_Excel_inventarios.Cantidad_Ajuste, Inserciones_Excel_inventarios.Sucursal, Inserciones_Excel_inventarios.Tipo_ajuste, Inserciones_Excel_inventarios.Agrego, Inserciones_Excel_inventarios.Fecha_registro,
     SucursalesCorre.ID_SucursalC,SucursalesCorre.Nombre_Sucursal FROM Inserciones_Excel_inventarios, SucursalesCorre WHERE Inserciones_Excel_inventarios.Sucursal = SucursalesCorre.ID_SucursalC";
     $result = mysqli_query($con, $sqlSelect);
@@ -243,7 +243,7 @@ if (mysqli_num_rows($result) > 0)
       
 
      
-  
+   -->
   <!-- /.content-wrapper -->
 
   <!-- Control Sidebar -->
