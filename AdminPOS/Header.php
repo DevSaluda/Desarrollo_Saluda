7bb2463ca9d5f5772f337ed50b773b72f99c4c95
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-04-24 16:39:47 -0600

    Update Header.php

--- AdminPOS/Header.php
+++ AdminPOS/Header.php
@@ -32,10 +32,12 @@
 
 
 
+<link rel="stylesheet" type="text/css" href="Componentes/datatables.min.css"/>
+  <script type="text/javascript" src="Componentes/datatables.min.js"></script>
+  <script src="Componentes/sweetalert2@9.js"></script>
   <link href="Componentes/mdb.min.css" rel="stylesheet">
   <script src="js/validation.min.js"></script><link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.js"></script>
+
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <link rel="stylesheet" href="Componentes/Multiselect/dist/css/bootstrap-multiselect.css" type="text/css">
 <script type="text/javascript" src="Componentes/Multiselect/dist/js/bootstrap-multiselect.js"></script>
@@ -43,4 +45,4 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <link rel="stylesheet" href="/path/to/select2.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css">
-
+<script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
