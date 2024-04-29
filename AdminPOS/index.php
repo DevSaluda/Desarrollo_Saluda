663f80954e63fc4d31087fdea371b61d4383f2fa
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-04-29 15:48:24 -0600

    Update index.php

--- AdminPOS/index.php
+++ AdminPOS/index.php
@@ -241,7 +241,7 @@ include "datatables.php";
 ?>
 
 <!-- Bootstrap -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 <!-- overlayScrollbars -->
 <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
