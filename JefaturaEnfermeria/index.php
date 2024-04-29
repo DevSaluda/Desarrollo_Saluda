ba51ca295d10392bc0fa46eeaf2d2be817124d32
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-04-29 09:48:20 -0600

    Include Modales

--- JefaturaEnfermeria/index.php
+++ JefaturaEnfermeria/index.php
@@ -78,7 +78,10 @@ include "Consultas/ContadorIndex.php";
   </div>
   <!-- /.container-fluid -->
 
-  <?php include ("footer.php");?>
+  <?php include ("footer.php");
+    include ("JefaturaEnfermeria/Modales/ModalEnfermetosVigentes.php");
+    include ("JefaturaEnfermeria/Modales/ModalSignosVitales.php");
+  ?>
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
