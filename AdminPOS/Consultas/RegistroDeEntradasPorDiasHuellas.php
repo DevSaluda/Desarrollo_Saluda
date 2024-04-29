0a09c7238c80ffcd36be93dedd89895315e945e2
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-04-29 15:20:52 -0600

    Update RegistroDeEntradasPorDiasHuellas.php

--- AdminPOS/Consultas/RegistroDeEntradasPorDiasHuellas.php
+++ AdminPOS/Consultas/RegistroDeEntradasPorDiasHuellas.php
@@ -262,30 +262,3 @@ tabla = $('#Productos').DataTable({
 </div>
 
 
-<?php
-function fechaCastellano ($fecha) {
-  $fecha = substr($fecha, 0, 10);
-  $numeroDia = date('d', strtotime($fecha));
-  $dia = date('l', strtotime($fecha));
-  $mes = date('F', strtotime($fecha));
-  $anio = date('Y', strtotime($fecha));
-  $dias_ES = array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
-  $dias_EN = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
-  $nombredia = str_replace($dias_EN, $dias_ES, $dia);
-$meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-  $meses_EN = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-  $nombreMes = str_replace($meses_EN, $meses_ES, $mes);
-  return $nombredia." ".$numeroDia." de ".$nombreMes." de ".$anio;
-}
-
-function convertirDecimalAHoraMinutosSegundos($decimalHoras) {
-  $horas = floor($decimalHoras);  // Parte entera: horas
-  $minutosDecimal = ($decimalHoras - $horas) * 60;  // Decimal a minutos
-  $minutos = floor($minutosDecimal);  // Parte entera: minutos
-  $segundosDecimal = ($minutosDecimal - $minutos) * 60;  // Decimal a segundos
-  $segundos = round($segundosDecimal);  // Redondear a segundos
-
-  return sprintf("%02d:%02d:%02d", $horas, $minutos, $segundos);  // Formatear como HH:MM:SS
-}
-
-?>
