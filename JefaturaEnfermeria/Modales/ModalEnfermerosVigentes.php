72bdc6ad42d7b6449ab7ee366786f0da95ebbe4c
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-04-29 09:45:46 -0600

    Update ModalEnfermerosVigentes.php

---
+++ JefaturaEnfermeria/Modales/ModalEnfermerosVigentes.php
@@ -0,0 +1,42 @@
+<script type="text/javascript">
+$(document).ready( function () {
+    $('#SignosVitales2').DataTable({
+      "order": [[ 0, "desc" ]],
+      "language": {
+        "url": "Componentes/Spanish.json"
+    }
+    
+    } 
+    
+    );
+} );
+</script>
+
+
+<!-- Central Modal Medium Info -->
+<div class="modal fade" id="EnferVigentes" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+   aria-="true" style="overflow-y: scroll;">
+   <div class="modal-dialog modal-xl modal-notify modal-success" role="document">
+     <!--Content-->
+     <div class="modal-content">
+       <!--Header-->
+       <div class="modal-header">
+         <p class="heading lead">Farmacéuticos Vigentes</p>
+
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+           <span aria-="true" class="white-text">&times;</span>
+         </button>
+       </div>
+     
+       <!--Body-->
+       <div class="modal-body">
+         <div class="text-center">
+        
+  
+     </div>
+     <!--/.Content-->
+   </div>
+ </div>
+ </div>
+ </div>
+
