c57fa97c7a2757ffc7446a3e3edd14fa3aaaf9e1
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-04-29 16:05:30 -0600

    Update index.php

--- AdminPOS/index.php
+++ AdminPOS/index.php
@@ -189,12 +189,12 @@ Registros del reloj checador de <?php echo $row['ID_H_O_D']?> al <?php echo Fech
 
 </div>
 
-<div id="resultados_div" class="table-responsive"></div></div></div>
+<div id="resultados_div" class="table-responsive"></div>
 
 <script>
     $(document).ready(function(){
         $.ajax({
-            url: 'Consultas/consulta_api.php',
+            url: 'consulta_api.php',
             type: 'GET',
             dataType: 'json',
             success: function(data){
@@ -216,8 +216,24 @@ Registros del reloj checador de <?php echo $row['ID_H_O_D']?> al <?php echo Fech
                 html += '</tbody></table>';
                 $('#resultados_div').html(html);
 
-                // Inicializar Datatables
-                $('#resultados_tabla').DataTable();
+                // Inicializar Datatables con los estilos
+                $('#resultados_tabla').DataTable({
+                    "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+                    "language": {
+                        "lengthMenu": "Mostrar _MENU_ registros por página",
+                        "zeroRecords": "No se encontraron resultados",
+                        "info": "Mostrando página _PAGE_ de _PAGES_",
+                        "infoEmpty": "No hay registros disponibles",
+                        "infoFiltered": "(filtrados de _MAX_ registros totales)",
+                        "search": "Buscar:",
+                        "paginate": {
+                            "first": "Primero",
+                            "last": "Último",
+                            "next": "Siguiente",
+                            "previous": "Anterior"
+                        }
+                    }
+                });
             },
             error: function(xhr, status, error){
                 console.error(error);
@@ -225,6 +241,7 @@ Registros del reloj checador de <?php echo $row['ID_H_O_D']?> al <?php echo Fech
         });
     });
 </script>
+
 
             
          
