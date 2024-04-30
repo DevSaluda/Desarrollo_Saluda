168ef46b1b97453e70354c7d28fde3482aac4366
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-04-30 10:37:54 -0600

    Update Header.php

--- AdminPOS/Header.php
+++ AdminPOS/Header.php
@@ -8,14 +8,13 @@
   <script src="Componentes/jquery-3.5.1.slim.min.js"></script>
 <script src="Componentes/jquery-3.5.0.js"></script>
 
-  
-  <link rel="stylesheet" href="Componentes/bootstrap.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   
 
   <!-- JS, Popper.js, and jQuery -->
 
-  <!-- <script src="Componentes/popper.min.js"></script>
-  <script src="Componentes/bootstrap.min.js"></script> -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   
   <!-- JS, Popper.js, and jQuery -->
   
