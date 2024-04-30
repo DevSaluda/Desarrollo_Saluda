7157b37c58f7544234a7ef2d134a4dda64ff1f9b
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-04-30 10:41:01 -0600

    Update Header.php

--- AdminPOS/Header.php
+++ AdminPOS/Header.php
@@ -9,7 +9,7 @@
 <script src="Componentes/jquery-3.5.0.js"></script>
 
   <!-- Google Font: Source Sans Pro -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="Componentes/bootstrap.min.css">
   
 
   <!-- JS, Popper.js, and jQuery -->
