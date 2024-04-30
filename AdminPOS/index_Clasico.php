3aae480d8c1a07f9be951ba59e3432931c4b38f5
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-04-30 09:47:20 -0600

    Update index_Clasico.php

--- AdminPOS/index_Clasico.php
+++ AdminPOS/index_Clasico.php
@@ -1,22 +1,6 @@
 <?php
 
-  # Iniciando la variable de control que permitirá mostrar o no el modal
-  $exibirModal = false;
-  # Verificando si existe o no la cookie
-  if(!isset($_COOKIE["mostrarModal"]))
-  {
-    # Caso no exista la cookie entra aqui
-    # Creamos la cookie con la duración que queramos
-     
-    //$expirar = 3600; // muestra cada 1 hora
-    //$expirar = 10800; // muestra cada 3 horas
-    //$expirar = 21600; //muestra cada 6 horas
-    $expirar = 43200; //muestra cada 12 horas
-    //$expirar = 86400;  // muestra cada 24 horas
-    setcookie('mostrarModal', 'SI', (time() + $expirar)); // mostrará cada 12 horas.
-    # Ahora nuestra variable de control pasará a tener el valor TRUE (Verdadero)
-    $exibirModal = true;
-  }
+
   include "Consultas/Consultas.php";
   include "Consultas/ContadorIndex.php";
 
@@ -29,27 +13,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title><?php echo $row['ID_H_O_D']?> PUNTO DE VENTA  </title>
+  <title><?php echo $row['ID_H_O_D']?> INICIO  </title>
 
   <!-- Font Awesome Icons -->
   <?php include "Header.php"?>
 </head>
 <?php include_once ("Menu.php")?>
 
-<?php
-
-function fechaCastellano ($fecha) {
-  $fecha = substr($fecha, 0, 10);
-  $numeroDia = date('d', strtotime($fecha));
-  $dia = date('l', strtotime($fecha));
-  $mes = date('F', strtotime($fecha));
-  $anio = date('Y', strtotime($fecha));
-  $dias_ES = array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
-  $dias_EN = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
-  $nombredia = str_replace($dias_EN, $dias_ES, $dia);
-$meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-  $meses_EN = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-  $nombreMes = str_replace($meses_EN, $meses_ES, $mes);
-  return $nombredia." ".$numeroDia." de ".$nombreMes." de ".$anio;
-}
-?>
