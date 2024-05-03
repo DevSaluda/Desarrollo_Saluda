d32f090e4d77b764f39e9b5cb70f3b20471b57a4
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-05-03 08:02:17 -0500

    Cambio en el footer para permitir el  modal salida

--- JefaturaEnfermeria/footer.php
+++ JefaturaEnfermeria/footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Derechos reservados.&copy; 2020 <a href="https://somosgrupoe.com/">Somos Grupo E</a>.</strong>
+    <!-- <strong>Derechos reservados.&copy; 2020 <a href="https://somosgrupoe.com/">Somos Grupo E</a>.</strong> -->
   
     <div class="float-right d-none d-sm-inline-block">
     CONTROL CONSULTA | Version 2.0 |
@@ -8,7 +8,7 @@
     </div>
   </footer>
 </div>
-<?
+<?php
 include ("Modales/Salidas.php");?>
 <script src="js/LogSalida.js"></script>
 <script src="https://controlconsulta.com/Enfermeria2/js/Refresca.js"></script>
