f7a9cfcc379e94a050e1a73ba7b2ed4868d137ef
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-05-06 16:19:15 -0600

    Update AltaPromosCred.php

--- ControlDental/Modales/AltaPromosCred.php
+++ ControlDental/Modales/AltaPromosCred.php
@@ -13,13 +13,13 @@
          </button>
        </div>
         <div class="alert alert-success alert-styled-left text-blue-800 content-group">
-						                <span class="text-semibold"><?echo $row['Nombre_Apellidos']?>, </span>
-                            los campos con un  <span class="text-danger"> * </span> son campos necesarios para el correcto ingreso de datos.
+						                <span class="text-semibold"><?echo $row['Nombre_Apellidos']?> </span>
+                            Los campos con un  <span class="text-danger"> * </span> son campos necesarios para el correcto ingreso de datos.
                           
 						                <button type="button" class="close" data-dismiss="alert">×</button>
                             </div>
       <div class="modal-body">
-     
+
  <form action="javascript:void(0)" method="post" id="AgregaPromoCreditos">
     
  
@@ -58,16 +58,16 @@
   
     <span class="input-group-text" id="Tarjeta"><i class="fas fa-list-ol"></i></span>
   </div>
-  <select id = "tratamiento" class = "form-control" name = "Tratamiento">
+<select name="Tratamiento" class="form-control" id="tratamiento">
     <option value="">Seleccione un tratamiento:</option>
-        <?
-          $query = $conn -> query ("SELECT 	ID_Tip_Cred,Nombre_Tip FROM Tipos_Credit_POS WHERE  ID_H_O_D='".$row['ID_H_O_D']."' AND Estatus='Vigente'");
-          while ($valores = mysqli_fetch_array($query)) {
-            echo '<option value="'.$valores[ID_Tip_Cred].'">'.$valores[Nombre_Tip].'</option>';
-          }
-        ?>  </select>
-             </div>
-    </div>
+    <?php 
+    $query = $conn->query("SELECT ID_Tip_Cred, Nombre_Tip, ID_H_O_D FROM Tipos_Credit_POS WHERE ID_H_O_D='".$row['ID_H_O_D']."' AND Estatus='Vigente'");
+    while ($valores = mysqli_fetch_array($query)) {
+        echo '<option value="'.$valores["ID_Tip_Cred"].'">'.$valores["Nombre_Tip"].'</option>';
+    }
+    ?>
+</select>
+      </div>
       
     <div class="col">
 <label for="exampleFormControlInput1">Vigencia<span class="text-danger">*</span></label>
@@ -111,7 +111,7 @@
 </div>
 <input type="text" class="form-control " hidden  readonly name="UsuarioPromo" id="usuarioPromo"  readonly value="<?echo $row['Nombre_Apellidos']?>">
 <input type="text"  class="form-control " hidden  readonly name="VigenciaEstPromo" id="vigenciaestPromo">
-<input type="text" class="form-control "  hidden  readonly id="sistemaPromo" name="SistemaPromo" readonly value="POS <?echo $row['Nombre_rol']?>">
+<input type="text" class="form-control "  hidden  readonly id="sistemaPromo" name="SistemaPromo" readonly value=" <?echo $row['Nombre_rol']?>">
 <input type="text" class="form-control "  hidden id="empresaPromo" name="EmpresaPromo" readonly value="<?echo $row['ID_H_O_D']?>">
   <div>
    
