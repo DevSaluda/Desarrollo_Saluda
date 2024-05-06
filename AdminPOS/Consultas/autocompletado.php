3d3246fc980a9513846bbf94397a8eb39bcf28ab
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-05-06 14:47:28 -0600

    Update autocompletado.php

--- AdminPOS/Consultas/autocompletado.php
+++ AdminPOS/Consultas/autocompletado.php
@@ -1,21 +1,15 @@
 <?php
-// Conecta con la base de datos y realiza la consulta para obtener los resultados de autocompletado
-$dbHost = 'localhost'; // Cambia esto por la dirección de tu servidor de base de datos
-$dbUsername = 'u155356178_SaludDevCenter'; // Cambia esto por tu nombre de usuario de la base de datos
-$dbPassword = 'uE;bAISz;*6c|I4PvEnfSys324\Zavp2zJ:9TLx{]L&QMcmhAdmSCDBSN3iH4UV3D24WMF@2024myV>'; // Cambia esto por tu contraseña de la base de datos
-$dbName = 'u155356178_saludapos'; // Cambia esto por el nombre de tu base de datos
-
-$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
-
-if (!$conn) {
-  die('Error de conexión: ' . mysqli_connect_error());
-}
+include_once "db_connection.php";
+include_once "Consultas.php";
 
 // Obtén el término de búsqueda enviado desde el campo de autocompletado
 $term = $_GET['term'];
 
-// Realiza la consulta utilizando el término de búsqueda y DISTINCT
-$query = "SELECT DISTINCT Cod_Barra, Nombre_Prod FROM Stock_POS WHERE Cod_Barra LIKE '%{$term}%' OR Nombre_Prod LIKE '%{$term}%'";
+// Obtén la sucursal de la variable $row['Fk_Sucursal']
+$sucursalbusqueda = $row['Fk_Sucursal'];
+
+// Realiza la consulta utilizando el término de búsqueda, la sucursal y DISTINCT
+$query = "SELECT DISTINCT Cod_Barra, Nombre_Prod FROM Stock_POS WHERE (Cod_Barra LIKE '%{$term}%' OR Nombre_Prod LIKE '%{$term}%') AND Fk_Sucursal = '{$sucursalbusqueda}'";
 $result = mysqli_query($conn, $query);
 
 // Genera un array con los resultados de autocompletado
