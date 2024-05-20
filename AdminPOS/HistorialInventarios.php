038f73e5eed1941fddc5a3e9d3d5fb73a32c4b37
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-05-20 09:59:06 -0600

    Update HistorialInventarios.php

--- AdminPOS/HistorialInventarios.php
+++ AdminPOS/HistorialInventarios.php
@@ -42,7 +42,7 @@ include "Consultas/Consultas.php";
 <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
 <div class="card text-center">
   <div class="card-header" style="background-color:#0057b8 !important;color: white;">
-    Productos de <?php echo $row['ID_H_O_D']?> al <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>  
+ Resultados de conteo de inventario <?php echo $row['ID_H_O_D']?> al <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>  
   </div>
   
   <div >
