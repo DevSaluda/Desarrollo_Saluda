d08cd5f55d4d23bfc2ab565c43b096602e1590ca
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-05-20 10:19:49 -0600

    Update ArrayResultadosInventariosContabilizados.php

--- AdminPOS/Consultas/ArrayResultadosInventariosContabilizados.php
+++ AdminPOS/Consultas/ArrayResultadosInventariosContabilizados.php
@@ -3,36 +3,41 @@ header('Content-Type: application/json');
 include("db_connection.php");
 include "Consultas.php";
 
+// Obtén la fecha actual en formato YYYY-MM-DD
+$today = date('Y-m-d');
 
+// Define la consulta SQL con la variable de fecha
 $sql = "SELECT 
-ic.Cod_Barra, 
-ic.Nombre_Prod, 
-sc.Nombre_Sucursal AS Nombre_Sucursal,
-ic.Precio_Venta,
-ic.Precio_C,
-ic.Precio_Venta * ic.Contabilizado AS Total_Precio_Venta, 
-ic.Precio_C * ic.Contabilizado AS Total_Precio_Compra, 
-ic.Contabilizado, 
-ic.StockEnMomento, 
-ic.Diferencia, 
-ic.Sistema, 
-ic.AgregadoPor, 
-ic.AgregadoEl, 
-ic.ID_H_O_D, 
-ic.FechaInventario
+    ic.Cod_Barra, 
+    ic.Nombre_Prod, 
+    sc.Nombre_Sucursal AS Nombre_Sucursal,
+    ic.Precio_Venta,
+    ic.Precio_C,
+    ic.Precio_Venta * ic.Contabilizado AS Total_Precio_Venta, 
+    ic.Precio_C * ic.Contabilizado AS Total_Precio_Compra, 
+    ic.Contabilizado, 
+    ic.StockEnMomento, 
+    ic.Diferencia, 
+    ic.Sistema, 
+    ic.AgregadoPor, 
+    ic.AgregadoEl, 
+    ic.ID_H_O_D, 
+    ic.FechaInventario
 FROM 
-InventariosStocks_Conteos AS ic
+    InventariosStocks_Conteos AS ic
 JOIN 
-SucursalesCorre AS sc ON ic.Fk_sucursal = sc.ID_SucursalC
+    SucursalesCorre AS sc ON ic.Fk_sucursal = sc.ID_SucursalC
 WHERE 
-CAST(ic.FechaInventario AS DATE) = CAST(GETDATE() AS DATE);
+    CAST(ic.FechaInventario AS DATE) = '$today';
 ";
- 
+
+// Ejecuta la consulta
 $result = mysqli_query($conn, $sql);
- 
-$c=0;
- 
-while($fila=$result->fetch_assoc()){
+
+$data = [];  // Asegúrate de inicializar el array $data
+$c = 0;
+
+while ($fila = $result->fetch_assoc()) {
     $data[$c]["IdbD"] = $fila["Cod_Barra"];
     $data[$c]["Cod_Barra"] = $fila["Nombre_Prod"];
     $data[$c]["NombreSucursal"] = $fila["Nombre_Sucursal"];
@@ -40,23 +45,23 @@ while($fila=$result->fetch_assoc()){
     $data[$c]["PrecioCompra"] = $fila["Precio_C"];
     $data[$c]["TotalPrecioVenta"] = $fila["Total_Precio_Venta"];
     $data[$c]["TotalPrecioCompra"] = $fila["Total_Precio_Compra"];
-    
     $data[$c]["Nombre_Prod"] = $fila["Contabilizado"];
     $data[$c]["Clave_interna"] = $fila["StockEnMomento"];
     $data[$c]["Clave_Levic"] = $fila["Diferencia"];
     $data[$c]["Cod_Enfermeria"] = $fila["AgregadoPor"];
     $data[$c]["FechaInventario"] = $fila["FechaInventario"];
-   
-   
-   
-    $c++; 
- 
+    $c++;
 }
- 
-$results = ["sEcho" => 1,
-            "iTotalRecords" => count($data),
-            "iTotalDisplayRecords" => count($data),
-            "aaData" => $data ];
- 
+
+$results = [
+    "sEcho" => 1,
+    "iTotalRecords" => count($data),
+    "iTotalDisplayRecords" => count($data),
+    "aaData" => $data
+];
+
 echo json_encode($results);
+
+// Cierra la conexión
+$conn->close();
 ?>
