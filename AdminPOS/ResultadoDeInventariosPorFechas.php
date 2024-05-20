103564fe73ca22983cc3a9fa3552366290d280c7
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-05-20 14:51:29 -0600

    Ajustes

--- AdminPOS/ResultadoDeInventariosPorFechas.php
+++ AdminPOS/ResultadoDeInventariosPorFechas.php
@@ -219,7 +219,7 @@ include "Consultas/Consultas.php";
             "order": [[ 0, "desc" ]],
             "ajax": {
               "type": "POST", // Especifica el método de envío de la solicitud AJAX
-              "url": "https://saludapos.com/AdminPOS/Consultas/ArrayTraspasosPorFechas.php",
+              "url": "https://saludapos.com/AdminPOS/Consultas/ArrayResultadosConteoInventarios.php",
               "data": function (d) {
         // Aquí puedes definir el código PHP directamente
         var mes = '<?php echo $mes; ?>'; // Obtén el valor de mes desde PHP
