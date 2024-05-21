dc07bfb83a54daea2567f292bfb4007225f834f0
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-05-21 15:55:15 -0600

    Update Menu.php

--- AdminPOS/Menu.php
+++ AdminPOS/Menu.php
@@ -1004,7 +1004,7 @@
           </li>
           <li class="nav-item has-treeview">
             <a href="HistorialInventarios" class="nav-link">
-            <i class="fas fa-boxes"></i>
+            <i class="fa-solid fa-calendar-minus"></i>
               <p>
         Prontos a caducar
                
