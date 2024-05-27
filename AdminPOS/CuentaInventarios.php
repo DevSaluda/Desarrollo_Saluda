daa1c444b2ffa09976f8e99cc985817916318d76
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-05-27 11:50:11 -0600

    Update CuentaInventarios.php

--- AdminPOS/CuentaInventarios.php
+++ AdminPOS/CuentaInventarios.php
@@ -314,7 +314,7 @@ $fechaActual = date('Y-m-d'); // Esto obtiene la fecha actual en el formato 'Añ
 
 <script>
 document.addEventListener("DOMContentLoaded", function() {
-  var fkSucursal = <?php echo $row['Fk_Sucursal'] ?>
+  var fkSucursal = <?php echo $row['Fk_Sucursal'] ?>;
     document.getElementById("Tipodeajuste").addEventListener("change", function() {
         var selectedOption = this.value;
         if (selectedOption === "Inventario inicial") {
