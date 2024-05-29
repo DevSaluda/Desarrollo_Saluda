c94ea42374486825ff0f3f44b9fb696b82633019
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-05-29 10:35:16 -0600

    Update GeneraPedidoPorDia.php

--- POS2/Modales/GeneraPedidoPorDia.php
+++ POS2/Modales/GeneraPedidoPorDia.php
@@ -45,6 +45,6 @@
     </div>
   </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGaTmrJ3HivW2UG0pE8E2J/Uf3" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
