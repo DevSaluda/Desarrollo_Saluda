483c815f33408f73eed130e109e5b62d6bd16cd6
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-05-29 11:17:30 -0600

    Update GeneraPedidoPorDia.php

--- POS2/Modales/GeneraPedidoPorDia.php
+++ POS2/Modales/GeneraPedidoPorDia.php
@@ -11,18 +11,33 @@
         </div>
         
         <div class="modal-body">
+          <div class="icon-container mb-4">
+            <i class="fas fa-store fa-5x animated bounceIn"></i>
+          </div>
+          
           <form action="javascript:void(0)" method="post" id="Filtrapormediodesucursalconajax">
             
+            <div class="row mb-4">
+              <div class="col">
+                <label for="exampleFormControlInput1">Sucursal Actual</label>
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="Tarjeta"><i class="far fa-hospital"></i></span>
+                  </div>
+                  <input type="text" class="form-control" disabled readonly value="<?php echo $row['Nombre_Sucursal']?>">
+                </div>
+              </div>
+            </div>
             
             <div class="row mb-4">
               <div class="col text-center">
-                <p class="h5">¿Deseas generar tu orden de prepedido?</p>
+                <p class="h4 animated fadeIn">¿Deseas generar tu orden de prepedido?</p>
               </div>
             </div>
-            <input type="text" hidden class="form-control" disabled readonly value="<?php echo $row['Nombre_Sucursal']?>">
+            
             <div class="row">
               <div class="col text-center">
-                <button type="submit" id="submit_registroarea" value="Guardar" class="btn btn-success">Generar <i class="fas fa-exchange-alt ml-2"></i></button>
+                <button type="submit" id="submit_registroarea" value="Guardar" class="btn btn-success btn-lg animated rubberBand">Generar <i class="fas fa-exchange-alt ml-2"></i></button>
               </div>
             </div>
             
