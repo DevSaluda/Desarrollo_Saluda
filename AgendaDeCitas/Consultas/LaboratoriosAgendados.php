cf776f25667310167cfc103b1f6e2c2fd7872f18
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-05-29 15:10:11 -0600

    Update LaboratoriosAgendados.php

--- AgendaDeCitas/Consultas/LaboratoriosAgendados.php
+++ AgendaDeCitas/Consultas/LaboratoriosAgendados.php
@@ -32,7 +32,7 @@
         <tbody>
           <!-- Aquí se generarán las filas de la tabla con datos dinámicos -->
           <?php
-          function fechaCastellano ($fecha) {
+          function fechaCastellano($fecha) {
             $fecha = substr($fecha, 0, 10);
             $numeroDia = date('d', strtotime($fecha));
             $dia = date('l', strtotime($fecha));
@@ -43,8 +43,8 @@
             $nombredia = str_replace($dias_EN, $dias_ES, $dia);
             $meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
             $meses_EN = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-            $nombreMes = str_replace($meses_EN, $mes);
-            return $nombredia." ".$numeroDia." de ".$nombreMes." de ".$anio;
+            $nombreMes = str_replace($meses_EN, $meses_ES, $mes);
+            return $nombredia . " " . $numeroDia . " de " . $nombreMes . " de " . $anio;
           }
 
           // Incluye el código PHP para obtener los datos de la base de datos
