21f7513ae85af58dac4de3a5b027859d01e59d1f
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-06-03 14:41:43 -0600

    Update ExportToExcel.php

--- AdminPOS/Consultas/ExportToExcel.php
+++ AdminPOS/Consultas/ExportToExcel.php
@@ -75,7 +75,6 @@ if (!$resultado) {
 // Crear un nuevo archivo CSV
 $filename = 'registro_de_ventas_del_' . str_replace('-', '_', $mes) . '_al_' . str_replace('-', '_', $anual) . '.csv';
 $output = fopen($filename, 'w');
-stream_filter_append($output, 'convert.iconv.UTF-8/ISO-8859-1', STREAM_FILTER_WRITE);
 $header = [
     "Cod_Barra",
     "Nombre_Prod",
@@ -103,8 +102,8 @@ fputcsv($output, $header);
 // Escribir los datos en el archivo CSV
 while ($row = $resultado->fetch_assoc()) {
     $data = [
-        utf8_decode($row["Cod_Barra"]),
-        utf8_decode($row["Nombre_Prod"]),
+        $row["Cod_Barra"],
+        $row["Nombre_Prod"],
         $row["Precio_C"],
         $row["Precio_Venta"],
         $row["FolioSucursal"] . ' ' . $row["Folio_Ticket"],
@@ -120,7 +119,7 @@ while ($row = $resultado->fetch_assoc()) {
         $row["Nom_Serv"],
         date("d/m/Y", strtotime($row["Fecha_venta"])),
         $row["AgregadoEl"],
-        utf8_decode($row["AgregadoPor"]),
+        $row["AgregadoPor"],
         $row["EnfermeroEnturno"],
         $row["MedicoEnturno"]
     ];
@@ -131,7 +130,7 @@ while ($row = $resultado->fetch_assoc()) {
 fclose($output);
 
 // Descargar el archivo CSV
-header('Content-Type: text/csv; charset=ISO-8859-1');
+header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="' . $filename . '"');
 readfile($filename);
 
