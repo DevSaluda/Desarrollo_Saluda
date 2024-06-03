98fedca47299f9ee288a0c83487dabbe6f6a1bf4
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-06-03 11:42:36 -0600

    Update FiltroPorFechasVentas.php

--- AdminPOS/FiltroPorFechasVentas.php
+++ AdminPOS/FiltroPorFechasVentas.php
@@ -292,17 +292,21 @@ include "Consultas/Consultas.php";
               ocultarCargando();
             },
             "buttons": [
-              {
-  text: 'Exportar a Excel <i class="fas fa-file-excel"></i>',
-  titleAttr: 'Exportar a Excel',
-  title: 'registro de ventas',
-  className: 'btn btn-success',
-  action: function(e, dt, button, config) {
-    window.location.href = 'https://saludapos.com/AdminPOS/Consultas/ExportToExcel.php?Mes=' + mes + '&anual=' + anual;
-  }
-}
-
-            ],
+      {
+        text: 'Exportar a Excel <i class="fas fa-file-excel"></i>',
+        titleAttr: 'Exportar a Excel',
+        className: 'btn btn-success',
+        action: function(e, dt, button, config) {
+          var form = $('<form method="POST" action="https://saludapos.com/AdminPOS/Consultas/ExportToExcel.php"></form>');
+          var mesInput = $('<input type="hidden" name="Mes" value="' + mes + '">');
+          var anualInput = $('<input type="hidden" name="anual" value="' + anual + '">');
+          form.append(mesInput);
+          form.append(anualInput);
+          $('body').append(form);
+          form.submit();
+        }
+      }
+    ],
             "dom": '<"d-flex justify-content-between"lBf>rtip', // Modificar la disposición aquí
             "responsive": true
           });
