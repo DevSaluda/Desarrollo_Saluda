06b0bf96e00759558711cb88cec6149127ef4f91
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-06-07 10:56:45 -0600

    AjustesTraspasos

--- AdminPOS/ListadoDeTraspasos.php
+++ AdminPOS/ListadoDeTraspasos.php
@@ -89,12 +89,7 @@ include("footer.php") ?>
 <script src="js/ListaDeTraspasos.js"></script>
 
 
-<script src="datatables/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>
-<script src="datatables/JSZip-2.5.0/jszip.min.js"></script>
-<script src="datatables/pdfmake-0.1.36/pdfmake.min.js"></script>
-<script src="datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
-<script src="datatables/Buttons-1.5.6/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
+<?php include "datatables.php"?>
 
 
 <!-- Bootstrap -->
