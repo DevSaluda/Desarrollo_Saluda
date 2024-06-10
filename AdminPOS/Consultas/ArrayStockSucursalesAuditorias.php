531605adf09c3dcfb3094ffbfe6323217588a092
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-06-10 12:46:47 -0600

    Update movimientos Stock

--- AdminPOS/Consultas/ArrayStockSucursalesAuditorias.php
+++ AdminPOS/Consultas/ArrayStockSucursalesAuditorias.php
@@ -5,14 +5,46 @@ include("db_connection.php");
 include "Consultas.php";
 
 
-$sql = "SELECT Stock_POS.Folio_Prod_Stock,Stock_POS.Clave_adicional,Stock_POS.ID_Prod_POS,Stock_POS.AgregadoEl,Stock_POS.Clave_adicional,Stock_POS.Clave_Levic,
-Stock_POS.Cod_Barra,Stock_POS.Nombre_Prod,Stock_POS.Tipo_Servicio,Stock_POS.Tipo,Stock_POS.Fk_sucursal,
-Stock_POS.Max_Existencia,Stock_POS.Min_Existencia, Stock_POS.Existencias_R,Stock_POS.Proveedor1,
-Stock_POS.Proveedor2,Stock_POS.CodigoEstatus,Stock_POS.Estatus,Stock_POS.ID_H_O_D, SucursalesCorre.ID_SucursalC,
-SucursalesCorre.Nombre_Sucursal,Servicios_POS.Servicio_ID,Servicios_POS.Nom_Serv, Productos_POS.ID_Prod_POS,
-Productos_POS.Precio_Venta,Productos_POS.Precio_C FROM Stock_POS,SucursalesCorre,Servicios_POS,Productos_POS WHERE 
-Stock_POS.Fk_Sucursal = SucursalesCorre.ID_SucursalC AND Stock_POS.Tipo_Servicio= Servicios_POS.Servicio_ID AND Productos_POS.ID_Prod_POS =Stock_POS.ID_Prod_POS AND
- Stock_POS.ID_H_O_D ='".$row['ID_H_O_D']."' AND Stock_POS.Fk_Sucursal='".$row['Fk_Sucursal']."'";
+$sql = "SELECT 
+    Stock_POS_Audita.Folio_Prod_Stock,
+    Stock_POS_Audita.Clave_adicional,
+    Stock_POS_Audita.ID_Prod_POS,
+    Stock_POS_Audita.AgregadoEl,
+    Stock_POS_Audita.Clave_adicional,
+    Stock_POS_Audita.Clave_Levic,
+    Stock_POS_Audita.Cod_Barra,
+    Stock_POS_Audita.Nombre_Prod,
+    Stock_POS_Audita.Tipo_Servicio,
+    Stock_POS_Audita.Tipo,
+    Stock_POS_Audita.Fk_sucursal,
+    Stock_POS_Audita.Max_Existencia,
+    Stock_POS_Audita.Min_Existencia, 
+    Stock_POS_Audita.Existencias_R,
+    Stock_POS_Audita.Proveedor1,
+    Stock_POS_Audita.Proveedor2,
+    Stock_POS_Audita.CodigoEstatus,
+    Stock_POS_Audita.Estatus,
+    Stock_POS_Audita.ID_H_O_D, 
+    SucursalesCorre.ID_SucursalC,
+    SucursalesCorre.Nombre_Sucursal,
+    Servicios_POS.Servicio_ID,
+    Servicios_POS.Nom_Serv, 
+    Productos_POS.ID_Prod_POS,
+    Productos_POS.Precio_Venta,
+    Productos_POS.Precio_C 
+FROM 
+    Stock_POS_Audita,
+    SucursalesCorre,
+    Servicios_POS,
+    Productos_POS 
+WHERE 
+    Stock_POS_Audita.Fk_Sucursal = SucursalesCorre.ID_SucursalC 
+    AND Stock_POS_Audita.Tipo_Servicio = Servicios_POS.Servicio_ID 
+    AND Productos_POS.ID_Prod_POS = Stock_POS_Audita.ID_Prod_POS 
+    AND Stock_POS_Audita.ID_H_O_D = '".$row['ID_H_O_D']."' 
+    AND Stock_POS_Audita.Fk_Sucursal = '".$row['Fk_Sucursal']."'
+    AND MONTH(Stock_POS_Audita.AgregadoEl) = MONTH(CURDATE())
+    AND YEAR(Stock_POS_Audita.AgregadoEl) = YEAR(CURDATE())";
  
 $result = mysqli_query($conn, $sql);
  
