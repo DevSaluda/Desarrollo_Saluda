f763e05f72db2de75efff1a449544527acb90c27
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-06-20 14:20:49 -0600

    Update ArrayProductos.php

--- AdminPOS/Consultas/ArrayProductos.php
+++ AdminPOS/Consultas/ArrayProductos.php
@@ -4,11 +4,11 @@ include("db_connection.php");
 include "Consultas.php";
 
 
-$sql = "SELECT Productos_POS.ID_Prod_POS,Productos_POS.Nombre_Prod,Productos_POS.Cod_Barra,Productos_POS.Cod_Enfermeria,Productos_POS.Proveedor1,Productos_POS.Proveedor2,
+$sql = "SELECT Productos_POS.ID_Prod_POS,Productos_POS.Nombre_Prod,Productos_POS.Cod_Barra,Productos_POS.Cod_Enfermeria,Productos_POS.Proveedor1,Productos_POS.Proveedor2,Productos_POS.Estatus,
 Productos_POS.ID_H_O_D,Productos_POS.Clave_adicional,Productos_POS.Clave_Levic,Productos_POS.FkMarca,Productos_POS.FkCategoria,Productos_POS.FkPresentacion,Productos_POS.Tipo,
 Productos_POS.Precio_Venta,Productos_POS.Precio_C,Productos_POS.Stock,Productos_POS.Saldo,Productos_POS.AgregadoPor,Productos_POS.Vendido,Productos_POS.Tipo_Servicio,
 Servicios_POS.Servicio_ID,Servicios_POS.Nom_Serv,Productos_POS.AgregadoEl FROM Productos_POS,Servicios_POS where 
-Servicios_POS.Servicio_ID = Productos_POS.Tipo_Servicio  AND Productos_POS.ID_H_O_D ='".$row['ID_H_O_D']."'";
+Servicios_POS.Servicio_ID = Productos_POS.Tipo_Servicio AND Productos_POS.Estatus='Vigente' AND Productos_POS.ID_H_O_D ='".$row['ID_H_O_D']."'";
  
 $result = mysqli_query($conn, $sql);
  
