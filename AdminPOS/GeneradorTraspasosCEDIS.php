ed3980294ee9187217a5326a2377c3cbccee1d5e
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-06-25 16:16:53 -0600

    xd

--- AdminPOS/GeneradorTraspasosCEDIS.php
+++ AdminPOS/GeneradorTraspasosCEDIS.php
@@ -889,14 +889,8 @@ function eliminarFila(element) {
   include ("footer.php")?>
 
 <!-- ./wrapper -->
-<script src="js/RealizaTraspasosV2.js"></script>
+<script src="js/RealizaTraspasosCedis.js"></script>
 
-<script src="datatables/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>  
-    <script src="datatables/JSZip-2.5.0/jszip.min.js"></script>    
-    <script src="datatables/pdfmake-0.1.36/pdfmake.min.js"></script>    
-    <script src="datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
-    <script src="datatables/Buttons-1.5.6/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
 
 
 <!-- Bootstrap -->
