37329f562c0e1dff5a313e9892ad1b57230d4610
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-06-26 11:23:24 -0600

    Update GeneradorTraspasosCEDIS.php

--- AdminPOS/GeneradorTraspasosCEDIS.php
+++ AdminPOS/GeneradorTraspasosCEDIS.php
@@ -667,7 +667,7 @@ function calcularDiferencia(fila) {
         <td style="display:none;"><input type="text" hidden name="Recibio[]" value="" class="form-control" ></td>
         <td style="display:none;"><input type="text" class="form-control " hidden name="NumeroDelTraspaso[]" readonly  value="<?php echo $NumeroOrdenTraspaso?>"  > </td>
         <td style="display:none;"><input type="text" class="form-control " hidden  name="ProveedorDelTraspaso[]" readonly  value="<?php echo $ProveedorFijo?>"  ></td>
-        <td style="display:none;"><input type="text" class="form-control " hidden name="NumeroDeFacturaTraspaso[]" readonly  value="<?php echo $NumeroDeFacturaTrapaso?>"  > </td>
+        <td><input type="text" class="form-control " name="NumeroDeFacturaTraspaso[]" readonly  value="<?php echo $NumeroDeFacturaTrapaso?>"  > </td>
      
      
         </tr>`;
