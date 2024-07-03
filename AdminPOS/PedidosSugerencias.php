5b8a2cd2b5cf8cec8e5a69f88eec3bf0c2dbe90f
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-07-03 09:07:43 -0600

    Update PedidosSugerencias.php

--- AdminPOS/PedidosSugerencias.php
+++ AdminPOS/PedidosSugerencias.php
@@ -34,7 +34,7 @@ include "Consultas/Consultas.php";
 <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
 <div class="card text-center">
   <div class="card-header" style="background-color:#0057b8 !important;color: white;">
-   Registro de ventas de Saluda al <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>  
+   Devoluciones de productos <?php echo FechaCastellano(date('d-m-Y H:i:s')); ?>  
   </div>
  
   <div >
