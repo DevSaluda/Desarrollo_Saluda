2e1766faa74994bf3d30393694ae1299d80b36d4
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-07-09 15:20:39 -0600

    Update ConvierteEnTraspaso.php

--- AdminPOS/Modales/ConvierteEnTraspaso.php
+++ AdminPOS/Modales/ConvierteEnTraspaso.php
@@ -69,9 +69,9 @@ if($query->num_rows>0){
                     <span class="input-group-text" id="Factura"> <i class="fas fa-info-circle"></i></span>
                 </div>
                 <input type="text" class="form-control" readonly value="<?php echo $Devoluciones->Num_Factura; ?>">
-                <input type="text" class="form-control"  name="IdBasedatos" hidden readonly value="<?php echo $Devoluciones->ID_Prod_POS; ?>">
+                <input type="text" class="form-control" hidden name="IdBasedatos" hidden readonly value="<?php echo $Devoluciones->ID_Prod_POS; ?>">
                 <input type="text" class="form-control" hidden value="<?php echo $row['Nombre_Apellidos']?>" readonly name="GeneradoPor">
-                <input type="text" class="form-control"  name="TipodeServicio"  readonly value="<?php echo $Devoluciones->Tipo_Servicio; ?>">
+                <input type="text" class="form-control" hidden name="TipodeServicio"  readonly value="<?php echo $Devoluciones->Tipo_Servicio; ?>">
                
             </div>
         </div>
