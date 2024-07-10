a9bd4b6fe39902fa2fd93b81496628a00626d8f2
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-07-10 15:21:32 -0600

    Update ExportToExcel.php

--- AdminPOS/Consultas/ExportToExcel.php
+++ AdminPOS/Consultas/ExportToExcel.php
@@ -6,7 +6,7 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 // Conexión a la base de datos
 include("db_connection.php");
-include "Consultas.php";
+include("Consultas.php");
 
 // Obtener las variables POST
 if (isset($_POST['Mes']) && isset($_POST['anual'])) {
@@ -17,39 +17,39 @@ if (isset($_POST['Mes']) && isset($_POST['anual'])) {
 }
 
 $query = "SELECT 
-Ventas_POS.Cod_Barra,
-Ventas_POS.Nombre_Prod,
-Stock_POS.Precio_C AS PrecioCompra,
-Stock_POS.Precio_Venta AS PrecioVenta,
-CONCAT(Ventas_POS.FolioSucursal, Ventas_POS.Folio_Ticket) AS FolioTicket,
-SucursalesCorre.Nombre_Sucursal AS Sucursal,
-Ventas_POS.Turno,
-Ventas_POS.Cantidad_Venta,
-Ventas_POS.Importe,
-Ventas_POS.Total_Venta,
-Ventas_POS.Fk_Caja,
-Ventas_POS.DescuentoAplicado AS Descuento,
-CONVERT(Ventas_POS.FormaDePago USING utf8) AS FormaPago,
-CONVERT(Ventas_POS.Cliente USING utf8) AS Cliente,
-Ventas_POS.FolioSignoVital,
-Servicios_POS.Nom_Serv AS NomServ,
-Ventas_POS.Fecha_venta AS AgregadoEl,
-Ventas_POS.AgregadoEl AS AgregadoEnMomento,
-Ventas_POS.AgregadoPor,
-CONVERT(Cajas_POS.EnfermeroEnturno USING utf8) AS Enfermero,
-CONVERT(Cajas_POS.MedicoEnturno USING utf8) AS Doctor
+    Ventas_POS.Cod_Barra,
+    Ventas_POS.Nombre_Prod,
+    Stock_POS.Precio_C AS PrecioCompra,
+    Stock_POS.Precio_Venta AS PrecioVenta,
+    CONCAT(Ventas_POS.FolioSucursal, Ventas_POS.Folio_Ticket) AS FolioTicket,
+    SucursalesCorre.Nombre_Sucursal AS Sucursal,
+    Ventas_POS.Turno,
+    Ventas_POS.Cantidad_Venta,
+    Ventas_POS.Importe,
+    Ventas_POS.Total_Venta,
+    Ventas_POS.Fk_Caja,
+    Ventas_POS.DescuentoAplicado AS Descuento,
+    CONVERT(Ventas_POS.FormaDePago USING utf8) AS FormaPago,
+    CONVERT(Ventas_POS.Cliente USING utf8) AS Cliente,
+    Ventas_POS.FolioSignoVital,
+    Servicios_POS.Nom_Serv AS NomServ,
+    Ventas_POS.Fecha_venta AS AgregadoEl,
+    Ventas_POS.AgregadoEl AS AgregadoEnMomento,
+    Ventas_POS.AgregadoPor,
+    CONVERT(Cajas_POS.EnfermeroEnturno USING utf8) AS Enfermero,
+    CONVERT(Cajas_POS.MedicoEnturno USING utf8) AS Doctor
 FROM 
-Ventas_POS
+    Ventas_POS
 INNER JOIN 
-SucursalesCorre ON Ventas_POS.Fk_sucursal = SucursalesCorre.ID_SucursalC 
+    SucursalesCorre ON Ventas_POS.Fk_sucursal = SucursalesCorre.ID_SucursalC 
 INNER JOIN 
-Servicios_POS ON Ventas_POS.Identificador_tipo = Servicios_POS.Servicio_ID 
+    Servicios_POS ON Ventas_POS.Identificador_tipo = Servicios_POS.Servicio_ID 
 INNER JOIN 
-Cajas_POS ON Cajas_POS.ID_Caja = Ventas_POS.Fk_Caja
+    Cajas_POS ON Cajas_POS.ID_Caja = Ventas_POS.Fk_Caja
 INNER JOIN 
-Stock_POS ON Stock_POS.ID_Prod_POS = Ventas_POS.ID_Prod_POS
+    Stock_POS ON Stock_POS.ID_Prod_POS = Ventas_POS.ID_Prod_POS
 WHERE 
-Ventas_POS.Fecha_venta BETWEEN '$mes' AND '$anual'";
+    Ventas_POS.Fecha_venta BETWEEN '$mes' AND '$anual'";
 
 // Ejecutar la consulta
 $resultado = $conn->query($query);
@@ -67,7 +67,7 @@ header('Cache-Control: max-age=0');
 $output = fopen('php://output', 'w');
 
 // Escribir los encabezados en el archivo CSV
-$encabezados = ["Codigo de Barras", "Nombre del Producto", "# de Ticket", "Sucursal", "Turno", "Cantidad", "P.U", "Importe", "Descuento", "Forma de Pago", "Cliente", "FolioSignoVital", "Servicio", "Fecha de venta", "Fecha y hora", "Vendedor", "Enfermero", "Doctor","PrecioCompra", "PrecioVenta" ];
+$encabezados = ["Codigo de Barras", "Nombre del Producto", "Folio de Ticket", "Sucursal", "Turno", "Cantidad", "Total Venta", "Importe", "Descuento", "Forma de Pago", "Cliente", "Folio Signo Vital", "Servicio", "Fecha de venta", "Fecha y hora", "Vendedor", "Enfermero", "Doctor", "Precio Compra", "Precio Venta"];
 fputcsv($output, $encabezados);
 
 // Escribir los datos del archivo CSV
@@ -77,20 +77,18 @@ while ($fila = $resultado->fetch_assoc()) {
         $value = utf8_decode($value);
     });
 
-       // Formatear la fecha en 'dd/mm/yyyy'
-       $date = new DateTime($fila["AgregadoEl"]);
-       $formattedDate = $date->format('d/m/Y');
-   
+    // Formatear la fecha en 'dd/mm/yyyy'
+    $date = new DateTime($fila["AgregadoEl"]);
+    $formattedDate = $date->format('d/m/Y');
+
     // Construir una fila con los datos específicos
     $data = [
         $fila["Cod_Barra"],
         $fila["Nombre_Prod"],
-      
         $fila["FolioTicket"],
         $fila["Sucursal"],
         $fila["Turno"],
         $fila["Cantidad_Venta"],
-       
         $fila["Total_Venta"],
         $fila["Importe"],
         $fila["Descuento"],
@@ -105,8 +103,8 @@ while ($fila = $resultado->fetch_assoc()) {
         $fila["Doctor"],
         $fila["PrecioCompra"],
         $fila["PrecioVenta"],
-       
     ];
+
     // Escribir la fila en el archivo CSV
     fputcsv($output, $data);
 }
