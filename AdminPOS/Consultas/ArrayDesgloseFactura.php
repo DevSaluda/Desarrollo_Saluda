31ee6b7afae5c65bcc12f821a83d5e5656abdb92
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-07-23 11:02:38 -0600

    Update ArrayDesgloseFactura.php

--- AdminPOS/Consultas/ArrayDesgloseFactura.php
+++ AdminPOS/Consultas/ArrayDesgloseFactura.php
@@ -34,7 +34,7 @@ Traspasos_generados.Precio_Venta,Traspasos_generados.Precio_Compra, Traspasos_ge
     $data[$c]["Destino"] = $fila["Fk_Sucursal_Destino"];
     $data[$c]["Cantidad"] = $fila["Cantidad_Enviada"];
  
-    $data[$c]["FechaEntrega"] = fechaCastellano($fila["FechaEntrega"]);
+    $data[$c]["FechaEntrega"] = $fila["FechaEntrega"];
    
             $c++;
         }
