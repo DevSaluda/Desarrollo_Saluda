12dd1e2f12699d197cbe2d27679424618016b9ec
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-07-23 10:44:20 -0600

    Update Preresultadoparaimpresionescedis.php

--- AdminPOS/Preresultadoparaimpresionescedis.php
+++ AdminPOS/Preresultadoparaimpresionescedis.php
@@ -38,277 +38,200 @@ include "Consultas/Consultas.php";
 </div>
 
 <?php
-      // Verificar si el formulario ha sido enviado
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
-          // Verificar si las variables están seteadas y no son nulas
-          if (isset($_POST['Factura'])) {
-              // Obtener los valores del formulario
-              $factura = $_POST['Factura'];
-              
+// Verificar si el formulario ha sido enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Depurar el contenido de $_POST para verificar los datos recibidos
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre>';
 
-              // Realizar las operaciones que necesites con estas variables
-              // Por ejemplo, imprimir su valor
-              echo "Mes seleccionado: $factura<br>";
+    // Verificar si las variables están seteadas y no son nulas
+    if (isset($_POST['Factura'])) {
+        // Obtener los valores del formulario
+        $factura = $_POST['Factura'];
 
-          } else {
-              // Si alguna de las variables no está seteada o es nula, mostrar un mensaje de error
-              echo "Error: No se recibieron todas las variables necesarias.";
-          }
-      }
-      ?>
+        // Realizar las operaciones que necesites con estas variables
+        // Por ejemplo, imprimir su valor
+        echo "Factura seleccionada: $factura<br>";
+    } else {
+        // Si alguna de las variables no está seteada o es nula, mostrar un mensaje de error
+        echo "Error: No se recibieron todas las variables necesarias.";
+    }
+}
+?>
 
-      <style>
-        /* Personalizar el diseño de la paginación con CSS */
-        .dataTables_wrapper .dataTables_paginate {
-          text-align: center !important; /* Centrar los botones de paginación */
-          margin-top: 10px !important;
-        }
+<style>
+  /* Personalizar el diseño de la paginación con CSS */
+  .dataTables_wrapper .dataTables_paginate {
+    text-align: center !important;
+    margin-top: 10px !important;
+  }
+  .dataTables_paginate .paginate_button {
+    padding: 5px 10px !important;
+    border: 1px solid #007bff !important;
+    margin: 2px !important;
+    cursor: pointer !important;
+    font-size: 16px !important;
+    color: #007bff !important;
+    background-color: #fff !important;
+  }
+  .dataTables_paginate .paginate_button.current {
+    background-color: #007bff !important;
+    color: #fff !important;
+    border-color: #007bff !important;
+  }
+  .dataTables_paginate .paginate_button:hover {
+    background-color: #C80096 !important;
+    color: #fff !important;
+    border-color: #C80096 !important;
+  }
+</style>
 
-        .dataTables_paginate .paginate_button {
-          padding: 5px 10px !important;
-          border: 1px solid #007bff !important;
-          margin: 2px !important;
-          cursor: pointer !important;
-          font-size: 16px !important;
-          color: #007bff !important;
-          background-color: #fff !important;
-        }
+<style>
+  /* Estilos personalizados para la tabla */
+  #Productos th {
+    font-size: 12px;
+    padding: 4px;
+    white-space: nowrap;
+  }
+</style>
 
-        /* Cambiar el color del paginado seleccionado */
-        .dataTables_paginate .paginate_button.current {
-          background-color: #007bff !important;
-          color: #fff !important;
-          border-color: #007bff !important;
-        }
+<style>
+  /* Estilos para la tabla */
+  #Productos {
+    font-size: 12px;
+    border-collapse: collapse;
+    width: 100%;
+    text-align: center;
+  }
+  #Productos th {
+    font-size: 16px;
+    background-color: #0057b8 !important;
+    color: white;
+    padding: 10px;
+  }
+  #Productos td {
+    font-size: 14px;
+    padding: 8px;
+    border-bottom: 1px solid #ccc;
+  }
+  .dt-buttons {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+  }
+  .dt-buttons button {
+    font-size: 14px;
+    margin: 0 5px;
+    color: white;
+    background-color: #fff;
+  }
+</style>
 
-        /* Cambiar el color del hover */
-        .dataTables_paginate .paginate_button:hover {
-          background-color: #C80096 !important;
-          color: #fff !important;
-          border-color: #C80096 !important;
-        }
-      </style>
-
-      <style>
-        /* Estilos personalizados para la tabla */
-        #Productos th {
-          font-size: 12px; /* Tamaño de letra para los encabezados */
-          padding: 4px; /* Ajustar el espaciado entre los encabezados */
-          white-space: nowrap; /* Evitar que los encabezados se dividan en varias líneas */
-        }
-      </style>
-
-      <style>
-        /* Estilos para la tabla */
-        #Productos {
-          font-size: 12px; /* Tamaño de letra para el contenido de la tabla */
-          border-collapse: collapse; /* Colapsar los bordes de las celdas */
-          width: 100%;
-          text-align: center; /* Centrar el contenido de las celdas */
-        }
-
-        #Productos th {
-          font-size: 16px; /* Tamaño de letra para los encabezados de la tabla */
-          background-color: #0057b8 !important; /* Nuevo color de fondo para los encabezados */
-          color: white; /* Cambiar el color del texto a blanco para contrastar */
-          padding: 10px; /* Ajustar el espaciado de los encabezados */
-        }
-
-        #Productos td {
-          font-size: 14px; /* Tamaño de letra para el contenido de la tabla */
-          padding: 8px; /* Ajustar el espaciado de las celdas */
-          border-bottom: 1px solid #ccc; /* Agregar una línea de separación entre las filas */
-        }
-
-        /* Estilos para el botón de Excel */
-        .dt-buttons {
-          display: flex;
-          justify-content: center;
-          margin-bottom: 10px;
-        }
-
-        .dt-buttons button {
-          font-size: 14px;
-          margin: 0 5px;
-          color: white; /* Cambiar el color del texto a blanco */
-          background-color: #fff; /* Cambiar el color de fondo a blanco */
-        }
-      </style>
-
-      <style>
-        /* Estilos para la capa de carga */
-        #loading-overlay {
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background-color: rgba(0, 0, 0, 0.7);
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          z-index: 9999; /* Asegurarse de que el overlay esté encima de todo */
-          display: none; /* Ocultar inicialmente el overlay */
-        }
-
-        /* Estilo para el ícono de carga */
-        .loader {
-          border: 6px solid #f3f3f3; /* Color del círculo externo */
-          border-top: 6px solid #C80096; /* Color del círculo interno */
-          border-radius: 50%;
-          width: 60px;
-          height: 60px;
-          animation: spin 1s linear infinite; /* Animación de rotación */
-        }
-
-        @keyframes spin {
-          0% { transform: rotate(0deg); }
-          100% { transform: rotate(360deg); }
-        }
-      </style>
+<style>
+  #loading-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+    display: none;
+  }
+  .loader {
+    border: 6px solid #f3f3f3;
+    border-top: 6px solid #C80096;
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    animation: spin 1s linear infinite;
+  }
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+</style>
 <script type="text/javascript">
     var factura = "<?php echo $factura; ?>";
-   
 </script>
 
-      <script>
-        // Definir una lista de mensajes para el mensaje de carga
-        var mensajesCarga = [
-          "Consultando ventas...",
-          "Estamos realizando la búsqueda...",
-          "Cargando datos...",
-          "Procesando la información...",
-          "Espere un momento...",
-          "Cargando... ten paciencia, incluso los planetas tardaron millones de años en formarse.",
-          "¡Espera un momento! Estamos contando hasta el infinito... otra vez.",
-          "¿Sabías que los pingüinos también tienen que esperar mientras cargan su comida?",
-          "¡Zapateando cucarachas de carga! ¿Quién necesita un exterminador?",
-          "Cargando... ¿quieres un chiste para hacer más amena la espera? ¿Por qué los pájaros no usan Facebook? Porque ya tienen Twitter.",
-          "¡Alerta! Un koala está jugando con los cables de carga. Espera un momento mientras lo persuadimos.",
-          "¿Sabías que las tortugas cargan a una velocidad épica? Bueno, estamos intentando superarlas.",
-          "¡Espera un instante! Estamos pidiendo ayuda a los unicornios para acelerar el proceso.",
-          "Cargando... mientras nuestros programadores disfrutan de una buena taza de café.",
-          "Cargando... No estamos seguros de cómo llegamos aquí, pero estamos trabajando en ello.",
-          "Estamos contando en binario... 10%, 20%, 110%... espero que esto no sea un error de desbordamiento.",
-          "Cargando... mientras cazamos pokémons para acelerar el proceso.",
-          "Error 404: Mensaje gracioso no encontrado. Estamos trabajando en ello.",
-          "Cargando... ¿Sabías que los programadores también tienen emociones? Bueno, nosotros tampoco.",
-          "Estamos buscando la respuesta a la vida, el universo y todo mientras cargamos... Pista: es un número entre 41 y 43.",
-          "Cargando... mientras los gatos toman el control. ¡Meowtrix está en marcha!",
-          "Estamos ajustando tu espera a la velocidad de la luz. Aún no es suficientemente rápida, pero pronto llegaremos.",
-          "Cargando... Ten paciencia, incluso los programadores necesitan tiempo para pensar en nombres de variables.",
-          "Estamos destilando líneas de código para obtener la solución perfecta. ¡Casi listo!",
-        ];
+<script>
+  var mensajesCarga = [
+    "Consultando ventas...",
+    "Estamos realizando la búsqueda...",
+    "Cargando datos...",
+    "Procesando la información...",
+    "Espere un momento...",
+    "Cargando... ten paciencia, incluso los planetas tardaron millones de años en formarse.",
+    "¡Espera un momento! Estamos contando hasta el infinito... otra vez.",
+    "¿Sabías que los pingüinos también tienen que esperar mientras cargan su comida?",
+    "¡Zapateando cucarachas de carga! ¿Quién necesita un exterminador?",
+    "Cargando... ¿quieres un chiste para hacer más amena la espera? ¿Por qué los pájaros no usan Facebook? Porque ya tienen Twitter.",
+    "¡Alerta! Un koala está jugando con los cables de carga. Espera un momento mientras lo persuadimos.",
+    "¿Sabías que las tortugas cargan a una velocidad épica? Bueno, estamos intentando superarlas.",
+    "¡Espera un instante! Estamos pidiendo ayuda a los unicornios para acelerar el proceso.",
+    "Cargando... mientras nuestros programadores disfrutan de una buena taza de café.",
+    "Cargando... No estamos seguros de cómo llegamos aquí, pero estamos trabajando en ello.",
+    "Estamos contando en binario... 10%, 20%, 110%... espero que esto no sea un error de desbordamiento.",
+    "Cargando... mientras cazamos pokémons para acelerar el proceso.",
+    "Error 404: Mensaje gracioso no encontrado. Estamos trabajando en ello.",
+    "Cargando... ¿Sabías que los programadores también tienen emociones? Bueno, nosotros tampoco.",
+    "Estamos buscando la respuesta a la vida, el universo y todo mientras cargamos... Pista: es un número entre 41 y 43.",
+    "Cargando... mientras los gatos toman el control. ¡Meowtrix está en marcha!",
+    "Estamos ajustando tu espera a la velocidad de la luz. Aún no es suficientemente rápida, pero pronto llegaremos.",
+    "Cargando... Ten paciencia, incluso los programadores necesitan tiempo para pensar en nombres de variables.",
+    "Estamos destilando líneas de código para obtener la solución perfecta. ¡Casi listo!",
+  ];
 
-        // Función para mostrar el mensaje de carga con un texto aleatorio
-        function mostrarCargando(event, settings) {
-          var randomIndex = Math.floor(Math.random() * mensajesCarga.length);
-          var mensaje = mensajesCarga[randomIndex];
-          document.getElementById('loading-text').innerText = mensaje;
-          document.getElementById('loading-overlay').style.display = 'flex';
+  function mostrarCargando(event, settings) {
+    var randomIndex = Math.floor(Math.random() * mensajesCarga.length);
+    var mensaje = mensajesCarga[randomIndex];
+    document.getElementById('loading-text').innerText = mensaje;
+    document.getElementById('loading-overlay').style.display = 'flex';
+  }
+
+  function ocultarCargando() {
+    document.getElementById('loading-overlay').style.display = 'none';
+  }
+
+  var tabla;
+  $(document).ready(function() {
+    tabla = $('#Productos').DataTable({
+      "processing": true,
+      "ordering": true,
+      "stateSave": true,
+      "autoWidth": true,
+      "order": [[ 0, "desc" ]],
+      "ajax": {
+        "type": "POST",
+        "url": "https://saludapos.com/AdminPOS/Consultas/ArrayDesgloseFactura.php",
+        "data": function (d) {
+          var factura = '<?php echo $factura; ?>';
+          var dataToSend = {
+              "Factura": factura,
+          };
+          return dataToSend;
+        },
+        "error": function(xhr, error, code) {
+          console.log(xhr);
         }
-
-        // Función para ocultar el mensaje de carga
-        function ocultarCargando() {
-          document.getElementById('loading-overlay').style.display = 'none';
-        }
-
-        var tabla;
-        $(document).ready(function() {
-          tabla = $('#Productos').DataTable({
-            "processing": true,
-            "ordering": true,
-            "stateSave": true,
-            "autoWidth": true,
-            "order": [[ 0, "desc" ]],
-            "ajax": {
-              "type": "POST", // Especifica el método de envío de la solicitud AJAX
-              "url": "https://saludapos.com/AdminPOS/Consultas/ArrayDesgloseFactura.php",
-              "data": function (d) {
-        // Aquí puedes definir el código PHP directamente
-        var factura = '<?php echo $factura; ?>'; // Obtén el valor de mes desde PHP
-       
-        // Construye el objeto de datos para enviar al servidor
-        var dataToSend = {
-            "Factura": factura,
-           
-        };
-
-        return dataToSend;
-    },
-              "error": function(xhr, error, thrown) {
-            console.log("Error en la solicitud AJAX:", error);
-        }
-    },
-            "columns": [
-              { "data": "IDTraspasoGenerado" },
-              { "data": "Cod_Barra" },
-              { "data": "Nombre_Prod" },
-              { "data": "Fk_sucursal" },
-              { "data": "Destino" },
-              { "data": "Cantidad" },
-              { "data": "FechaEntrega" },
-              
-            ],
-            "lengthMenu": [[10,20,150,250,500, -1], [10,20,50,250,500, "Todos"]],
-            "language": {
-              "lengthMenu": "Mostrar _MENU_ registros",
-              "sPaginationType": "extStyle",
-              "zeroRecords": "No se encontraron resultados",
-              "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-              "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-              "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-              "sSearch": "Buscar:",
-              "paginate": {
-                "first": '<i class="fas fa-angle-double-left"></i>',
-                "last": '<i class="fas fa-angle-double-right"></i>',
-                "next": '<i class="fas fa-angle-right"></i>',
-                "previous": '<i class="fas fa-angle-left"></i>'
-              },
-              "processing": function () {
-                mostrarCargando();
-              }
-            },
-            "initComplete": function() {
-              // Al completar la inicialización de la tabla, ocultar el mensaje de carga
-              ocultarCargando();
-            },
-            "buttons": [
-      {
-        text: 'Exportar a Excel <i class="fas fa-file-excel"></i>',
-titleAttr: 'Exportar a Excel',
-className: 'btn btn-success',
-action: function(e, dt, button, config) {
-    // Mostrar una alerta con SweetAlert2
-    Swal.fire({
-        title: 'Advertencia',
-        html: 'Estimado usuario, te comentamos que el archivo se descarga en formato .csv. Te recomendamos que, una vez completada la descarga, lo conviertas al formato necesario que requieras.',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Exportar',
-        cancelButtonText: 'Cancelar'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            // Crear y enviar el formulario si el usuario confirma
-            var form = $('<form method="POST" action="https://saludapos.com/AdminPOS/Consultas/ExportToExcel" target="_blank"></form>');
-            var mesInput = $('<input type="hidden" name="Mes" value="' + mes + '">');
-            var anualInput = $('<input type="hidden" name="anual" value="' + anual + '">');
-            form.append(mesInput);
-            form.append(anualInput);
-            $('body').append(form);
-            form.submit();
-        }
-    });
-}
-
+      },
+      "columns": [
+        { "data": "Cod_Barra" },
+        { "data": "Nombre_Prod" },
+        { "data": "Cantidad_Prod" },
+        { "data": "Fk_sucursal" },
+      ],
+      "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
       }
-    ],
-            "dom": '<"d-flex justify-content-between"lBf>rtip', // Modificar la disposición aquí
-            "responsive": true
-          });
-        });
-      </script>
-
+    });
+  });
+</script>
 
 <style>
 @media print {
