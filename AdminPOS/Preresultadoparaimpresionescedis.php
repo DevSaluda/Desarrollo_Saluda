bbc8b8844e92a355b2e7e3b19fa2eb0caa37668e
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-07-23 11:19:27 -0600

    Update Preresultadoparaimpresionescedis.php

--- AdminPOS/Preresultadoparaimpresionescedis.php
+++ AdminPOS/Preresultadoparaimpresionescedis.php
@@ -225,7 +225,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         { "data": "Fk_sucursal" },
         { "data": "Destino" },
         { "data": "Cantidad" },
-        { "data": "FechaEntrega" },
+       
       ],
       "language": {
         "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
