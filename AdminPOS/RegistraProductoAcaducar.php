4a1b560527830138b45c1578aa91710ab9a9e468
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-07-29 10:10:25 -0600

    Ajustes

--- AdminPOS/RegistraProductoAcaducar.php
+++ AdminPOS/RegistraProductoAcaducar.php
@@ -447,7 +447,7 @@ $fechaActual = date('Y-m-d'); // Esto obtiene la fecha actual en el formato 'Añ
 
 
 <script>
-  table = $('#tablaAgregarArticulos').DataTable({
+  table = $('#tablaAgregarArticuloss').DataTable({
     searching: false, // Deshabilitar la funcionalidad de búsqueda
     paging: false, // Deshabilitar el paginador
     lengthChange: false, // Ocultar el selector de número de registros
