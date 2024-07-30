f16344c73d9f0027f951aa62e26e8f0264288c4a
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-07-30 09:47:35 -0600

    Update ArrayResultadosCaducados.php

--- AdminPOS/Consultas/ArrayResultadosCaducados.php
+++ AdminPOS/Consultas/ArrayResultadosCaducados.php
@@ -41,7 +41,7 @@ while ($fila = $result->fetch_assoc()) {
     $estado = $fila["Estado"];
     
     if ($estado == "Caducado") {
-        $estado = '<button style="background-color: red; color: white; padding: 2px 4px; border-radius: 3px; border: none;" class="btn btn-default btn-sm">Caducado</button>';
+        $estado = '<button style="background-color: #ff9900 !important;" class="btn btn-default btn-sm">Caducado</button>';
     } else if (empty($estado)) {
         $estado = '<button style="background-color: gray; color: white; padding: 2px 4px; border-radius: 3px; border: none;" class="btn btn-default btn-sm">Sin estado</button>';
     } else {
