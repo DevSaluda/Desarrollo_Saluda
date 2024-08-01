26a3b00656245f925b6313742a016f32e28b0c83
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-08-01 16:38:18 -0600

    Agregadas pantallas faltantes e intento de corrección de guardadp

--- CEDISMOVIL/Modales/BusquedaTraspasosFechasV2.php
+++ CEDISMOVIL/Modales/BusquedaTraspasosFechasV2.php
@@ -14,7 +14,7 @@
 
         <div class="modal-body">
 
-          <form method="POST" action="https://saludapos.com/CEDISMOVIL/TraspasosPorFechasV2">
+          <form method="POST" action="https://saludapos.com/CEDISMOVIL/TraspasosPorFechas">
 
 
 
