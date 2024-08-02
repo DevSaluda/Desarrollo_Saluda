b1850a1e734161f2d46cbd7d449f1d36d0378406
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-08-02 12:08:08 -0600

    Update EntregaUltrasF.php

--- AgendaDeCitas/Consultas/EntregaUltrasF.php
+++ AgendaDeCitas/Consultas/EntregaUltrasF.php
@@ -21,6 +21,7 @@ $query = $conn->query($sql1);
 ?>
 
 <?php if($query->num_rows>0):?>
+  
   <div class="text-center">
 	<div class="table-responsive">
 	<table id="Ultras" class="table ">
@@ -36,7 +37,9 @@ $query = $conn->query($sql1);
 
 
 </thead>
-<?php while ($Ultras=$query->fetch_array()):?>
+<?php while ($Ultras=$query->fetch_array()):
+  $nombreSucursal = ($Ultras["Nombre_Sucursal"] === "TeaClinica") ? "Teabo Clinica" : $Ultras["Nombre_Sucursal"];  
+?>
 <tr>
 	<td style="width:50px;" ><?php echo $Ultras["ID_resultado"]; ?></td>
 	<td><?php echo $Ultras["Nombre_paciente"]; ?></td>
