fdfb8d50961bdd84af9561ee4ce5517e9d422d36
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-08-05 15:25:00 -0600

    Update Header.php

--- AdminPOS/Header.php
+++ AdminPOS/Header.php
@@ -1,42 +1,51 @@
-  <!-- jQuery -->
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<!-- DataTables CSS -->
-<link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-
-<!-- DataTables JS -->
-<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-
-<!-- DataTables Buttons CSS -->
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
-
-<!-- DataTables Buttons JS -->
-<script src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
-
 <!-- Font Awesome Icons -->
 <script src="https://kit.fontawesome.com/ef7185cf29.js" crossorigin="anonymous"></script>
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.css">
+ 
+  <script src="Componentes/jquery-3.5.1.slim.min.js"></script>
+<script src="Componentes/jquery-3.5.0.js"></script>
 
-<!-- Bootstrap CSS and JS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="Componentes/bootstrap.min.css">
+  
 
-<!-- Additional CSS and JS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+  <!-- JS, Popper.js, and jQuery -->
 
-<!-- Custom CSS and JS -->
-<link rel="stylesheet" href="Componentes/datatables.min.css"/>
-<script type="text/javascript" src="Componentes/datatables.min.js"></script>
-<script src="Componentes/sweetalert2@9.js"></script>
-<link href="Componentes/mdb.min.css" rel="stylesheet">
-<script src="js/validation.min.js"></script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <script src="Componentes/popper.min.js"></script>
+  <script src="Componentes/bootstrap.min.js"></script>
+  
+  <!-- JS, Popper.js, and jQuery -->
+  
+
+<link rel="stylesheet" href="https://cdn.datatables.net/2.0.5/css/dataTables.dataTables.css" />
+  
+<script src="https://cdn.datatables.net/2.0.5/js/dataTables.js"></script>
+
+<!-- DataTables Buttons CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
+
+<!-- DataTables Buttons JavaScript -->
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+
+
+
+
+<link rel="stylesheet" type="text/css" href="Componentes/datatables.min.css"/>
+  <script type="text/javascript" src="Componentes/datatables.min.js"></script>
+  <script src="Componentes/sweetalert2@9.js"></script>
+  <link href="Componentes/mdb.min.css" rel="stylesheet">
+  <script src="js/validation.min.js"></script><link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <link rel="stylesheet" href="Componentes/Multiselect/dist/css/bootstrap-multiselect.css" type="text/css">
 <script type="text/javascript" src="Componentes/Multiselect/dist/js/bootstrap-multiselect.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@1.0.6/dist/select2-bootstrap4.min.css" rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css">
 <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.1/chart.min.js"></script>
