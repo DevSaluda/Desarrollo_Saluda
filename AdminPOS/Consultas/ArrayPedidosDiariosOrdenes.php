042a640b90c2f4c48d097fba8b74142d24c0dfca
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-08-05 10:58:22 -0600

    Update ArrayPedidosDiariosOrdenes.php

--- AdminPOS/Consultas/ArrayPedidosDiariosOrdenes.php
+++ AdminPOS/Consultas/ArrayPedidosDiariosOrdenes.php
@@ -3,10 +3,11 @@ header('Content-Type: application/json');
 include("db_connection.php");
 include "Consultas.php";
 
+// Consulta SQL
 $sql = "SELECT 
     Sugerencias_POS.NumOrdPedido, 
     SucursalesCorre.Nombre_Sucursal, 
-    Sugerencias_POS.AgregadoEl
+    DATE_FORMAT(Sugerencias_POS.AgregadoEl, '%Y-%m-%d') AS Fecha_Agregado
 FROM 
     Sugerencias_POS
 INNER JOIN 
@@ -18,24 +19,27 @@ GROUP BY
     Sugerencias_POS.NumOrdPedido, 
     SucursalesCorre.Nombre_Sucursal";
 
+// Ejecutar la consulta
 $result = mysqli_query($conn, $sql);
 
 $data = [];
 $c = 0;
 
+// Procesar los resultados
 while($fila = $result->fetch_assoc()) {
-    $data[$c]["Id_Sugerencia"] = $fila["NumOrdPedido"];
-    $data[$c]["Cod_Barra"] = $fila["Nombre_Sucursal"];
-    $data[$c]["Nombre_Prod"] = $fila["AgregadoEl"];
+    $data[$c]["NumOrdPedido"] = $fila["NumOrdPedido"];
+    $data[$c]["Nombre_Sucursal"] = $fila["Nombre_Sucursal"];
+    $data[$c]["Fecha_Agregado"] = $fila["Fecha_Agregado"];
     $data[$c]["Acciones"] = '
     <td>
-     <a data-id="' . $fila["NumOrdPedido"] . '" class="btn btn-warning btn-sm btn-GeneraRotacion"><i class="fas fa-people-carry"></i></a>
-  <a data-id="' . $fila["NumOrdPedido"] . '" class="btn btn-primary btn-sm btn-GeneraIngreso"><i class="fas fa-pills"></i></a>
+        <a data-id="' . $fila["NumOrdPedido"] . '" class="btn btn-warning btn-sm btn-GeneraRotacion"><i class="fas fa-people-carry"></i></a>
+        <a data-id="' . $fila["NumOrdPedido"] . '" class="btn btn-primary btn-sm btn-GeneraIngreso"><i class="fas fa-pills"></i></a>
     </td>';
   
     $c++;
 }
 
+// Estructura de la respuesta
 $results = [
     "sEcho" => 1,
     "iTotalRecords" => count($data),
@@ -43,7 +47,9 @@ $results = [
     "aaData" => $data
 ];
 
+// Devolver la respuesta en formato JSON
 echo json_encode($results);
 
+// Cerrar la conexión
 mysqli_close($conn);
 ?>
