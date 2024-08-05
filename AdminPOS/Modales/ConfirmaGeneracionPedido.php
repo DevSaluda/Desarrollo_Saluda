51ba0a526a1417c9e6258e4d9ae968a8b5778577
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-08-05 12:34:49 -0600

    Update ConfirmaGeneracionPedido.php

--- AdminPOS/Modales/ConfirmaGeneracionPedido.php
+++ AdminPOS/Modales/ConfirmaGeneracionPedido.php
@@ -42,39 +42,67 @@ if ($query->num_rows > 0) {
 // Verificar si se obtuvieron resultados
 if ($Sugerencias != null) {
     ?>
-    <form action="javascript:void(0)" method="post" id="RegistraCaducados">
-        <input type="text" class="form-control" hidden name="IdBasedatos" readonly value="<?php echo $Sugerencias->Id_Sugerencia; ?>">
-        <input type="text" class="form-control" hidden name="AgregadoPor" value="<?php echo $Sugerencias->AgregadoPor; ?>">
-        <input type="text" class="form-control" hidden name="ID_H_O_D" value="<?php echo $Sugerencias->ID_H_O_D; ?>">
-        <input type="text" class="form-control" hidden name="Precio_Venta" value="<?php echo $Sugerencias->Precio_Venta; ?>">
-        <input type="text" class="form-control" hidden name="Precio_C" value="<?php echo $Sugerencias->Precio_C; ?>">
-        <input type="text" class="form-control" hidden name="Fecha_Ingreso" value="<?php echo $Sugerencias->Fecha_Ingreso; ?>">
-        <input type="text" class="form-control" hidden name="Fk_sucursal" value="<?php echo $Sugerencias->Fk_sucursal; ?>">
+    <!-- Modal que se abre al cargar la página -->
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Registrar Caducados</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <!-- Mensaje de Confirmación -->
+                    <div class="alert alert-info">
+                        ¿Desea realizar el pedido para la sucursal "<?php echo $Sugerencias->Fk_sucursal; ?>" con número de orden de BD "<?php echo $Sugerencias->NumOrdPedido; ?>"?
+                    </div>
+                    
+                    <!-- Formulario para registrar caducados -->
+                    <form action="javascript:void(0)" method="post" id="RegistraCaducados">
+                        <input type="text" class="form-control" hidden name="IdBasedatos" readonly value="<?php echo $Sugerencias->Id_Sugerencia; ?>">
+                        <input type="text" class="form-control" hidden name="AgregadoPor" value="<?php echo $Sugerencias->AgregadoPor; ?>">
+                        <input type="text" class="form-control" hidden name="ID_H_O_D" value="<?php echo $Sugerencias->ID_H_O_D; ?>">
+                        <input type="text" class="form-control" hidden name="Precio_Venta" value="<?php echo $Sugerencias->Precio_Venta; ?>">
+                        <input type="text" class="form-control" hidden name="Precio_C" value="<?php echo $Sugerencias->Precio_C; ?>">
+                        <input type="text" class="form-control" hidden name="Fecha_Ingreso" value="<?php echo $Sugerencias->Fecha_Ingreso; ?>">
+                        <input type="text" class="form-control" hidden name="Fk_sucursal" value="<?php echo $Sugerencias->Fk_sucursal; ?>">
 
-        <div class="text-center">
-            <div class="table-responsive">
-                <table id="HistorialDevoluciones" class="table table-hover">
-                    <thead>
-                        <th>Código de Barra</th>
-                        <th>Producto</th>
-                        <th>Cantidad</th>
-                        <th>Cantidad a registrar</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><input type="text" value="<?php echo $Sugerencias->Cod_Barra; ?>" class="form-control" name="CodBarra" readonly></td>
-                            <td><input type="text" value="<?php echo $Sugerencias->Nombre_Prod; ?>" class="form-control" name="NombreProd" readonly></td>
-                            <td><input type="text" value="<?php echo $Sugerencias->Cantidad; ?>" class="form-control" readonly></td>
-                            <td><input type="number" class="form-control" name="CantidadAregistrar"></td>
-                        </tr>
-                    </tbody>
-                </table> 
-                <button type="submit" id="submit" class="btn btn-success">Registrar caducado <i class="fas fa-check"></i></button>
+                        <div class="text-center">
+                            <div class="table-responsive">
+                                <table id="HistorialDevoluciones" class="table table-hover">
+                                    <thead>
+                                        <th>Código de Barra</th>
+                                        <th>Producto</th>
+                                        <th>Cantidad</th>
+                                        <th>Cantidad a registrar</th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><input type="text" value="<?php echo $Sugerencias->Cod_Barra; ?>" class="form-control" name="CodBarra" readonly></td>
+                                            <td><input type="text" value="<?php echo $Sugerencias->Nombre_Prod; ?>" class="form-control" name="NombreProd" readonly></td>
+                                            <td><input type="text" value="<?php echo $Sugerencias->Cantidad; ?>" class="form-control" readonly></td>
+                                            <td><input type="number" class="form-control" name="CantidadAregistrar"></td>
+                                        </tr>
+                                    </tbody>
+                                </table> 
+                                <button type="submit" id="submit" class="btn btn-success">Registrar caducado <i class="fas fa-check"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </form>
+    </div>
 
     <script src="js/RegistraMedicamentosCaducadosdef.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#editModal').modal('show'); // Mostrar el modal al cargar el contenido
+        });
+    </script>
+
     <?php
 } else {
     // Si no hay datos, mostrar un mensaje
