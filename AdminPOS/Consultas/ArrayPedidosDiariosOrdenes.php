f4e411c75b5b7434143396fabeeb14f824a82c2f
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-08-05 10:55:45 -0600

    Update ArrayPedidosDiariosOrdenes.php

--- AdminPOS/Consultas/ArrayPedidosDiariosOrdenes.php
+++ AdminPOS/Consultas/ArrayPedidosDiariosOrdenes.php
@@ -30,7 +30,7 @@ while($fila = $result->fetch_assoc()) {
     $data[$c]["Acciones"] = '
     <td>
      <a data-id="' . $fila["NumOrdPedido"] . '" class="btn btn-warning btn-sm btn-GeneraRotacion"><i class="fas fa-people-carry"></i></a>
-  
+   <a data-id="' . $fila["NumOrdPedido"] . '" class="btn btn-warning btn-sm btn-GeneraRotacion"><i class="fas fa-people-carry"></i></a>
     </td>';
   
     $c++;
