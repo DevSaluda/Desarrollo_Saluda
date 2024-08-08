cd72a982b0884465054c945af9564e281d3c0254
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-08-08 15:35:57 -0600

    Update BusquedaDeFoliosFacturas.php

--- AdminPOS/Modales/BusquedaDeFoliosFacturas.php
+++ AdminPOS/Modales/BusquedaDeFoliosFacturas.php
@@ -52,7 +52,7 @@ $(document).ready(function() {
         placeholder: "Seleccione una Factura:",
         allowClear: true,
         width: '100%', // Asegura que el select ocupe todo el ancho disponible
-        theme: "classic"
+        theme: 'bootstrap-5'
     });
 
     // Configura el evento change en el select de sucursales
