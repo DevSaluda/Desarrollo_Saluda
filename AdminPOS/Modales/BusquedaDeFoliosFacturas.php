fdff9580fe5811430ff1379c7bf75d184547a5a5
SaludaDev <156374082+DevSaluda@users.noreply.github.com>
2024-08-08 15:31:36 -0600

    Update BusquedaDeFoliosFacturas.php

--- AdminPOS/Modales/BusquedaDeFoliosFacturas.php
+++ AdminPOS/Modales/BusquedaDeFoliosFacturas.php
@@ -1,6 +1,6 @@
 <div class="modal fade bd-example-modal-xl" id="FiltroDeFacturas" tabindex="-1" role="dialog"
   aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog  modal-notify modal-primary">
+  <div class="modal-dialog modal-lg modal-notify modal-primary">
     <div class="modal-content">
 
       <div class="text-center">
