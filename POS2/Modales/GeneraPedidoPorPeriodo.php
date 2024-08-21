fadc44ac1987d7da705f6c7ec016b29abc52a196
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-08-21 09:54:39 -0600

    Update GeneraPedidoPorPeriodo.php

--- POS2/Modales/GeneraPedidoPorPeriodo.php
+++ POS2/Modales/GeneraPedidoPorPeriodo.php
@@ -31,7 +31,7 @@
   <div class="input-group-prepend">  <span class="input-group-text" id="Tarjeta"><i class="far fa-hospital"></i></span>
   </div>
   <input type="date" class="form-control " id="Fecha1" name="Fecha1">
-  <input type="text" name="Sucursal" hidden value="<?echo $row['Fk_Sucursal']?>">
+  <input type="text" name="Sucursal" hidden value="<?php echo $row['Fk_Sucursal']?>">
   <script>
   document.addEventListener("DOMContentLoaded", function() {
     const dateInput = document.getElementById('Fecha1');
