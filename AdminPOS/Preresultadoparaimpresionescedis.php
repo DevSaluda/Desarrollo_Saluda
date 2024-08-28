703df4d6210c402ac01ac7b77e4a885c974df6a4
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-08-28 11:23:04 -0600

    Update Preresultadoparaimpresionescedis.php

--- AdminPOS/Preresultadoparaimpresionescedis.php
+++ AdminPOS/Preresultadoparaimpresionescedis.php
@@ -261,7 +261,7 @@ $(document).ready(function() {
         left: 0;
         top: 0;
         width: 100%;
-        height: 100%;
+        height: 50%;
         margin: 0;
         padding: 0;
     }
