f056e6cb9ee534369dd82f3db44a1e53cb378e9f
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-09-06 10:33:09 -0600

    Correccion de Tabla y tamaño del Modal

--- AgendaDeCitas/Modales/EdicionDeHorarios.php
+++ AgendaDeCitas/Modales/EdicionDeHorarios.php
@@ -42,81 +42,94 @@ if ($query->num_rows > 0) {
 <?php if($Especialistas != null): ?>
 
 
-<div id="AutorizaFin">
-    <form action="javascript:void(0)" method="post" id="ProgramaHorasNuevas">
-        <div class="row">
-            <div class="col">
-                <label for="exampleFormControlInput1">Médico<span class="text-danger">*</span></label>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="Tarjeta"><i class="fas fa-at"></i></span>
-                    </div>
-                    <input type="text" class="form-control" readonly value="<?php echo $Especialistas->Nombre_Apellidos; ?>">
-                </div>
+<!-- Modal structure -->
+<div id="AutorizaFin" class="modal-dialog modal-dialog-centered modal-xl"> <!-- modal-xl para que sea grande -->
+    <div class="modal-content">
+        <form action="javascript:void(0)" method="post" id="ProgramaHorasNuevas">
+            <div class="modal-header">
+                <h5 class="modal-title">Programar Horarios</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <div class="col">
-                <label for="exampleFormControlInput1">Sucursal<span class="text-danger">*</span></label>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="Tarjeta"><i class="fas fa-at"></i></span>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col">
+                        <label for="exampleFormControlInput1">Médico<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="Tarjeta"><i class="fas fa-at"></i></span>
+                            </div>
+                            <input type="text" class="form-control" readonly value="<?php echo $Especialistas->Nombre_Apellidos; ?>">
+                        </div>
                     </div>
-                    <input type="text" class="form-control" readonly value="<?php echo $Especialistas->Nombre_Sucursal; ?>">
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col">
-                <label for="FechaSeleccionada">Fecha<span class="text-danger">*</span></label>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="Tarjeta"><i class="fas fa-calendar"></i></span>
+                    <div class="col">
+                        <label for="exampleFormControlInput1">Sucursal<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="Tarjeta"><i class="fas fa-at"></i></span>
+                            </div>
+                            <input type="text" class="form-control" readonly value="<?php echo $Especialistas->Nombre_Sucursal; ?>">
+                        </div>
                     </div>
-                    <select id="FechaSeleccionada" class="form-control" name="FechaSeleccionada" required>
-                        <?php
-                        $query = $conn->query("SELECT ID_Fecha_Esp, Fecha_Disponibilidad FROM Fechas_EspecialistasExt WHERE Fk_Programacion=$Especialistas->ID_Programacion");
-                        while ($valores = mysqli_fetch_array($query)) {
-                            echo '<option value="'.$valores["ID_Fecha_Esp"].'">'.fechaCastellano($valores["Fecha_Disponibilidad"]).'</option>';
-                        }
-                        ?>
-                    </select>
                 </div>
-            </div>
 
-            <div class="col">
-                <label for="HoraSeleccionada">Hora disponible<span class="text-danger">*</span></label>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="Tarjeta"><i class="fas fa-clock"></i></span>
+                <div class="row">
+                    <div class="col">
+                        <label for="FechaSeleccionada">Fecha<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="Tarjeta"><i class="fas fa-calendar"></i></span>
+                            </div>
+                            <select id="FechaSeleccionada" class="form-control" name="FechaSeleccionada" required>
+                                <?php
+                                $query = $conn->query("SELECT ID_Fecha_Esp, Fecha_Disponibilidad FROM Fechas_EspecialistasExt WHERE Fk_Programacion=$Especialistas->ID_Programacion");
+                                while ($valores = mysqli_fetch_array($query)) {
+                                    echo '<option value="'.$valores["ID_Fecha_Esp"].'">'.fechaCastellano($valores["Fecha_Disponibilidad"]).'</option>';
+                                }
+                                ?>
+                            </select>
+                        </div>
                     </div>
-                    <select id="HoraSeleccionada" class="form-control" name="HoraSeleccionada" required>
-                        <option value="">Selecciona una hora</option>
-                    </select>
-                </div>
-            </div>
 
-            <div class="col">
-                <label for="NuevaHora">Nueva hora<span class="text-danger">*</span></label>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="Tarjeta"><i class="fas fa-clock"></i></span>
+                    <div class="col">
+                        <label for="HoraSeleccionada">Hora disponible<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="Tarjeta"><i class="fas fa-clock"></i></span>
+                            </div>
+                            <select id="HoraSeleccionada" class="form-control" name="HoraSeleccionada" required>
+                                <option value="">Selecciona una hora</option>
+                            </select>
+                        </div>
                     </div>
-                    <input type="time" class="form-control" name="NuevaHora" required>
+
+                    <div class="col">
+                        <label for="NuevaHora">Nueva hora<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="Tarjeta"><i class="fas fa-clock"></i></span>
+                            </div>
+                            <input type="time" class="form-control" name="NuevaHora" required>
+                        </div>
+                    </div>
                 </div>
+
+                <!-- Hidden inputs -->
+                <input type="text" class="form-control" hidden name="MedicoHoras[]" readonly value="<?php echo $Especialistas->FK_Medico; ?>">
+                <input type="text" class="form-control" hidden name="NumberProgramaHoras[]" readonly value="<?php echo $Especialistas->ID_Programacion; ?>">
+                <input type="text" class="form-control" hidden name="UsuarioHoras[]" readonly value=" <?php echo $row['Nombre_Apellidos']?>">
+                <input type="text" class="form-control" hidden name="EmpresaHoras[]" readonly value=" <?php echo $row['ID_H_O_D']?>">
+                <input type="text" class="form-control" hidden name="SistemaHoras[]" readonly value="<?php echo $row['Nombre_rol']?>">
             </div>
-        </div>
 
-        <input type="text" class="form-control" hidden name="MedicoHoras[]" readonly value="<?php echo $Especialistas->FK_Medico; ?>">
-        <input type="text" class="form-control" hidden name="NumberProgramaHoras[]" readonly value="<?php echo $Especialistas->ID_Programacion; ?>">
-        <input type="text" class="form-control" hidden name="UsuarioHoras[]" readonly value=" <?php echo $row['Nombre_Apellidos']?>">
-        <input type="text" class="form-control" hidden name="EmpresaHoras[]" readonly value=" <?php echo $row['ID_H_O_D']?>">
-        <input type="text" class="form-control" hidden name="SistemaHoras[]" readonly value="<?php echo $row['Nombre_rol']?>">
-
-        <div class="modal-footer justify-content-center">
-            <button type="submit" id="EnviarDatos" value="Guardar" class="btn btn-success">Guardar <i class="fas fa-save"></i></button>
-        </div>
-    </form>
+            <div class="modal-footer justify-content-center">
+                <button type="submit" id="EnviarDatos" value="Guardar" class="btn btn-success">Guardar <i class="fas fa-save"></i></button>
+            </div>
+        </form>
+    </div>
 </div>
+
 
 <script>
     document.getElementById('FechaSeleccionada').addEventListener('change', function() {
@@ -130,18 +143,19 @@ if ($query->num_rows > 0) {
             success: function(data) {
                 var horaSelect = document.getElementById('HoraSeleccionada');
                 horaSelect.innerHTML = ''; // Limpiar opciones previas
-                var horas = JSON.parse(data);
+                var horas = JSON.parse(data); // Asegurarnos que data es un array con varias horas
 
-                horas.forEach(function(hora) {
+                horas.forEach(function(hora) {  // Iterar sobre todas las horas
                     var option = document.createElement('option');
-                    option.value = hora;
-                    option.text = hora;
-                    horaSelect.appendChild(option);
+                    option.value = hora; // Asignar el valor de la hora
+                    option.text = hora;  // Asignar el texto visible en el dropdown
+                    horaSelect.appendChild(option); // Agregar la opción al select
                 });
             }
         });
     });
 </script>
+
 <?php else: ?>
 <p class="alert alert-danger">404 No se encuentra</p>
 <?php endif; ?>
