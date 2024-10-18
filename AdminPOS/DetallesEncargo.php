4235ed39c486e4dca094d57d2a173049b167df91
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-10-18 11:31:43 -0600

    Update DetallesEncargo.php

--- AdminPOS/DetallesEncargo.php
+++ AdminPOS/DetallesEncargo.php
@@ -28,6 +28,8 @@ while ($row = mysqli_fetch_assoc($result)) {
 // Volver a ejecutar la consulta para mostrar todos los productos en la tabla
 $result = mysqli_query($conn, $query);
 ?>
+<?php
+include 'Consultas/Consultas.php';?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
