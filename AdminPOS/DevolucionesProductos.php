27f03ac349de13b4f5516180b845961b5fd069ef
SaludaDev <saludasistemas@gmail.com>
2024-10-24 12:12:11 -0600

    Ajustes

--- AdminPOS/DevolucionesProductos.php
+++ AdminPOS/DevolucionesProductos.php
@@ -132,7 +132,7 @@ include ("footer.php")?>
     $(document).on("click", ".btn-caducado", function() {
     console.log("Botón de edición clickeado");
         var id = $(this).data("id");
-        $.post("https://saludapos.com/AdminPOS/Modales/IngresaloACedisDevolucion.php", { id: id }, function(data) {
+        $.post("https://saludapos.com/AdminPOS/Modales/IngresaloASucursalesDevolucion.php", { id: id }, function(data) {
             $("#form-edit").html(data);
             $("#Titulo").html("Productos Caducados");
             $("#Di").removeClass("modal-dialog modal-lg modal-notify modal-info");
