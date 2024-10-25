ae9527e920afa03f2525286e9d696d770b600312
SaludaDev <155038647+YisusDevZer0@users.noreply.github.com>
2024-10-25 16:19:13 -0600

    Update CalendarioDeCitas.php

--- AgendaDeCitas/CalendarioDeCitas.php
+++ AgendaDeCitas/CalendarioDeCitas.php
@@ -1,9 +1,17 @@
+<?php
+include "Consultas/Consultas.php";
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendario de Citas</title>
+    
+  <?php include "Header.php"?>
     <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -32,7 +40,7 @@
     </style>
 </head>
 <body>
-
+<?php include_once ("Menu.php")?>
 <div class="container my-4">
     <!-- Filtro para especialistas y sucursales -->
     <form id="filter-form" class="mb-3">
@@ -67,6 +75,11 @@
     </div>
   </div>
 </div>
+
+<?php
+  
+
+  include ("footer.php")?>
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 <script>
