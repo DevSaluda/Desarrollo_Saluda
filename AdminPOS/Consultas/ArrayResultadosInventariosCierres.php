f23c8406f923501d54d75d966b6cc622eecaf7be
SaludaDev <saludasistemas@gmail.com>
2024-11-14 15:20:13 -0600

    Update ArrayResultadosInventariosCierres.php

--- AdminPOS/Consultas/ArrayResultadosInventariosCierres.php
+++ AdminPOS/Consultas/ArrayResultadosInventariosCierres.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $c = 0;
         while ($fila = $result->fetch_assoc()) {
             
-            $data[$c]["IdbD"] = '<input type="text" name="Cod_Barra[]" value="' . $fila["Cod_Barra"] . '" readonly>';
-            $data[$c]["Cod_Barra"] = '<input type="text" name="Nombre_Prod[]" value="' . $fila["Nombre_Prod"] . '" readonly>';
-            $data[$c]["NombreSucursal"] = '<input type="text" name="Nombre_Sucursal[]" value="' . $fila["Nombre_Sucursal"] . '" readonly>';
-            $data[$c]["Destino"] = '<input type="text" name="Nombre_Sucursal[]" value="' . $Destino . '" readonly>';
+            $data[$c]["IdbD"] = '<input type="text" class="form-control form-control-sm" name="Cod_Barra[]" value="' . $fila["Cod_Barra"] . '" readonly>';
+            $data[$c]["Cod_Barra"] = '<input type="text" class="form-control form-control-sm" name="Nombre_Prod[]" value="' . $fila["Nombre_Prod"] . '" readonly>';
+            $data[$c]["NombreSucursal"] = '<input type="text" class="form-control form-control-sm" name="Nombre_Sucursal[]" value="' . $fila["Nombre_Sucursal"] . '" readonly>';
+            $data[$c]["Destino"] = '<input type="text" class="form-control form-control-sm" name="Nombre_Sucursal[]" value="' . $Destino . '" readonly>';
           
-            $data[$c]["Nombre_Prod"] = '<input type="text" name="Contabilizado[]" value="' . $fila["Contabilizado"] . '" readonly>';
-            $data[$c]["Cod_Enfermeria"] = '<input type="text" name="AgregadoPor[]" value="' . $fila["AgregadoPor"] . '" readonly>';
-            $data[$c]["FechaInventario"] = '<input type="text" name="FechaInventario[]" value="' . $fila["FechaInventario"] . '" readonly
+            $data[$c]["Nombre_Prod"] = '<input type="text" class="form-control form-control-sm" name="Contabilizado[]" value="' . $fila["Contabilizado"] . '" readonly>';
+            $data[$c]["Cod_Enfermeria"] = '<input type="text" class="form-control form-control-sm" name="AgregadoPor[]" value="' . $fila["AgregadoPor"] . '" readonly>';
+            $data[$c]["FechaInventario"] = '<input type="text" class="form-control form-control-sm" name="FechaInventario[]" value="' . $fila["FechaInventario"] . '" readonly
              <input type="text" hidden class="form-control" name="Folio_Prod_Stock[]" value="' . $fila["Folio_Prod_Stock"] . '"  readonly>
                 <input type="text" hidden class="form-control" name="Precio_Venta[]" value="' . $fila["Precio_Venta"] . '"  readonly>
                 <input type="text" hidden class="form-control"  name="Precio_C[]" value="' . $fila["Precio_C"] . '" readonly>';
