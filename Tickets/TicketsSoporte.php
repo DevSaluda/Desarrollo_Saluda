e5f1bf701e50ceada371cbdbd7e97f1c14eb734a
Cabowebo <soporte.sistemas.aux@gmail.com>
2024-12-12 12:05:26 -0600

    Update TicketsSoporte.php

--- Tickets/TicketsSoporte.php
+++ Tickets/TicketsSoporte.php
@@ -46,7 +46,7 @@ table td {
 
 
 </div><div class="col-md-12">
-<div id="resultadoTicket"></div>
+<div id="RegistrosTicketSoporteTabla"></div>
   <!-- /.content-wrapper -->
 
   <!-- Control Sidebar -->
