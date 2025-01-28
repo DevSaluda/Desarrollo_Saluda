89529e0d6c5c6dc33db0faa5ebb363968d6c81a4
SaludaDev <saludasistemas@gmail.com>
2025-01-28 12:17:26 -0600

    Update AbonoCredito.php

--- POS2/Modales/AbonoCredito.php
+++ POS2/Modales/AbonoCredito.php
@@ -177,7 +177,7 @@ while ($r=$query->fetch_object()){
 </div>
 <!-- FORMULARIO DE REIMPRESION DE TICKETS -->
 <!-- FORMULARIO DE ACTUALIZACION DE SALDO-->
-<div style="display: none;">
+
 <form action="javascript:void(0)" method="post" id="ActualizaSaldo" >
 
     <div class="form-group">
@@ -250,4 +250,4 @@ precio2.addEventListener("change", () => {
 </script>
 
 
-<script src="js/AbonoCreditosDentales.js"></script>
+<script src="js/AbonaCreditosDentales.js"></script>
