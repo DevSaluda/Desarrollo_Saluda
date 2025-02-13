230c3619c87eb3df2748414eb151ccb1ddfdb80b
SaludaDev <saludasistemas@gmail.com>
2025-02-13 10:52:55 -0600

    Update LotesYFechasEnGeneral.php

--- AdminPOS/Consultas/LotesYFechasEnGeneral.php
+++ AdminPOS/Consultas/LotesYFechasEnGeneral.php
@@ -37,6 +37,7 @@ while ($fila = $result->fetch_assoc()) {
     
     $data[$c]["Comentario"] =  $fila["lote"];
     $data[$c]["FechaInventario"] = $fila["fecha_caducidad"];
+    $data[$c]["Piezas"] = $fila["Cantidad"];
     $data[$c]["Tipo_Ajuste"] = $fila["estatus"];
     $data[$c]["PrecioVenta"] = $fila["Precio_Venta"];
     $data[$c]["PrecioCompra"] = $fila["Precio_C"];
