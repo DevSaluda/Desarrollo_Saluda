7294e6031a75f8b2d74932967a3843b9b72d44d4
SaludaDev <saludasistemas@gmail.com>
2025-02-13 15:29:09 -0600

    Update LotesYFechasCaducidadesGeneral.php

--- AdminPOS/LotesYFechasCaducidadesGeneral.php
+++ AdminPOS/LotesYFechasCaducidadesGeneral.php
@@ -46,14 +46,7 @@ include "Consultas/Consultas.php";
   </div>
   
   <div >
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#FiltroPorFechasInventarios"
-      class="btn btn-default">
-      Busqueda por fechas <i class="fas fa-search"></i>
-    </button>
-    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#BajaDeInventarios"
-      class="btn btn-default">
-      Baja por cierre <i class="fa-sharp fa-solid fa-shop-lock"></i>
-    </button>
+  
 </div>
 </div>
     
