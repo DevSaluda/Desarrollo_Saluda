165ddaea4d0b5b15bd3eafe69a0e657936f99c39
YisusDevZer0 <jesusemutul@gmail.com>
2025-03-25 21:56:59 -0600

    Ajustes en js

--- AdminPOS/footer.php
+++ AdminPOS/footer.php
@@ -9,5 +9,5 @@
 
 ?>
 <script src="js/LogSalida.js"></script>
-<script src="js/ModalFiltraadoDinamico"></script>
+<script src="js/ModalFiltraadoDinamico.js"></script>
 <script src="https://saludapos.com/AdminPOS/js/Refresca.js"></script>
