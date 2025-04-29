2e590bf0c8d60a4e94bdcfe0a9d7257f8ddabf12
Cabowebo <soporte.sistemas.aux@gmail.com>
2025-04-29 10:36:41 -0600

    LOG

--- ServiciosEspecializados/Consultas/SubeFotos.php
+++ ServiciosEspecializados/Consultas/SubeFotos.php
@@ -1,6 +1,11 @@
 <?php
 	include('conn.php');
 	
+	// Manejo de errores y logging
+	function log_debug($msg) {
+	    file_put_contents(__DIR__ . '/debug_subefotos.log', date('Y-m-d H:i:s') . " - " . $msg . "\n", FILE_APPEND);
+	}
+
 	foreach ($_FILES['upload']['name'] as $key => $name){
 		$newFilename = time() . "_" . $name;
 		move_uploaded_file($_FILES['upload']['tmp_name'][$key], 'upload/' . $newFilename);
@@ -11,8 +16,16 @@
 
 	// Actualizar el estatus en Resultados_Ultrasonidos para el paciente
 	$estatus = isset($_POST['Estatus']) ? $_POST['Estatus'] : '';
+	log_debug("Valor recibido en Estatus: '" . $estatus . "' para paciente: '" . $_POST['Nombre'] . "'");
 	if (!empty($estatus) && !empty($_POST['Nombre'])) {
-	    mysqli_query($conn, "UPDATE Resultados_Ultrasonidos SET Estatus='$estatus' WHERE Nombre_paciente='" . $_POST['Nombre'] . "'");
+	    $update = mysqli_query($conn, "UPDATE Resultados_Ultrasonidos SET Estatus='$estatus' WHERE Nombre_paciente='" . $_POST['Nombre'] . "'");
+	    if ($update) {
+	        log_debug("Actualización exitosa de estatus a '$estatus' para " . $_POST['Nombre']);
+	    } else {
+	        log_debug("Error al actualizar estatus: " . mysqli_error($conn));
+	    }
+	} else {
+	    log_debug("No se recibió Estatus o Nombre para actualizar");
 	}
 
 	header('location:https://controlfarmacia.com/Servicios_Especializados/index');
