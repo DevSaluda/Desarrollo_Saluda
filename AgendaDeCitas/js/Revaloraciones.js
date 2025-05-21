41e8d7e1e88d2922ff9c991d19c99e1cd0571996
Cabowebo <soporte.sistemas.aux@gmail.com>
2025-05-21 15:08:52 -0600

    Update Revaloraciones.js

--- AgendaDeCitas/js/Revaloraciones.js
+++ AgendaDeCitas/js/Revaloraciones.js
@@ -2,7 +2,7 @@ function CargaRevaloraciones() {
 
 
     $.post("https://saludapos.com/AgendaDeCitas/Consultas/RevaloracionesAgendadas.php", "", function(data) {
-        $("#CitasDeRevaloracion").html(data);
+        $("#RevaloracionesAgendadas").html(data);
     })
 
 }
