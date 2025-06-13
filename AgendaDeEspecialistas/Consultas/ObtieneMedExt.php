482808f57804bfb411dda90aa7fec0f69025ffeb
Cabowebo <167923158+Cabowebo@users.noreply.github.com>
2025-06-13 12:22:40 -0600

    PRUEBA FILTRADO POR NOMBRE SUCURSALES Y ESPECIALIDADES

--- AgendaDeEspecialistas/Consultas/ObtieneMedExt.php
+++ AgendaDeEspecialistas/Consultas/ObtieneMedExt.php
@@ -1,12 +1,13 @@
 <?php
 	include("ConeSelectDinamico.php");
 	$sucursal=intval($_REQUEST['sucursalExt']);
-	$medicos = $conn->prepare("SELECT * FROM Especialidades_Express WHERE Estatus_Especialidad='Disponible' AND Fk_Sucursal = '$sucursal'") or die(mysqli_error());
-		echo '<option value = "">Selecciona especialidad </option>';
-	if($medicos->execute()){
-		$a_result = $medicos->get_result();
-	}
+	$especialidades = $conn->prepare("SELECT DISTINCT ee.ID_Especialidad, ee.Nombre_Especialidad FROM Especialidades_Express ee INNER JOIN Personal_Medico_Express pme ON ee.ID_Especialidad = pme.Especialidad_Express WHERE ee.Estatus_Especialidad='Disponible' AND pme.Estatus='Disponible' AND ee.Fk_Sucursal = ?") or die(mysqli_error());
+	echo '<option value = "">Selecciona especialidad </option>';
+	$especialidades->bind_param('i', $sucursal);
+	if($especialidades->execute()){
+		$a_result = $especialidades->get_result();
 		while($row = $a_result->fetch_array()){
-			echo '<option value = "'.$row['ID_Especialidad'].'">'.( $row['Nombre_Especialidad']).'</option>';
+			echo '<option value = "'.$row['ID_Especialidad'].'">'.($row['Nombre_Especialidad']).'</option>';
 		}
+	}
 ?>
--- AgendaDeEspecialistas/Consultas/ObtieneunmemedicoExt2.php
+++ AgendaDeEspecialistas/Consultas/ObtieneunmemedicoExt2.php
@@ -1,13 +1,17 @@
 <?php
 	include("ConeSelectDinamico.php");
-	$sucursal=intval($_REQUEST['especialidadExt']);
-	$medicos = $conn->prepare("SELECT * FROM Personal_Medico_Express WHERE Estatus='Disponible' AND Especialidad_Express = '$sucursal'") or die(mysqli_error());
-		echo '<option value = "">Selecciona un medico </option>';
-	if($medicos->execute()){
-		$a_result = $medicos->get_result();
-	}
-		while($row = $a_result->fetch_array()){
-			echo '<option value = "'.$row['Medico_ID'].'">'.( $row['Nombre_Apellidos']).'</option>';
+	$especialidad = isset($_REQUEST['especialidadExt']) ? intval($_REQUEST['especialidadExt']) : 0;
+	$sucursal = isset($_REQUEST['sucursalExt']) ? intval($_REQUEST['sucursalExt']) : 0;
+	echo '<option value = "">Selecciona un medico </option>';
+	if($especialidad && $sucursal) {
+		$medicos = $conn->prepare("SELECT Medico_ID, Nombre_Apellidos FROM Personal_Medico_Express WHERE Estatus='Disponible' AND Especialidad_Express = ? AND Fk_Sucursal = ?") or die(mysqli_error());
+		$medicos->bind_param('ii', $especialidad, $sucursal);
+		if($medicos->execute()){
+			$a_result = $medicos->get_result();
+			while($row = $a_result->fetch_array()){
+				echo '<option value = "'.$row['Medico_ID'].'">'.($row['Nombre_Apellidos']).'</option>';
+			}
 		}
-		//PRUEBA
+	}
+	//PRUEBA
 ?>
