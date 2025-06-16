03438c9b8f75c42e319f96d8a0edd9a4d7317704
Cabowebo <167923158+Cabowebo@users.noreply.github.com>
2025-06-16 10:20:59 -0600

    Update AgendarCitasExt.php

--- AgendaDeEspecialistas/Modales/AgendarCitasExt.php
+++ AgendaDeEspecialistas/Modales/AgendarCitasExt.php
@@ -209,10 +209,9 @@
 </div>
     </div>
     </div>  
-    <input type="text" class="form-control" name="UsuarioExt" id="usuarioExt"  value="<?php echo $row['Nombre_Apellidos']?>"  hidden readonly >
-    <input type="text" class="form-control" name="SistemaExt" id="sistemaExt"  value="Agenda de citas" hidden  readonly >
-    <input type="text" class="form-control" name="EmpresaExt" id="empresaExt"  value="<?php echo $row['ID_H_O_D']?>" hidden  readonly >
-
+    <input type="hidden" class="form-control" name="UsuarioExt" id="usuarioExt" value="<?php echo isset($_SESSION['Nombre_Medico']) ? $_SESSION['Nombre_Medico'] : ''; ?>">
+<input type="hidden" class="form-control" name="SistemaExt" id="sistemaExt" value="Agenda de citas">
+<input type="hidden" class="form-control" name="EmpresaExt" id="empresaExt" value="Saluda">
     
 <button type="submit"  name="submit_AgeExt" id="submit_AgeExt"  class="btn btn-success">Confirmar datos <i class="fas fa-user-check"></i></button>
     </div>    </div>
