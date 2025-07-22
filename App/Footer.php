18e03f789bba68ba992f9a302bd0a35034814077
DevZero <jesusemutul@gmail.com>
2025-07-21 22:43:36 -0600

    d

--- App/Footer.php
+++ App/Footer.php
@@ -6,8 +6,7 @@
   <div class="footer-copyright text-center py-3">Copyright &copy; <?php echo date('Y'); ?> SALUDA <a href="https://somosgrupoe.com/">Somos Grupo E</a> <br>
   <b>Control Farmacia</b> V 1.3.1
   </div>
-  <!-- Copyright -->
-
+  
 </footer>
 <!-- Footer -->
 
