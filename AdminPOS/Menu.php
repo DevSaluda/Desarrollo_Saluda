5582fa34436cc2299809caf88c817eb1d2746125
Cabowebo <167923158+Cabowebo@users.noreply.github.com>
2025-08-25 10:44:39 -0600

    Update Menu.php

--- AdminPOS/Menu.php
+++ AdminPOS/Menu.php
@@ -1369,6 +1369,16 @@ if ($row['Permisos'] == 0) {
                     </p>
                   </a>
              </li>
+
+             <li class="nav-item">
+                  <a href="https://saludapos.com/AdminPOS/TicketsSoporte" class="nav-link" style="color: white;">
+                  <i class="fa-solid fa-ticket-alt"></i>
+                    <p>
+                    Tickets de Soporte
+
+                    </p>
+                  </a>
+             </li>
           <div  style=<?php switch($row){
 	case $row['Permisos']==10 ;				
   echo "display:none;";
@@ -1554,19 +1564,6 @@ if ($row['Permisos'] == 0) {
           </li>
 
         </ul>
-        <?php if($row['Permisos'] == 0 || $row['Permisos'] == 1): ?>
-        <li class="nav-header" style="font-size: medium; font-weight: bold; color: white;">TICKETS DE INCIDENCIAS <i class="fas fa-code-fork"></i></li>
-        <li class="nav-item">
-            <a href="https://saludapos.com/AdminPOS/TicketsSoporte" class="nav-link">
-            <i class="fas fa-ticket-alt"></i>
-              <p>
-              Registro Tickets de Soporte
-              </p>
-            </a>
-          </li>
-        <?php endif; ?>
-            </ul>
-          </li>
       </nav>
       <!-- /.sidebar-menu -->
     </div>
